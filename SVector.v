f96f73da0ba0961bf7e0048a0246e5ef1c8509b8
Vadim Zaliva <lord@crocodile.org>
2015-06-24 17:54:07 -0700

    rework in progress

--- SVector.v
+++ SVector.v
@@ -17,20 +17,14 @@ Require Import Spiral.
 (* "sparse" vector type *)
 Notation svector A n := (vector (option A) n).
 
-Global Instance sparse_vec_equiv `{Equiv A} {n}: Equiv (svector A n) :=
+Global Instance svector_equiv `{Equiv A} {n}: Equiv (svector A n) :=
   Vforall2 (n:=n) opt_equiv.
 
-Definition SparseCast {A} {n} (v:vector A n): svector A n :=
+Definition svector_from_vector {A} {n} (v:vector A n): svector A n :=
   Vmap (Some) v.
 
-Definition is_Dense {A} {n} (v:svector A n) : Prop :=
+Definition svector_is_dense {A} {n} (v:svector A n) : Prop :=
   Vforall is_Some v.
-
-(*Lemma is_Dense_dec {A} {n} (v:svector A n): Decision(is_Dense v).
-Proof.
-  induction v.
-  auto.
-Qed. *)
 
 Definition from_Some {A} (x:option A) {S: is_Some x}: A.
 Proof.
@@ -40,9 +34,9 @@ Proof.
   tauto.
 Defined.
 
-Lemma dense_get_hd {A} {n} (v:svector A (S n)): is_Dense v -> A.
+Lemma svector_hd {A} {n} (v:svector A (S n)): svector_is_dense v -> A.
 Proof.
-  unfold is_Dense.
+  unfold svector_is_dense.
   intros.
   assert (is_Some (Vhead v)).
   apply Vforall_hd. assumption.
@@ -50,9 +44,10 @@ Proof.
   apply from_Some.
 Defined.
 
-Lemma dense_tl {A} {n} {v: svector A (S n)}: is_Dense v -> is_Dense (Vtail v).
+Lemma svector_tl_dense {A} {n} {v: svector A (S n)}:
+  svector_is_dense v -> svector_is_dense (Vtail v).
 Proof.
-  unfold is_Dense.
+  unfold svector_is_dense.
   intros.
   dep_destruct v.
   simpl in H.
@@ -60,28 +55,55 @@ Proof.
   apply H.
 Defined.
 
-Inductive DenseV {A:Type} {n:nat} : Type :=
-| dvector (v:svector A n) (H:is_Dense v): DenseV.
-
-Lemma DenseVnil {A:Type}: is_Dense (@Vnil (option A)).
+Lemma svector_nil_is_dense {A:Type}: svector_is_dense (@Vnil (option A)).
 Proof.
   crush.
 Qed.
 
-Definition DenseVtail {A:Type} {n:nat} (d:@DenseV A (S n)): @DenseV A n :=
+
+Fixpoint vector_from_svector {A} {n} (v:svector A n) (D:svector_is_dense v): vector A n :=
+  match n return (svector A n) -> (svector_is_dense v) -> (vector A n) with
+  | O => fun _ _ => @Vnil A
+  | (S p) => fun v0 D0 => Vcons
+                            (svector_hd v0 D0)
+                            (vector_from_svector (Vtail v) (svector_tl_dense D))
+  end v D.
+
+(* -------------------------------------------------------- *)
+
+
+(* Inductive type representing both sparse and dense vectors. While underlying
+vector storage structure is the same (svector), in dense case it is paired with
+proof of density.
+
+It should be noted that "sparse" vector could be in fact dense, but we just
+lacking a proof of it.
+ *)
+Inductive Vector {A:Type} {n:nat}: Type :=
+| DVector (v:svector A n) (D:svector_is_dense v): Vector
+| SVector (v:svector A n): Vector.
+
+Definition VectorTail {A:Type} {n:nat} (d:@Vector A (S n)): @Vector A n :=
   match d with
-  | dvector v H => dvector (Vtail v) (dense_tl H)
+  | DVector v D => DVector (Vtail v) (svector_tl_dense D)
+  | SVector v => SVector (Vtail v)
   end.
-  
-Fixpoint DenseCast' {A} {n} (d:@DenseV A n): vector A n :=
-  match n return @DenseV A n -> (vector A n) with
-  | O => fun _ => @Vnil A
-  | (S p) => fun d0 =>
-               match d0 return @DenseV A (S p) -> (vector A (S p)) with
-               | dvector v i =>
-                 fun d2 => Vcons (dense_get_hd v i) (DenseCast' (DenseVtail d2))
-               end d0
-  end d.
+
+Definition VectorDenseExtract  {A} {n} (d:@Vector A n): @maybeError (vector A n) :=
+  match  with
+  | SVector _ => Error "Attempting to extract dense vector from sparse or"
+  | DVector v H => OK (vector_from_svector v H)
+  end.
+    
+
+
+
+
+
+
+
+
+
 
 Definition DenseCast {A} {n} (v:svector A n) (H:is_Dense v): vector A n :=
   DenseCast' (dvector v H).
