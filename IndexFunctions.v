8d5825a76098ff265a79bd284291fab94bd52b1f
Vadim Zaliva <lord@crocodile.org>
2015-08-14 15:23:57 -0400

    starting defining total index functions per PLDI papers. Defined total function composition

--- IndexFunctions.v
+++ IndexFunctions.v
@@ -25,4 +25,27 @@ Definition index_map_dom {d r:nat} (s: index_map_spec d r) := d.
 (* Returns upper rang bound for given `index_map_spec` *)
 Definition index_map_range {d r:nat} (s: index_map_spec d r) := r.
 
+Section TotalIndexMap.
 
+  (* Vector index mapping functon which maps between two sets of natrual
+     numers. *)
+  Definition total_index_map_spec (domain range: nat) :=
+    forall n : nat, n < domain -> {v : nat | v < range}.
+  
+  (* Returns upper domain bound for given `total_index_map_spec` *)
+  Definition total_index_map_dom {d r:nat} (s: total_index_map_spec d r) := d.
+  
+  (* Returns upper rang bound for given `total_index_map_spec` *)
+  Definition total_index_map_range {d r:nat} (s: total_index_map_spec d r) := r.
+
+  Definition total_index_map_compose
+             {i o t: nat}
+             (g: total_index_map_spec t o)
+             (f: total_index_map_spec i t) :
+    total_index_map_spec i o
+    := fun x xdom_bound =>
+         (* manual uncurry *)
+         let (fv, fv_dom_bound) := f x xdom_bound in
+         g fv fv_dom_bound.
+
+End TotalIndexMap.
