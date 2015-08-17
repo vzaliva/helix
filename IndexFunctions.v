9d133d359f719977cdaaca1b6f94ad54b200c27d
Vadim Zaliva <lord@crocodile.org>
2015-08-17 18:14:45 -0400

    stuck proving inequvalities

--- IndexFunctions.v
+++ IndexFunctions.v
@@ -4,6 +4,8 @@ Require Import Coq.Arith.Arith.
 Require Import Coq.Strings.String.
 Require Import Coq.Arith.Peano_dec.
 Require Import Coq.Numbers.Natural.Peano.NPeano.
+Require Import Coq.Sorting.Permutation.
+Require Import Coq.Lists.List.
 
 Require Import CpdtTactics.
 Require Import JRWTactics.
@@ -15,6 +17,8 @@ Require Import MathClasses.interfaces.canonical_names.
 Require Import MathClasses.orders.minmax MathClasses.interfaces.orders.
 Require Import MathClasses.implementations.peano_naturals.
 Require Import MathClasses.orders.orders.
+
+Require Export ListUtil.
 
 Global Open Scope nat_scope.
 
@@ -28,6 +32,69 @@ Definition index_map_dom {d r:nat} (s: index_map_spec d r) := d.
 
 (* Returns upper rang bound for given `index_map_spec` *)
 Definition index_map_range {d r:nat} (s: index_map_spec d r) := r.
+
+Fixpoint natrange_list (n:nat) : list nat :=
+  match n with
+  | 0 => nil
+  | S p => cons p (natrange_list p)
+  end.
+
+Lemma natrange_to_cons (n:nat):
+  natrange_list (S n) ≡ cons n (natrange_list n).
+Proof.
+  auto.
+Qed.
+
+Lemma natrage_le (n:nat):
+  forall x, In x (natrange_list n) -> x<n.
+Proof.
+  intros.
+  induction n.
+  simpl in H. contradiction.
+
+  rewrite natrange_to_cons in H.
+  rewrite In_cons in H.
+  destruct H; crush.
+Qed.
+
+
+Lemma lt_pred_l {n m} (H: S n < m): n < m.
+Proof.
+  auto with arith.
+Defined.
+
+Fixpoint natrange_f_spec
+         (n:nat)
+         {i o: nat}
+         (nd: n<i)
+         (f_spec: index_map_spec i o)
+  : list nat
+  :=
+    match n return list nat with
+    | 0 => nil
+    | S n' => cons n' (natrange_f_spec n' (lt_pred_l nd) f_spec)
+    end.
+
+Program Definition index_map_f_is_permutation
+           {n: nat}
+           (f_spec: index_map_spec n n) :=
+  let i := natrange_list n in
+  let o := map (fun z => proj1_sig (f_spec (natrage_le n z))) i in
+  Permutation i o.
+
+Lemma xxx:
+  (f1: index_func)
+    (f2: index_func)
+    index_map_f_is_permutation f1 ->
+  index_map_f_is_permutation f2 ->
+  index_map_f_is_permutation (tens f1 f2)
+
+Lemma l1: index_map_f_is_permutation (f_id).
+Proof.
+  ...
+  Qed.
+
+index_map_f_is_permutation (f_id) -> .....
 
 Section Primitive_Functions.
   
