00503bbdfe5b0b76971daaefb1ff33d412d72208
Vadim Zaliva <lord@crocodile.org>
2015-11-09 11:51:27 -0800

    tightening pre-conditions for Lemma 3. Trying to prove it for (m) not just (S m)

--- SigmaHCOLRewriting.v
+++ SigmaHCOLRewriting.v
@@ -185,6 +185,29 @@ Section SigmaHCOLRewriting.
       reflexivity.
   Qed.
 
+
+  Lemma Vfold_OptionUnion_empty:
+    ∀ (m : nat) (h : Rtheta) (x : svector m),
+      Is_Val h -> Vforall Is_Struct x → Vfold_left Union h x ≡ h.
+  Proof.
+    intros m h x V E.
+    induction x.
+    auto.
+    simpl.
+    simpl in E. destruct E as [Eh Ex].
+    rewrite IHx; try assumption.
+
+    (* housekeeping *)
+    rename h into v.
+    rename h0 into s.
+    clear IHx Ex.
+
+    unfold Is_Val in V.
+    destruct_Rtheta v. destruct_Rtheta s.
+    destruct v1, v2, s1, s2; crush.
+  Qed.
+  
+  
   Lemma Union_Val_with_structs:
     ∀ x , Is_Val x -> Union x Rtheta_szero ≡ x.
   Proof.
@@ -194,11 +217,86 @@ Section SigmaHCOLRewriting.
     unfold Is_Val, Is_Struct, Is_SErr in H.
     destruct x1, x2; crush.
   Qed.
-  
+
+  Lemma Lemma3 m j (x:svector m) (jc:j<m):
+    (forall i (ic:i<m),
+        (i ≡ j -> Is_Val (Vnth x ic)) /\ (i ≢ j -> Is_Struct (Vnth x ic)))
+    -> (VecUnion x ≡ Vnth x jc).
+  Proof.
+    intros SZ.
+    dependent induction m.
+    - dep_destruct x.
+      destruct j.
+      omega.
+      omega.
+    -
+      dep_destruct x.
+      destruct (eq_nat_dec j 0).
+      +
+        rewrite Vnth_cons_head; try assumption.
+        unfold VecUnion.
+        simpl.
+        
+
+        (*
+        rewrite Vnth_cons_head; try assumption.
+        unfold VecUnion.
+        simpl.
+        replace (Vfold_right Union x0 Rtheta_szero) with (VecUnion x0)
+          by (unfold VecUnion; reflexivity).
+        rewrite IHm.
+         *)
+
+        assert(Vforall Is_Struct x0).
+        {
+          apply Vforall_nth_intro.
+          intros.
+          assert(ipp:S i < S m) by lia.
+          replace (Vnth x0 ip) with (Vnth (Vcons h x0) ipp) by apply Vnth_Sn.
+          apply SZ; lia.
+        }
+        
+        assert(Is_Val h).
+        {
+          specialize SZ with (i:=j) (ic:=jc).
+          destruct SZ as [SZ0 SZ1].
+          subst j.
+          simpl in *.
+          apply SZ0.
+          reflexivity.
+        }
+
+        apply Vfold_OptionUnion_empty.
+        apply Vfold_OptionUnion_empty; assumption.
+      +
+        assert(Zc: 0<(S (S m))) by lia.
+        assert (H0: is_None (Vnth (Vcons h x0) Zc))
+          by (apply SZ; auto).
+        rewrite Vnth_0 in H0. simpl in H0.
+        rewrite is_None_def in H0.
+        subst h.
+
+        rewrite VecOptionUnion_Cons_None.
+        
+        destruct j as [j0|js]; try congruence.
+        assert(jcp : js < S m) by lia.
+        rewrite Vnth_Sn with (ip':=jcp).
+
+        rewrite <-IHm.
+        reflexivity.
+        intros i ic inej.
+
+        assert(ics: (S i) < S (S m)) by lia.
+        rewrite <- Vnth_Sn with (v:=None) (ip:=ics).
+        apply SZ.
+        auto.      
+  Qed.
   
   (* Unary union of vector where all except exactly one element are "structural", and one is unknown, is the value of this element  *)
   Lemma Lemma3 m j (x:svector (S m)) (jc:j<(S m)):
-    (forall i (ic:i<(S m)) (inej: i ≢ j), Is_Struct (Vnth x ic)) -> (VecUnion x ≡ Vnth x jc).
+    (forall i (ic:i<(S m)),
+        (i ≡ j -> Is_Val (Vnth x ic)) /\ (i ≢ j -> Is_Struct (Vnth x ic)))
+    -> (VecUnion x ≡ Vnth x jc).
   Proof.
     intros SZ.
     
@@ -206,11 +304,16 @@ Section SigmaHCOLRewriting.
     - dep_destruct x.
       dep_destruct x0.
       destruct j.
+      assert(HV: Is_Val (Vnth [h] jc)).
+      {
+        apply SZ ;reflexivity.
+      }
       simpl.
       rewrite VecUnion_cons.
       unfold VecUnion; simpl.
-
-        
+      apply Union_Val_with_structs.
+      apply HV.
+      omega.
     (* got IHm *)
       
     - dep_destruct x.
@@ -371,20 +474,6 @@ Section SigmaHCOLRewriting.
 
 
   (* --------------- OLD STUFF BELOW ------------------ *)
-  
-  Lemma Vfold_OptionUnion_empty:
-    ∀ (m : nat) (h : option A) (x : vector (option A) m),
-      Vforall is_None x → Vfold_left OptionUnion h x ≡ h.
-  Proof.
-    intros m h x E.
-    induction x.
-    auto.
-    simpl.
-    simpl in E. destruct E as [Eh Ex].
-    rewrite IHx; try assumption.
-    dep_destruct h0; none_some_elim.
-    destruct h; auto.
-  Qed.
   
   (* Inductive definition of sparse vector which has at most one non-empty element. It is called "VecOptUnionCompSvector compatible" *)
   Inductive VecOptUnionCompSvector {B}: forall {n} (v: svector B n), Prop :=
