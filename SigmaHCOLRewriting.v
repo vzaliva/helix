fef50f26218e226f743e8de42109f0207c46885c
Vadim Zaliva <lord@crocodile.org>
2015-11-12 18:12:26 -0800

    cleanup

--- SigmaHCOLRewriting.v
+++ SigmaHCOLRewriting.v
@@ -44,12 +44,12 @@ Section SigmaHCOLRewriting.
     `{ASRO: !@SemiRingOrder A Ae Aplus Amult Az A1 Ale}
     `{ASSO: !@StrictSetoidOrder A Ae Alt}
   .
-
+  
   Add Ring RingA: (stdlib_ring_theory A).
   
   Open Scope vector_scope.
   Global Open Scope nat_scope.
-
+  
   Fact ScatH_j1_domain_bound base o (bc:base<o):
     (base+(pred 1)*1) < o.
   Proof.
@@ -261,7 +261,7 @@ Section SigmaHCOLRewriting.
     rewrite IHx; try assumption.
     rewrite Union_Val_with_Struct; try assumption.
     reflexivity.
-
+  Qed.
     
   Lemma Lemma3 m j (x:svector m) (jc:j<m):
     (forall i (ic:i<m),
@@ -533,198 +533,6 @@ Section SigmaHCOLRewriting.
     apply Vforall2_intro_nth.
     intros i ip.
     apply U_SAG1; assumption.
-  Qed.
-  
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  (* --------------- OLD STUFF BELOW ------------------ *)
-  
-  (* Inductive definition of sparse vector which has at most one non-empty element. It is called "VecOptUnionCompSvector compatible" *)
-  Inductive VecOptUnionCompSvector {B}: forall {n} (v: svector B n), Prop :=
-  | VecOptUnionCompSvector_nil: VecOptUnionCompSvector []
-  | VecOptUnionCompSvector_none {n} (v: svector B n): VecOptUnionCompSvector v -> VecOptUnionCompSvector (None::v)
-  | VecOptUnionCompSvector_some {x} {n} (v: svector B n): Vforall is_None v -> VecOptUnionCompSvector (Some x::v).
-
-  Lemma VecOptUnionCompSvector_spec {B} {n} {x:svector B n}:
-    VecOptUnionCompSvector x ->
-    forall i j (ic:i< n) (jc:j<n), (Vnth x ic ≢ None) /\ (Vnth x jc ≢ None) -> i ≡ j.
-  Proof.
-    intros V i j ic jc U.
-    destruct U as [Ui Uj].
-    dependent induction V.
-    destruct i, j; crush.
-
-    destruct i, j.
-    reflexivity.
-    + rewrite Vnth_0 in Ui.
-      simpl in Ui.
-      none_some_elim.
-    + rewrite Vnth_0 in Uj.
-      simpl in Uj.
-      none_some_elim.
-    +
-      assert (ic':i < n) by omega.
-      assert (jc':j < n) by omega.
-      f_equal.
-      apply IHV with (ic:=ic') (jc:=jc').
-      simpl in Ui.
-      replace (lt_S_n ic) with ic' in Ui by apply proof_irrelevance.
-      assumption.
-      simpl in Uj.
-      replace (lt_S_n jc) with jc' in Uj by apply proof_irrelevance.
-      assumption.
-    +
-      destruct i, j.
-      - reflexivity.
-      - simpl in Uj.
-        assert (jc':j < n) by omega.
-        replace (lt_S_n jc) with jc' in Uj by apply proof_irrelevance.
-        apply Vforall_nth with (ip:=jc') in H.
-        rewrite is_None_def in H.
-        congruence.
-      - simpl in Ui.
-        assert (ic':i < n) by omega.
-        replace (lt_S_n ic) with ic' in Ui by apply proof_irrelevance.
-        apply Vforall_nth with (ip:=ic') in H.
-        rewrite is_None_def in H.
-        congruence.
-      - simpl in Uj.
-        assert (jc':j < n) by omega.
-        replace (lt_S_n jc) with jc' in Uj by apply proof_irrelevance.
-        apply Vforall_nth with (ip:=jc') in H.
-        rewrite is_None_def in H.
-        congruence.
-  Qed.
-
-
-  Lemma VecOptionUnion_Cons_None:
-    ∀ (m : nat) (x : vector (option A) (S m)),
-      VecOptUnion (Vcons None x) ≡ VecOptUnion x.
-  Proof.
-    intros m x.
-    rewrite VecOptionUnion_cons.
-    simpl.
-    destruct (VecOptUnion x); reflexivity.
-  Qed.
-
-  Lemma SparseUnion_Cons_None:
-    ∀ (n : nat) (x : vector (option A) n), SparseUnion (Vconst None n) x ≡ x.
-  Proof.
-    intros n x.
-    induction x.
-    auto.
-    destruct h;
-    (simpl;
-     rewrite IHx;
-     reflexivity).
-  Qed.
-
-  
-  (* Unary union of vector where all except exactly one element are "structural zero", and one is unknown, is the value of this element  *)
-  Lemma Lemma3 m j (x:svector A (S m)) (jc:j<(S m)):
-      (forall i (ic:i<(S m)) (inej: i ≢ j), is_None (Vnth x ic)) -> (VecOptUnion x ≡ Vnth x jc).
-  Proof.
-    intros SZ.
-
-    dependent induction m.
-    - dep_destruct x.
-      dep_destruct x0.
-      destruct j; crush.
-      (* got IHm *)
-
-    - dep_destruct x.
-      destruct (eq_nat_dec j 0).
-      +
-        rewrite Vnth_cons_head; try assumption.
-        unfold VecOptUnion.
-        simpl.
-        
-        assert(Vforall is_None x0).
-        {
-          apply Vforall_nth_intro.
-          intros.
-          assert(ipp:S i < S (S m)) by lia.
-          replace (Vnth x0 ip) with (Vnth (Vcons h x0) ipp) by apply Vnth_Sn.
-          apply SZ; lia.
-        }
-
-        apply Vfold_OptionUnion_empty; assumption.
-      +
-        assert(Zc: 0<(S (S m))) by lia.
-        assert (H0: is_None (Vnth (Vcons h x0) Zc))
-          by (apply SZ; auto).
-        rewrite Vnth_0 in H0. simpl in H0.
-        rewrite is_None_def in H0.
-        subst h.
-
-        rewrite VecOptionUnion_Cons_None.
-        
-        destruct j as [j0|js]; try congruence.
-        assert(jcp : js < S m) by lia.
-        rewrite Vnth_Sn with (ip':=jcp).
-
-        rewrite <-IHm.
-        reflexivity.
-        intros i ic inej.
-
-        assert(ics: (S i) < S (S m)) by lia.
-        rewrite <- Vnth_Sn with (v:=None) (ip:=ics).
-        apply SZ.
-        auto.
-  Qed.
-
-  Lemma SparseUnionWithEmpty:
-    ∀ (m : nat) (x : vector (option A) m), SparseUnion (szero_svector m) x ≡ x.
-  Proof.
-    intros m x.
-    induction x.
-    auto.
-    destruct h;
-    (simpl;
-    rewrite SparseUnion_Cons_None; reflexivity).
-  Qed.
-
-  Lemma AbsorbUnionIndexBinary:
-    ∀ (m k : nat) (kc : k < m) (h t : vector (option A) m),
-      Vnth (SparseUnion t h) kc ≡ OptionUnion (Vnth t kc) (Vnth h kc).
-  Proof.
-    intros m k kc h t.
-    unfold SparseUnion.
-    apply Vnth_map2.
-  Qed.
-
-  Lemma Lemma2:
-    forall var st m n (x: vector (svector A m) n) k (kc: k<(m*n)), exists i (ic:i<n) j (jc:j<m),
-        Vnth (Vnth x ic) jc ≡ Vnth
-                                (Vfold_left SparseUnion (szero_svector (m * n))
-                                            (Vbuild 
-                                               (fun (i : nat) (ic : i < n) =>
-                                                  evalScatH (update st var i)
-                                                            (AMult (AValue var) (AConst m)) 
-                                                            (AConst 1)
-                                                            (Vnth x ic)
-                                ))) kc.
-  Proof.
-    intros var st m n x k kc.
   Qed.
   
   Section SigmaHCOLRewriting.
