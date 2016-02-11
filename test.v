69657bdb562b2acb925c7d8b32cb698536c37180
Vadim Zaliva <lord@crocodile.org>
2016-02-10 16:52:13 -0800

    using writer monad

--- test.v
+++ test.v
@@ -1,43 +1,47 @@
 Require Import Coq.ZArith.ZArith_base Coq.Strings.String.
-Require Import ExtLib.Data.Monads.StateMonad ExtLib.Structures.Monads.
+Require Import ExtLib.Structures.Monads.
+Require Import ExtLib.Data.Monads.IdentityMonad.
+Require Import ExtLib.Structures.Monoid.
+Require Import ExtLib.Data.Monads.WriterMonad.
+
 Require Import Coq.Bool.BoolEq.
 
+(* https://wiki.haskell.org/All_About_Monads#The_Writer_monad *)
+     
 Section with_monad.
   Import MonadNotation.
   Local Open Scope bool_scope.
   Local Open Scope monad_scope.
 
   Definition FlagsT : Type := bool.
-  Definition FlagsCombine :  FlagsT -> FlagsT -> FlagsT := orb.
   
+  Variable Monoid_B : Monoid FlagsT.
   Variable m : Type -> Type.
   Context {Monad_m: Monad m}.
-  Context {State_m: MonadState FlagsT m}.
+  Context {Writer_m: MonadWriter Monoid_B m}.
   
   Definition uop 
              (op: nat -> nat)
              (x: nat) : m nat :=
-    f <- get ;;
-      put (FlagsCombine f (beq_nat x 0)) ;;
-      ret (op x).
+    tell (beq_nat x 0) ;;
+         ret (op x).
   
   Definition bop 
              (op: nat -> nat -> nat)
              (x y: nat) : m nat :=
-    f <- get ;;
-      put (FlagsCombine f (orb (beq_nat x 0) (beq_nat y 0))) ;;
-      ret (op x y).
+    tell (orb (beq_nat x 0) (beq_nat y 0)) ;;
+         ret (op x y).
 
 End with_monad.
 
-Definition m : Type -> Type := state bool.
-Definition ex1 :=  bop m plus 1 2.
-Definition ex2 :=  bop m plus 0 5.
+Definition sticky := Build_Monoid orb false.
+Definition m : Type -> Type := writerT sticky ident .
 
-Compute (runState ex1 false).
-Compute (runState ex1 true).
-Compute (runState ex2 false).
-Compute (runState ex2 true).
+Definition ex1 :=  bop _ m plus 1 2.
+Definition ex2 :=  bop _ m plus 0 5.
+
+Compute (unIdent (runWriterT ex1)).
+Compute (unIdent (runWriterT ex2)).
 
 
 
