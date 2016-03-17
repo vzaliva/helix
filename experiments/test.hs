586a98b18d631b7424b357fab316eef5045593fc
Vadim Zaliva <lord@crocodile.org>
2016-03-16 18:39:00 -0700

    HS. wplus

--- experiments/test.hs
+++ experiments/test.hs
@@ -7,13 +7,11 @@ import Control.Monad.Trans.Maybe
 
 type W = MaybeT (WriterT Any Identity)
 type WInt = W Int
-    
-bplus :: Int -> Int -> WInt
-bplus a b =
-    do
-      tell (Any (a == 0 || b == 0)) ;
-           return (a+b)
 
+-- Regular plus, without collision checking
+wplus :: WInt -> WInt -> WInt
+wplus = liftM2 (+)
+    
 wbplus :: WInt -> WInt -> WInt
 wbplus wa wb =
     do
@@ -27,8 +25,8 @@ runW :: WInt -> (Maybe Int, Bool)
 runW x = let (v,f) = runWriter $ runMaybeT x
          in (v, getAny f)
                   
-ex0 = runW (bplus 1 2) 
-ex1 = runW (bplus 0 2)
+ex0 = runW (wbplus (mzero) (return 2))
+ex1 = runW (wbplus (return 2) (mzero))
 
 ex2 = runW (wbplus (return 1) (return 2))
 ex3 = runW (wbplus (return 0) (return 2))
@@ -37,4 +35,3 @@ ex4 = runW (wbplus (wbplus (return 1) (return 2)) (return 2))
 ex5 = runW (wbplus (wbplus (return 0) (return 2)) (return 2))
 ex6 = runW (wbplus (wbplus (return 1) (return 2)) (return 0))
 
--- ex7 = runW (bplus None 2) 
