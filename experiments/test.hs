4d52120266803def0986cdeb35b5e877b6ccea45
Vadim Zaliva <lord@crocodile.org>
2016-03-16 18:21:22 -0700

    HS 1st approach

--- experiments/test.hs
+++ experiments/test.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 import Control.Monad.Identity
 import Control.Monad.Writer
 import Data.Monoid
 import Data.Maybe
+import Control.Monad.Trans.Maybe
 
-type W = WriterT Any Identity
+type W = WriterT Any (MaybeT Identity)
 type WInt = W Int
     
 bplus :: Int -> Int -> WInt
@@ -20,17 +22,19 @@ wbplus wa wb =
       tell (Any (a == 0 || b == 0)) ;
            return (a+b)
 
-runW :: WInt -> (Int, Bool)
-runW x = let (v,f) = runWriter x
-         in (v,getAny f)
+
+runW (x::WInt) = runIdentity $ runMaybeT $ runWriterT x
+
+-- runW :: WInt -> (Int, Bool)
+-- runW x = let (v,f) = runWriter (fromJust x)
+--          in (v,getAny f)
                   
-ex0 = runW (bplus 1 2) 
-ex1 = runW (bplus 0 2)
+-- ex0 = runW (bplus 1 2) 
+-- ex1 = runW (bplus 0 2)
 
-ex2 = runW (wbplus (return 1) (return 2))
-ex3 = runW (wbplus (return 0) (return 2))
+-- ex2 = runW (wbplus (return 1) (return 2))
+-- ex3 = runW (wbplus (return 0) (return 2))
 
-ex4 = runW (wbplus (wbplus (return 1) (return 2)) (return 2))
-ex5 = runW (wbplus (wbplus (return 0) (return 2)) (return 2))
-ex6 = runW (wbplus (wbplus (return 1) (return 2)) (return 0))
-      
+-- ex4 = runW (wbplus (wbplus (return 1) (return 2)) (return 2))
+-- ex5 = runW (wbplus (wbplus (return 0) (return 2)) (return 2))
+-- ex6 = runW (wbplus (wbplus (return 1) (return 2)) (return 0))
