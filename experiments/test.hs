b57a28ab5fa589e18f0d21f05259b4b1468ea81f
Vadim Zaliva <lord@crocodile.org>
2016-03-17 10:32:38 -0700

    structural as writer monad

--- experiments/test.hs
+++ experiments/test.hs
@@ -6,35 +6,46 @@ import Data.Monoid
 import Data.Maybe
 import Control.Monad.Trans.Maybe
 
-data Structural a = Value a | Struct a deriving (Show)  
 
-instance Monad Structural where
-    return = Value
-    Struct x >>= k = k x
-    Value x >>= k = case (k x) of
-                      Struct y -> Value y
-                      Value y -> Value y -- fail "could not combine values"
-
-
-type WInt = Structural Int
-
-union :: WInt -> WInt -> WInt
-union = liftM2 (+)
+{- Structural flag is Any: a Boolean monoid under conjunction ('&&'). -}
     
-runW :: WInt -> WInt
-runW x = x
+type W = Writer All
+type WInt = W Int
+
+struct :: a -> W a
+struct x = return x
+
+value :: a -> W a
+value x = do tell (All False) ; return x
+    
+-- Regular plus, without collision checking
+wplus :: WInt -> WInt -> WInt
+wplus = liftM2 (+)
+  
+runW :: WInt -> (Int, Bool)
+runW x = let (v,f) = runWriter x
+         in (v, getAll f)
+
+-- ex0 = runW (wplus (mzero) (return 2))
+-- ex1 = runW (wplus (return 2) (mzero))
 
 x :: WInt
-x = return 5
-             
-ex0 = runW (union (Struct 2) (Struct 1))
-ex1 = runW (union (Struct 0) (return 2))
-ex2 = runW (union (return 2) (Struct 3))
+x = value 2
 
-ex3 = runW (union (return 1) (return 2))
-ex4 = runW (union (return 0) (return 2))
+y :: WInt
+y = struct 2
 
-ex5 = runW (union (union (return 1) (return 2)) (return 2))
-ex6 = runW (union (union (return 0) (return 2)) (return 2))
-ex7 = runW (union (union (return 1) (return 2)) (return 0))
+
+ex0 = runW (wplus (struct 2) (struct 1))
+ex1 = runW (wplus (struct 0) (value 2))
+ex2 = runW (wplus (value 2) (struct 3))
+    
+ex3 = runW (wplus (value 1) (value 2))
+ex4 = runW (wplus (value 0) (value 2))
+
+ex5 = runW (wplus (wplus (value 1) (value 2)) (value 2))
+ex6 = runW (wplus (wplus (value 0) (value 2)) (value 2))
+ex7 = runW (wplus (wplus (value 1) (value 2)) (value 0))
+ex8 = runW (wplus (wplus (struct 1) (value 2)) (struct 0))
+ex9 = runW (wplus (wplus (struct 1) (struct 2)) (value 0)) -- collision!
 
