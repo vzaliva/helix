aaba5b5af93295ac3b50ba4b99b1f673cd3b6ab8
Vadim Zaliva <lord@crocodile.org>
2016-03-18 13:51:37 -0700

    rename wplus to plus

--- experiments/test.hs
+++ experiments/test.hs
@@ -19,15 +19,15 @@ value :: a -> W a
 value x = do tell (All False) ; return x
     
 -- Regular plus, without collision checking
-wplus :: WInt -> WInt -> WInt
-wplus = liftM2 (+)
+plus :: WInt -> WInt -> WInt
+plus = liftM2 (+)
   
 runW :: WInt -> (Int, Bool)
 runW x = let (v,f) = runWriter x
          in (v, getAll f)
 
--- ex0 = runW (wplus (mzero) (return 2))
--- ex1 = runW (wplus (return 2) (mzero))
+-- ex0 = runW (plus (mzero) (return 2))
+-- ex1 = runW (plus (return 2) (mzero))
 
 x :: WInt
 x = value 2
@@ -36,16 +36,16 @@ y :: WInt
 y = struct 2
 
 
-ex0 = runW (wplus (struct 2) (struct 1))
-ex1 = runW (wplus (struct 0) (value 2))
-ex2 = runW (wplus (value 2) (struct 3))
+ex0 = runW (plus (struct 2) (struct 1))
+ex1 = runW (plus (struct 0) (value 2))
+ex2 = runW (plus (value 2) (struct 3))
     
-ex3 = runW (wplus (value 1) (value 2))
-ex4 = runW (wplus (value 0) (value 2))
+ex3 = runW (plus (value 1) (value 2))
+ex4 = runW (plus (value 0) (value 2))
 
-ex5 = runW (wplus (wplus (value 1) (value 2)) (value 2))
-ex6 = runW (wplus (wplus (value 0) (value 2)) (value 2))
-ex7 = runW (wplus (wplus (value 1) (value 2)) (value 0))
-ex8 = runW (wplus (wplus (struct 1) (value 2)) (struct 0))
-ex9 = runW (wplus (wplus (struct 1) (struct 2)) (value 0)) -- collision!
+ex5 = runW (plus (plus (value 1) (value 2)) (value 2))
+ex6 = runW (plus (plus (value 0) (value 2)) (value 2))
+ex7 = runW (plus (plus (value 1) (value 2)) (value 0))
+ex8 = runW (plus (plus (struct 1) (value 2)) (struct 0))
+ex9 = runW (plus (plus (struct 1) (struct 2)) (value 0)) -- collision!
 
