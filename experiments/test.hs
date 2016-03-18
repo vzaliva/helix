82e86089b962576eefd6148cacd100b08663e0c7
Vadim Zaliva <lord@crocodile.org>
2016-03-18 14:22:14 -0700

    two writers, w/o collision tracking

--- experiments/test.hs
+++ experiments/test.hs
@@ -9,39 +9,45 @@ import Control.Monad.Trans.Maybe
 
 {- Structural flag is All: a Boolean monoid under conjunction ('&&'). 
 The initial value is 'True' and values are combined using &&.
+
+So simple structural Writer monad is:
+    
+ type W = Writer All
+ type WInt = W Int
+
+Collision flag is Any: A Boolean monoind under disjunction ('||').
+The initial value is 'False' and values are combined using ||
+
+We can combine both using Monad Trasnformers:
 -}
     
-type W = Writer All
-type WInt = W Int
+type S = WriterT All (Writer Any)
+type SInt = S Int
 
-struct :: a -> W a
+struct :: a -> S a
 struct x = return x
-
-value :: a -> W a
-value x = do tell (All False) ; return x
     
--- Regular plus, without collision checking
-plus :: WInt -> WInt -> WInt
+value :: a -> S a
+value x = do (tell (All False)) ; return x
+
+v::SInt
+v = value 2
+s::SInt
+s = struct 2
+                                
+runW :: SInt -> (Int, Bool, Bool)
+runW x = let ((v, f), c) = runWriter (runWriterT x) in
+         (v, getAll f, getAny c)
+
+{- simple lifted (+), without collision tracking -}         
+plus :: SInt -> SInt -> SInt
 plus = liftM2 (+)
-  
-runW :: WInt -> (Int, Bool)
-runW x = let (v,f) = runWriter x
-         in (v, getAll f)
-
-x :: WInt
-x = value 2
-
-y :: WInt
-y = struct 2
-
-
+         
 ex0 = runW (plus (struct 2) (struct 1))
 ex1 = runW (plus (struct 0) (value 2))
 ex2 = runW (plus (value 2) (struct 3))
-    
 ex3 = runW (plus (value 1) (value 2))
 ex4 = runW (plus (value 0) (value 2))
-
 ex5 = runW (plus (plus (value 1) (value 2)) (value 2))
 ex6 = runW (plus (plus (value 0) (value 2)) (value 2))
 ex7 = runW (plus (plus (value 1) (value 2)) (value 0))
