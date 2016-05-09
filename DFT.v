076871b59c2215be88c8e4807e53e39865f23d27
Vadim Zaliva <lord@crocodile.org>
2016-05-09 15:57:55 -0700

    Eq 23 defined

--- DFT.v
+++ DFT.v
@@ -1,7 +1,9 @@
 
 
 Require Export Utf8_core.
+Require Import Coq.Arith.Arith.
 
+Require Coq.Program.Tactics.
 
 Record Matrix (m n : nat).
 
@@ -21,29 +23,44 @@ Bind Scope matrix_scope with Matrix.
 Definition KroneckerProduct {m n p q: nat} (A: Matrix m n) (B: Matrix p q):
   Matrix (m*p) (n*q). Admitted.
 
-Notation "x ⊗ y" := (KroneckerProduct x y) (at level 40, left associativity) : matrix_scope.
+Notation "x ⊗ y" := (KroneckerProduct x y) (at level 50, left associativity) : matrix_scope.
 
 Definition MatrixProduct {m n p: nat} (A: Matrix n m) (B: Matrix m p):
   Matrix n p. Admitted.
 
 Notation "x * y" := (MatrixProduct x y) : matrix_scope.
 
-Local Open Scope matrix_scope.
+Section ShortVectorCooleyTurkeyFFT.
+
+  Local Open Scope matrix_scope.
+
+  Definition Eq15 (m n: nat)
+             (mnz: m ≠ 0)
+             (nnz: n ≠ 0)
+    := ((DFT m) ⊗ (I n)) * (T (m*n) n) *
+       ((I m) ⊗ (DFT n)) * (L (m*n) m).
+
+  Program Definition Eq23 (m0 n0 v:nat)
+          (mnz: m0 ≠ 0)
+          (nnz: n0 ≠ 0)
+          (vzz: v ≠ 0)
+    :=
+      let m := (m0 * v)%nat in
+      let n := (n0 * v)%nat in
+      (((DFT m) ⊗ (I n0)) ⊗ (I v))
+      *
+      (T (m*n) n) *
+      (
+        (I m0)
+          ⊗ ((I n0) ⊗ (L (v*v) v))
+        * ((L n n0) ⊗ (I v))
+        * ((DFT n) ⊗ (I v))
+      ) *
+      ((L (m0*n0*v)%nat m0) ⊗ (I v)).
+  Next Obligation. Proof. ring. Qed.
+  Next Obligation. Proof. ring. Qed.
+  Next Obligation. Proof. ring. Qed.
+  Next Obligation. Proof. ring. Qed.
 
 
-Definition Eq15 (m n: nat) := ((DFT m) ⊗ (I n)) * (T (m*n) n) *
-                            ((I m) ⊗ (DFT n)) * (L (m*n) m).
-
-
-Definition Eq23 (m n v: nat) := (((DFT m) ⊗ (I (n/v))) * (I v)) *
-                              (T (m*n) n) *
-                              (
-                                (I (m/v))
-                                  ⊗ ((I (n/v)) ⊗ (L (v*v) v))
-                                * ((L n (n/v)) ⊗ (I v))
-                                * ((DFT n) ⊗ (I v))
-                              ) *
-                              ((L (m*n/v) (m/v)) ⊗ (I v)).
-
-
-Local Close Scope matrix_scope.
+End ShortVectorCooleyTurkeyFFT.
