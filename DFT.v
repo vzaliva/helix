e577aad9aa2c0c0b3c975d996775a34528837af2
Vadim Zaliva <lord@crocodile.org>
2016-05-09 18:43:28 -0700

    removing brackets

--- DFT.v
+++ DFT.v
@@ -42,6 +42,20 @@ Section Table1Lemmas.
   Lemma Lemma6:
     forall m n, I (m*n) = I m ⊗ I n.
   Admitted.
+
+(*
+  Program Lemma Identity1_6b (m n k:nat) (A: Matrix k k):
+    (L (k*m*n) n) * (L (k*m) m * (A ⊗ I m) * I n) =
+    (I n ⊗ L (k*m) m) *
+    (L (k*n) n * (A ⊗ I n) ⊗ I m) *
+    (I k ⊗ L (m*n) n).
+    .
+
+  Program Lemma Identity1_8b (m0 n v:nat) (A: Matrix n n):
+    let m := (m0 * v) in
+    ((I m ⊗ A) * L (m*n) n) =
+     (I m0 ⊗ L (n*v) v * (A ⊗ I v)) * (L (m0*n) m0 ⊗ I v).
+ *)
 End Table1Lemmas.
 
 Section ShortVectorCooleyTurkeyFFT.
@@ -49,8 +63,8 @@ Section ShortVectorCooleyTurkeyFFT.
 
   Definition Eq15 (m n: nat)
     : Matrix (m * n) (m * n)
-    := ((DFT m) ⊗ (I n)) * (T (m*n) n) *
-       ((I m) ⊗ (DFT n)) * (L (m*n) m).
+    := (DFT m ⊗ I n) * T (m*n) n *
+       (I m ⊗ DFT n) * L (m*n) m.
 
   Obligation Tactic := (Tactics.program_simpl; ring).
   Program Definition Eq23 (m0 n0 v:nat)
@@ -58,16 +72,16 @@ Section ShortVectorCooleyTurkeyFFT.
     :=
       let m := (m0 * v)%nat in
       let n := (n0 * v)%nat in
-      (((DFT m) ⊗ (I n0)) ⊗ (I v))
+      ((DFT m ⊗ I n0) ⊗ I v)
       *
       (T (m*n) n) *
       (
         (I m0)
-          ⊗ ((I n0) ⊗ (L (v*v) v))
-        * ((L n n0) ⊗ (I v))
-        * ((DFT n) ⊗ (I v))
+          ⊗ (I n0 ⊗ L (v*v) v)
+        * (L n n0 ⊗ I v)
+        * (DFT n ⊗ I v)
       ) *
-      ((L (m0*n0*v)%nat m0) ⊗ (I v)).
+      (L (m0*n0*v) m0 ⊗ I v).
 
   Lemma Eq23FromEq15 (m0 n0 v: nat)
         (mnz: m0 ≠ 0)
