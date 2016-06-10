e812134d6f00e7a538836f4ff68722908cc0d5f8
Vadim Zaliva <lord@crocodile.org>
2016-06-10 15:56:36 -0700

    debugging class resolution

---
+++ experiments/rewrite.v
@@ -0,0 +1,12 @@
+Require Import MathClasses.interfaces.abstract_algebra.
+
+Section RewritingTest.
+
+  Variable A:Type.
+
+  Global Instance A_Equiv: Equiv A. Admitted.
+  Global Instance A_Setoid: Setoid A. Admitted.
+
+  Lemma
+
+End RewritingTest.
