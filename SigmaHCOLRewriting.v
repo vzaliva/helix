fcfea20a093f76a77e97ef14ff0fcdd0fb9a57e7
Vadim Zaliva <lord@crocodile.org>
2016-08-17 09:58:37 -0700

    typo

--- SigmaHCOLRewriting.v
+++ SigmaHCOLRewriting.v
@@ -2265,6 +2265,6 @@ Proof.
     auto.
   }
 
-  rewrite rewrite_PointWise_ISumUnion ; [apply apply Apply_Family_SparseEmbedding_Single_NonZero_Per_Row | apply Pre1].
+  rewrite rewrite_PointWise_ISumUnion ; [apply Apply_Family_SparseEmbedding_Single_NonZero_Per_Row | apply Pre1].
 
 Qed.
