79efaa89469e5e4f335a173815047e49537807f3
Vadim Zaliva <lord@crocodile.org>
2016-08-31 17:23:13 -0700

    example for Franz

---
+++ experiments/correct_by_constr.v
@@ -0,0 +1,56 @@
+Require Import Coq.Logic.FunctionalExtensionality.
+Require Import Coq.Program.Basics.
+Require Import Coq.Init.Specif.
+Require Import Coq.Bool.Bool.
+Require Import Coq.ZArith.ZArith.
+Local Open Scope program_scope.
+Local Open Scope Z_scope.
+
+(* Functoins used:
+zsqrt - "Safe" square root, which requires non-negative argument
+Z.abs - absolute value
+Z.sgn - sign (returns -1|0|1)
+ *)
+
+(* "Refined" version of sqrt *)
+Definition zsqrt (x:Z) (ac:x>=0) := Z.sqrt x.
+
+Lemma abs_sgn_comm:
+  (Z.abs ∘ Z.sgn) = (Z.sgn ∘ Z.abs).
+Proof.
+  apply functional_extensionality.
+  intros.
+  unfold compose.
+  destruct x; auto.
+Qed.
+
+Lemma zabs_always_pos:
+  forall x, (Z.abs x) >= 0.
+Proof.
+  intros.
+  unfold Z.abs.
+  destruct x.
+  omega.
+  induction p;auto;omega.
+  induction p;auto;omega.
+Qed.
+
+Lemma zabs_pos:
+  forall x, x>=0 -> Z.abs x = x.
+Proof.
+  intros.
+  destruct x.
+  - auto.
+  - auto.
+  - assert(Z.neg p < 0) by apply Pos2Z.neg_is_neg.
+    omega.
+Qed.
+
+Lemma foo (x:Z) (xp:x>=0):
+  zsqrt (Z.abs x) (zabs_always_pos x) = zsqrt x xp.
+Proof.
+  unfold zsqrt.
+  rewrite zabs_pos.
+  - reflexivity.
+  - apply xp.
+Qed.
