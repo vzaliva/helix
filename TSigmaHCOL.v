a31d77070df28d74bc2cecea30512eade14ab788
Vadim Zaliva <lord@crocodile.org>
2016-10-20 15:12:08 -0700

    TSigmaHCOL.v generalized

--- TSigmaHCOL.v
+++ TSigmaHCOL.v
@@ -23,45 +23,50 @@ Require Import MathClasses.interfaces.abstract_algebra.
 Require Import MathClasses.orders.minmax MathClasses.interfaces.orders.
 Require Import MathClasses.theory.rings.
 
+Require Import ExtLib.Structures.Monoid.
+
 (*  CoLoR *)
 Require Import CoLoR.Util.Vector.VecUtil.
 Import VectorNotations.
 
 Open Scope vector_scope.
 
-Class TSHOperator2 {i1 o1 i2 o2 ix ox} (top: (svector i1 -> svector o1) -> (svector i2 -> svector o2) -> svector ix -> svector ox) :=
-  TSHOperator2_proper :> Proper (((=) ==> (=)) ==> ((=) ==> (=)) ==> (=) ==> (=)) (top).
-
-(* Curried Templete SigmaHCOL operator with arity 2 is SHOperators *)
-Global Instance THOperator2_SHOperator
-       `{O1: @SHOperator i1 o1 op1}
-       `{O2: @SHOperator i2 o2 op2}
-       `{T: @TSHOperator2 i1 o1 i2 o2 ix ox to}:
-  SHOperator (to op1 op2).
-Proof.
-  split; try apply vec_Setoid.
-  apply T ; [apply O1 | apply O2].
-Qed.
-
-
 Section TSigmaHCOLOperators.
+
+  Variable fm:Monoid.Monoid RthetaFlags.
+  Variable fml:@MonoidLaws RthetaFlags RthetaFlags_type fm.
+
+  Class TSHOperator2 {i1 o1 i2 o2 ix ox} (top: (svector fm i1 -> svector fm o1) -> (svector fm i2 -> svector fm o2) -> svector fm ix -> svector fm ox) :=
+    TSHOperator2_proper :> Proper (((=) ==> (=)) ==> ((=) ==> (=)) ==> (=) ==> (=)) (top).
+
+  (* Curried Templete SigmaHCOL operator with arity 2 is SHOperators *)
+  Global Instance THOperator2_SHOperator
+         `{O1: @SHOperator fm i1 o1 op1}
+         `{O2: @SHOperator fm i2 o2 op2}
+         `{T: @TSHOperator2 i1 o1 i2 o2 ix ox to}:
+    SHOperator fm (to op1 op2).
+  Proof.
+    split; try apply vec_Setoid.
+    apply T ; [apply O1 | apply O2].
+  Qed.
+
 
   (* Per Vadim's discussion with Franz on 2015-12-14, ISumUnion is
   just Union of two vectors, produced by application of two operators
   to the input. *)
   Definition HTSUMUnion {i o}
-             (dot:CarrierA->CarrierA->CarrierA)
-             (f: svector i -> svector o)
-             (g: svector i -> svector o)
-             (x: svector i): svector o
-    :=  Vec2Union dot (f x) (g x).
+             (dot: CarrierA -> CarrierA -> CarrierA)
+             (f: svector fm i -> svector fm o)
+             (g: svector fm i -> svector fm o)
+             (x: svector fm i): svector fm o
+    :=  Vec2Union fm dot (f x) (g x).
 
 
   Global Instance TSHOperator2_HTSUMUnion {i o}
-         (dot:CarrierA->CarrierA->CarrierA)
+         (dot: CarrierA -> CarrierA -> CarrierA)
          `{dot_mor: !Proper ((=) ==> (=) ==> (=)) dot}
     :
-    TSHOperator2 (@HTSUMUnion i o dot).
+      TSHOperator2 (@HTSUMUnion i o dot).
   Proof.
     intros f f' Ef g g' Eg x y Ex.
     unfold HTSUMUnion.
