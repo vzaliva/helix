98b93a150bbb258700a744c579877e6740999562
Vadim Zaliva <lord@crocodile.org>
2016-10-25 14:58:28 -0700

    more paper snippets

--- experiments/paper-snippets.v
+++ experiments/paper-snippets.v
@@ -1,8 +1,12 @@
+Variable A:Type.
+Variable dot: A->A->A.
+Notation "x · y" := (dot x y) (at level 80).
+
 (* For paper *)
 Theorem For_Paper_1:
   forall (op: CarrierA -> CarrierA -> CarrierA)
     (a b: Rtheta),
-    execWriter (liftM2 op a b) ≡
+    execWriter (liftM2 op a b) =
                RthetaFlagsAppend (execWriter a) (execWriter b).
 
 Proof.
