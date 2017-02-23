d2939d6092bfa6b3170eee98cd89d0837cc5818b
Vadim Zaliva <lord@crocodile.org>
2017-02-22 21:33:30 -0800

    matching debug lines

--- logparse/parselog.ml
+++ logparse/parselog.ml
@@ -1,19 +1,33 @@
 open Std
 open Str
-open BatList
-open BatFile
-open BatIO
 open String
 
-(* TODO: match mutliline import statements *)
-let import_regexp = Str.regexp "^[ \t]*Require[ \t]+Import[ \t]+\\(\\([A-Za-z_\\.]+\\)\\([ \t]+[A-Za-z_\\.]+\\)*\\)\\."
+let debug_regexp = Str.regexp "^Debug: ?[0-9]+\\(\\.[0-9]+\\)* ?:"
 
 let verbose = ref false
 let debug = ref false
 let fname = ref ""
 
-let process_file fname =
-  print_endline ("File: " ^ fname)
+let process_line l =
+  if string_match debug_regexp l 0 then
+    print_endline ("OK " ^ l)
+  else
+    print_endline ("ERR " ^ l)
+
+let process_file filename =
+  let chan = open_in filename in
+  let rec loop m = 
+    let s = input_line chan in
+    let open BatString in
+    if not (is_empty m) && starts_with s "Debug" then
+      (process_line m ; loop s)
+    else
+      loop (m ^ s)
+  in
+  try
+    loop ""
+  with End_of_file ->
+    close_in chan
 
 let main =
   begin
