e906a375b12e7e4edc5a828d5efaa6dd179fb905
Vadim Zaliva <lord@crocodile.org>
2017-02-23 17:24:15 -0800

    initial classification

--- logparse/parselog.ml
+++ logparse/parselog.ml
@@ -6,24 +6,63 @@ let debug_regexp = Str.regexp "^Debug: ?\\([0-9]+\\(\\.[0-9]+\\)*\\) ?: *"
 
 let verbose = ref false
 let debug = ref false
-let fname = ref ""
+let ifname = ref ""
+let ofname = ref ""
 
-exception UnparseableLine of (string*int)
+exception MissingArg of string
+
+type kind =
+  | Goal
+  | Looking
+  | SimpleApply
+  | SimpleEapply
+  | External
+  | NoMatch
+  | Exact
+  | Unknown
+
+let string_of_kind = function
+  | Looking      -> "Looking"
+  | SimpleApply  -> "SimpleApply"
+  | SimpleEapply -> "SimpleEapply"
+  | External     -> "External"
+  | NoMatch      -> "NoMatch"
+  | Exact        -> "Exact"
+  | Goal         -> "Goal"
+  | Unknown      -> "???"
+
+let classifiers = [
+    (Str.regexp "^looking for", Looking) ;
+    (Str.regexp "^simple apply", SimpleApply) ;
+    (Str.regexp "^simple eapply", SimpleEapply) ;
+    (Str.regexp "^(\\*external\\*)", External) ;
+    (Str.regexp "^no match for", NoMatch) ;
+    (Str.regexp "^(", Goal) ;
+    (Str.regexp "^exact ", Exact) ;
+  ]
+
+let classify l =
+  let rec loop = function
+    | [] -> Unknown
+    | (r,c)::rs -> if string_match r l 0 then c else loop rs
+  in
+  loop classifiers
 
 let process_line l n =
   if string_match debug_regexp l 0 then
     let b = (matched_group 1 l) in
     let me = match_end () in
     let m = (string_after l me) in
-    print_endline b
+    let k = classify m in
+    print_endline (string_of_int n ^ ":" ^ b ^ ":" ^ (string_of_kind k) ^ ":" ^ m)
   else
-    if !debug then print_endline (string_of_int n ^ ":" ^ l)
-    else raise (UnparseableLine (l,n))
+    if !debug && !verbose then print_endline ("Not numbered: " ^ string_of_int n ^ ":" ^ l)
 
-let process_file filename =
-  let chan = open_in filename in
-  let rec loop m start current = 
-    let s = input_line chan in
+let process_file ifilename ofilename =
+  let ic = open_in ifilename in
+  let oc = open_out ofilename in
+  let rec loop m start current =
+    let s = input_line ic in
     let open BatString in
     if not (is_empty m) && starts_with s "Debug" then
       begin
@@ -33,19 +72,27 @@ let process_file filename =
       loop (m ^ s) start (current+1)
   in
   try
+    Printf.fprintf oc "graph {\n" ;
     loop "" 1 1
   with End_of_file ->
-    close_in chan
+       begin
+         Printf.fprintf oc "}\n" ;
+         close_in ic ;
+         close_out oc
+       end
 
 let main =
   begin
   let speclist = [("-v", Arg.Set verbose, "Enables verbose mode");
                   ("-d", Arg.Set debug, "Enables debug mode");
-                  ("-f", Arg.Set_string fname, "File to process");
+                  ("-f", Arg.Set_string ifname, "File to process");
+                  ("-o", Arg.Set_string ofname, "Output file");
                  ]
   in let usage_msg = "Parse log file. Options available:"
      in Arg.parse speclist print_endline usage_msg;
-        process_file !fname
+        if BatString.is_empty !ifname then raise (MissingArg "Must specify -f") ;
+        if BatString.is_empty !ofname then raise (MissingArg "Must specify -o") ;
+        process_file !ifname !ofname
   end
 
 let () = main
