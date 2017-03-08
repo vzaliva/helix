16b43893313c947c9940eb460343f28b0ac11378
Vadim Zaliva <lord@crocodile.org>
2017-03-08 11:28:51 -0800

    removed duplicate. it also requires fmap

--- experiments/dual_monad.hs
+++ experiments/dual_monad.hs
@@ -50,7 +50,6 @@ It is not really needed here yet, except that we might need 'extract' in future
 instance (Monoid w) => Comonad (Writer w) where
     extract x = fst $ runWriter x
     extend f wa = (tell $ execWriter wa) >> return (f wa)
-    duplicate wa = (tell $ execWriter wa) >> return wa
 
 sstruct :: Int -> SInt
 sstruct x = return x
