86a16254b6d0270ad96d52b79ee1eb675b1950f4
Vadim Zaliva <lord@crocodile.org>
2017-03-08 19:12:43 -0800

    initial attempt to formulate monoid laws

--- experiments/comonad.v
+++ experiments/comonad.v
@@ -4,10 +4,13 @@ Require Import ExtLib.Structures.Monoid.
 Require Import ExtLib.Data.Monads.WriterMonad.
 Require Import ExtLib.Data.PPair.
 Require Import ExtLib.Structures.CoMonad.
+Require Import Coq.Program.Basics.
+Require Import Coq.Logic.FunctionalExtensionality.
 
 Set Implicit Arguments.
 
 Import MonadNotation.
+Local Open Scope program_scope.
 Local Open Scope monad_scope.
 
 Section WriterMonad.
@@ -29,6 +32,23 @@ Class CoMonad (m : Type → Type) : Type :=
 
 *)
 
+Section CoMonad_Laws.
+  Variable m : Type -> Type.
+  Variable C : CoMonad m.
+
+  (* Convenience functions, renaming and changing arguments order *)
+  Definition extract {A:Type}: (m A) -> A := @coret m C A.
+  Definition extend {A B:Type} (f: m A -> B): m A -> m B  :=
+    fun x => @cobind m C A B x f.
+
+  Class CoMonadLaws : Type :=
+    {
+      extend_extract: forall (A B:Type), extend (B:=A) extract = id ;
+      extract_extend: forall (A B:Type) {f}, extract ∘ (@extend A B) f = f;
+      extend_extend:forall (A B:Type) {f g},
+          (@extend B A) f ∘ (@extend A B) g = extend (f ∘ extend g)
+    }.
+End CoMonad_Laws.
 
 Instance WriterCoMonad {w:Type} {m: Monoid w}:
   CoMonad (@writer w m) :=
@@ -36,3 +56,23 @@ Instance WriterCoMonad {w:Type} {m: Monoid w}:
     coret A x := evalWriter x ;
     cobind A B wa f := tell (execWriter wa) ;; ret (f wa)
   }.
+
+Instance WriterCoMonadLaws {w:Type} {m: Monoid w}:
+  CoMonadLaws (@WriterCoMonad w m).
+Proof.
+  split.
+  -
+    intros A B.
+    extensionality x.
+    admit.
+  -
+    intros A B f.
+    extensionality x.
+    unfold compose.
+    admit.
+  -
+    intros A B f g.
+    extensionality x.
+    unfold compose.
+    admit.
+Qed.
