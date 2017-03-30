10a5f9f2642189fcd627c6ee45fc1449d0912b83
Vadim Zaliva <lord@crocodile.org>
2017-03-30 14:40:32 -0700

    first working implementation of MSet_set

--- experiments/FinNatSet.v
+++ experiments/FinNatSet.v
@@ -33,31 +33,40 @@ Module M := Make Nat_as_OT.
 
 Section MSet_set.
 
-  Definition has_upper_bound s n := M.fold (fun m b => (m <=? n) && b) s true.
-  Definition MFinNatSet n := {s : M.t | has_upper_bound s n = true}.
+  Definition has_upper_bound s n : bool
+    := M.fold (fun m b => (m <=? n) && b) s true.
+
+  Definition MFinNatSet (n:nat) : Type
+    := {s : M.t | has_upper_bound s n = true}.
 
   Definition MsingleS (n:nat) (i:nat): MFinNatSet n.
   Proof.
     unfold MFinNatSet.
-
     case (lt_dec i n); intros.
     -
       exists (M.singleton i).
-      admit.
+      unfold has_upper_bound.
+      rewrite M.fold_spec.
+      simpl.
+      assert(l1: i<=n).
+      {
+        apply Nat.le_lteq.
+        left.
+        apply l.
+      }
+      apply Nat_as_DT.leb_le in l1.
+      rewrite l1.
+      auto.
     -
       exists M.empty.
       auto.
   Defined.
 
-  Example Foo: Disjoint _ (singleS 5 1) (singleS 5 2).
+  Example MFoo: M.is_empty (M.inter
+                             (proj1_sig (MsingleS 5 2))
+                             (proj1_sig (MsingleS 5 1))) = true.
   Proof.
-    split.
-    intros x.
-    unfold In.
-    unfold not.
-    intros H.
-    destruct H as [x H1 H2].
-    congruence.
+    auto.
   Qed.
 
 End MSet_set.
