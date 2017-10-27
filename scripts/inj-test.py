31e8cf8a6361f0610f575d72531672670b3c01da
Vadim Zaliva <lord@crocodile.org>
2017-10-26 22:12:33 -0700

    test conversions in both directions

--- scripts/inj-test.py
+++ scripts/inj-test.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 
-m = 12
-n = 12
+m =273
+n = 82
 
-def f1(x):
-    return (x/n)+(x%n)*m
+def l(t1):
+    return (t1%n, t1/n)
 
-for i in range(0,m*n):
-    print "%s-%s" % (i,f1(i))
+def r(t2):
+    return (t2/m, t2%m)
 
+def lr(t1):
+    return t1/n + (t1%n)*m
 
+def rl(t2):
+    return t2/m + (t2%m)*n
+    
+for t in range(0,m*n):
+    # left -> right
+    lhs = l(t)
+    rhs = r(lr(t))
+    if lhs != rhs:
+        print "> (%s,%s)\t(%s,%s)" % (lhs+rhs)
+    # right to left
+    rhs = r(t)
+    lhs = l(rl(t))
+    if lhs != rhs:
+        print "< (%s,%s)\t(%s,%s)" % (lhs+rhs)
         
