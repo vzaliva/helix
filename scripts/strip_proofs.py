33caac72784a3a5a61d50a9553758b6c112ffff9
Vadim Zaliva <lord@crocodile.org>
2017-11-25 17:01:10 -0800

    small script to strip proofs

---
+++ scripts/strip_proofs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+
+proof=""
+inproof = False
+
+for s in sys.stdin:
+    if inproof:
+        if s.strip() == "Qed.":
+            sys.stdout.write("Admitted.\n")
+            proof = ""
+            inproof = False
+        elif s.strip() == "Defined.":
+            sys.stdout.write(proof)
+            sys.stdout.write(s)
+            proof = ""
+            inproof = False
+        else:
+            proof += s
+    else:
+        if s.strip() == "Proof.":
+            inproof = True
+            proof += s
+        else:
+            sys.stdout.write(s)
+            
+        
