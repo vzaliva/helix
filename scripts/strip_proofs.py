77d05b2c1bb7d3d2248479db00d3d0a4dbaa7d18
Vadim Zaliva <lord@crocodile.org>
2017-11-27 15:46:07 -0800

    ONCE option

--- scripts/strip_proofs.py
+++ scripts/strip_proofs.py
@@ -4,10 +4,12 @@ import sys
 import re
 
 STRIP_INSTANCE_PROOFS = True
+ONCE = True
 
-proof=""
+proof = ""
 inproof = False
 isintance = False
+passthrough = False
 
 lemma = re.compile(r"^\s*((Global|Local)\s+)?(Lemma|Theorem|Fact|Remark|Corollary|Proposition)\s+.*")
 instance = re.compile(r"^\s*((Global|Local)\s+)?(Instance)\s+.*")
@@ -15,7 +17,16 @@ instance = re.compile(r"^\s*((Global|Local)\s+)?(Instance)\s+.*")
 for s in sys.stdin:
     ss = s.strip()
     if inproof:
-        if ss == "Qed." or ss == "Admitted.":
+        if ss == "Qed.":
+            sys.stdout.write("Admitted.\n")
+            inproof = False
+            if ONCE:
+                sys.stderr.write("Replacing: %s\n" %ss)
+                passthrough = True
+            else:
+                proof = ""
+                isintance = False
+        elif ss == "Admitted.":
             sys.stdout.write("Admitted.\n")
             proof = ""
             inproof = False
@@ -29,14 +40,18 @@ for s in sys.stdin:
         else:
             proof += s
     else:
-        if re.fullmatch(lemma,ss):
-            isintance = False
-        if re.fullmatch(instance,ss):
-            isintance = True
-        if ss == "Proof." and (not(isintance) or STRIP_INSTANCE_PROOFS):
-            inproof = True
-            proof += s
-        else:
+        if passthrough:
             sys.stdout.write(s)
+        else:
+            if re.fullmatch(lemma,ss):
+                isintance = False
+            elif re.fullmatch(instance,ss):
+                isintance = True
+            
+            if ss == "Proof." and (not(isintance) or STRIP_INSTANCE_PROOFS):
+                inproof = True
+                proof += s
+            else:
+                sys.stdout.write(s)
             
         
