541ac443de6845c511cf530ab7a8dca8bac4025e
Vadim Zaliva <lord@crocodile.org>
2017-11-27 22:47:32 -0800

    script to print unused in bug1

---
+++ experiments/bug1_unused.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -f bug1.glob bug1.vo 
+coqc -w none bug1.v 
+
+rm -f depgraph.vcmd graph.dpd unused.txt
+
+echo "Require dpdgraph.dpdgraph." > depgraph.vcmd
+echo "Require bug1." >> depgraph.vcmd
+echo "Print FileDependGraph bug1." >> depgraph.vcmd
+
+coqtop  < depgraph.vcmd
+
+dpdusage -with-path graph.dpd | sort > unused.txt
