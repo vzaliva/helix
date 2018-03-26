a014a78b0900ca59a15262700fa0071eb38db4cd
Vadim Zaliva <lord@crocodile.org>
2018-03-26 16:23:17 -0700

    simplified icode by removing vector types and ops and dereferencing

--- proofs/HCode/Ast.v
+++ proofs/HCode/Ast.v
@@ -8,62 +8,31 @@ Import ListNotations.
 
 Open Scope list_scope.
 
-Inductive inttype :=
-| Int8Type
-| Int16Type
-| Int32Type
-| Int64Type
-| UInt8Type
-| UInt16Type
-| UInt32Type
-| UInt64Type
-| BoolType.
-
-Inductive fptype :=
-| FloatType (* IEEE 32-bit float *)
-| DoubleType (* IEEE 64-bit float *).
-
 Inductive arithtype :=
-| I (value:inttype)
-| F (value: fptype).
+| RealType
+| IntType.
 
 Inductive itype :=
 | A (value:arithtype)
 | VoidType
 | ArrType (t:itype) (len:Z)
-| VecType (t:arithtype) (len:Z)
 | PtrType (t:itype) (alignment:Z).
 
 (* Types for function and vairable names. For simplicity we will index them by integers *)
 Definition varname := Z.
 Definition funcname := string.
 
-Definition float := string.
-
-Inductive fconst :=
-| FLiteral (value:float)
-| FEPS.
-
-Inductive dconst :=
-| DLiteral (value:float)
-| DEPS.
+Definition fconst := string.
 
 Inductive rvalue :=
 | FunCallValue (f:funcall)
 | VarRValue (var:varname)
-| FConst (value:fconst)
-| DConst (value:dconst)
-| IConst (type:inttype) (value:Z)
-| IConstArr (type:inttype) (values: list Z)
-| FConstArr (values: list fconst)
-| DConstArr (values: list dconst)
-| FConstVec (values: list fconst)
-| DConstVec (values: list dconst)
-| IConstVec (type:inttype) (values: list Z)
-| VdupRvalue (r:rvalue) (n:Z)
+| RConst (value:fconst)
+| IConst (value:Z)
+| IConstArr (values: list Z)
+| RConstArr (values: list fconst)
 | NthRvalue (r:rvalue) (index:rvalue)
 | RCast (type:itype) (r:rvalue)
-| RDeref (r:rvalue)
 with funcall :=
      (* polymorphic codnition operator *)
      | F_cond: rvalue -> rvalue -> rvalue -> funcall
@@ -73,7 +42,6 @@ with funcall :=
      | F_max : rvalue -> rvalue -> list rvalue -> funcall
 
      (* polymorphic artihmetic binary operators *)
-
      | F_add : rvalue -> rvalue -> funcall
      | F_sub : rvalue -> rvalue -> funcall
      | F_mul : rvalue -> rvalue -> funcall
@@ -84,48 +52,12 @@ with funcall :=
 
      (* polimorphic unary operators *)
      | F_neg: rvalue -> funcall
-     | F_abs: rvalue -> funcall
-
-     | F_vcvt_64f32f: rvalue -> rvalue -> funcall
-     | F_addsub_4x32f: rvalue -> rvalue -> funcall
-     | F_addsub_2x64f: rvalue -> rvalue -> funcall
-
-     | F_vushuffle_2x64f: rvalue -> rvalue -> funcall
-
-     | F_vshuffle_2x64f: rvalue -> rvalue -> rvalue -> funcall
-     | F_vshuffle_4x32f: rvalue -> rvalue -> rvalue -> funcall
-     | F_vshuffle_8x32f: rvalue -> rvalue -> rvalue -> funcall
-
-     | F_vunpacklo_4x32f: rvalue -> rvalue -> funcall
-     | F_vunpacklo_8x32f: rvalue -> rvalue -> funcall
-     | F_vunpacklo_4x64f: rvalue -> rvalue -> funcall
-
-     | F_vunpackhi_4x32f: rvalue -> rvalue -> funcall
-     | F_vunpackhi_4x64f: rvalue -> rvalue -> funcall
-     | F_vunpackhi_8x32f: rvalue -> rvalue -> funcall
-
-     | F_cmpge_2x64f: rvalue -> rvalue -> funcall
-     | F_testc_4x32i: rvalue -> rvalue -> funcall
-     | F_testnzc_4x32i: rvalue -> rvalue -> funcall
-
-     | F_vpermf128_4x64f: rvalue -> rvalue -> rvalue -> funcall
-     | F_vpermf128_8x32f: rvalue -> rvalue -> rvalue -> funcall
-
-     | F_vload_2l_4x32f: rvalue -> rvalue -> funcall
-     | F_vload_2h_4x32f: rvalue -> rvalue -> funcall
-     | F_vloadu_4x32f: rvalue -> funcall
-
-     | F_vstore_2l_4x32f: rvalue -> rvalue -> funcall
-     | F_vstore_2h_4x32f: rvalue -> rvalue -> funcall
-
-     | F_vstoreu_4x32f: rvalue -> rvalue -> funcall.
+     | F_abs: rvalue -> funcall.
 
 Inductive lvalue :=
 | VarLValue (var:varname)
 | NthLvalue (l:lvalue) (index:rvalue)
-| LDeref (r:rvalue)
 | LCast (type:itype) (l:lvalue).
-
 
 Inductive istmt :=
 | IFunction (name: funcname) (type:itype) (param: list varname) (body:istmt)
