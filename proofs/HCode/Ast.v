7cdedec4de628d25624384565f4681377b462924
Vadim Zaliva <lord@crocodile.org>
2018-03-26 17:42:57 -0700

    naming

--- proofs/HCode/Ast.v
+++ proofs/HCode/Ast.v
@@ -1,22 +1,18 @@
-(* iCode AST *)
+(* hCode AST *)
 
 Require Import Coq.Lists.List.
 Require Import Coq.ZArith.ZArith.
 Require Import Coq.Strings.String.
 
-Import ListNotations.
-
-Open Scope list_scope.
-
 Inductive arithtype :=
 | RealType
 | IntType.
 
-Inductive itype :=
+Inductive htype :=
 | A (value:arithtype)
 | VoidType
-| ArrType (t:itype) (len:Z)
-| PtrType (t:itype) (alignment:Z).
+| ArrType (t:htype) (len:Z)
+| PtrType (t:htype) (alignment:Z).
 
 (* Types for function and vairable names. For simplicity we will index them by integers *)
 Definition varname := Z.
@@ -25,51 +21,52 @@ Definition funcname := string.
 Definition fconst := string.
 
 Inductive rvalue :=
-| FunCallValue (f:funcall)
+| FunCallValue (f:hbuiltin)
 | VarRValue (var:varname)
 | RConst (value:fconst)
 | IConst (value:Z)
 | IConstArr (values: list Z)
 | RConstArr (values: list fconst)
 | NthRvalue (r:rvalue) (index:rvalue)
-| RCast (type:itype) (r:rvalue)
-with funcall :=
+| RCast (type:htype) (r:rvalue)
+(* builtin functions *)
+with hbuiltin :=
      (* polymorphic codnition operator *)
-     | F_cond: rvalue -> rvalue -> rvalue -> funcall
+     | F_cond: rvalue -> rvalue -> rvalue -> hbuiltin
 
      (* polymorphic artihmetic n-nary (n >= 2) operators *)
-     | F_min: rvalue -> rvalue -> list rvalue -> funcall
-     | F_max : rvalue -> rvalue -> list rvalue -> funcall
+     | F_min: rvalue -> rvalue -> list rvalue -> hbuiltin
+     | F_max: rvalue -> rvalue -> list rvalue -> hbuiltin
 
      (* polymorphic artihmetic binary operators *)
-     | F_add : rvalue -> rvalue -> funcall
-     | F_sub : rvalue -> rvalue -> funcall
-     | F_mul : rvalue -> rvalue -> funcall
-     | F_div : rvalue -> rvalue -> funcall
+     | F_add: rvalue -> rvalue -> hbuiltin
+     | F_sub: rvalue -> rvalue -> hbuiltin
+     | F_mul: rvalue -> rvalue -> hbuiltin
+     | F_div: rvalue -> rvalue -> hbuiltin
 
      (* polymorphic binary operators *)
-     | F_geq : rvalue -> rvalue -> funcall
+     | F_geq: rvalue -> rvalue -> hbuiltin
 
      (* polimorphic unary operators *)
-     | F_neg: rvalue -> funcall
-     | F_abs: rvalue -> funcall.
+     | F_neg: rvalue -> hbuiltin
+     | F_abs: rvalue -> hbuiltin.
 
 Inductive lvalue :=
 | VarLValue (var:varname)
 | NthLvalue (l:lvalue) (index:rvalue)
-| LCast (type:itype) (l:lvalue).
+| LCast (type:htype) (l:lvalue).
 
-Inductive istmt :=
-| IFunction (name: funcname) (type:itype) (param: list varname) (body:istmt)
+Inductive hstmt :=
+| FunctionDef (name: funcname) (type:htype) (param: list varname) (body:hstmt)
 | Skip
-| Chain (body: list istmt)
-| Data (var: varname) (values: list rvalue) (body:istmt)
+| Chain (body: list hstmt)
+| Decl (vars: list varname) (body:hstmt)
+| Data (var: varname) (values: list rvalue) (body:hstmt)
 | Assign (l:lvalue) (r:rvalue)
-| Loop (var: varname) (from:Z) (to:Z) (body:istmt)
-| If (cond:rvalue) (thenbranch:istmt) (elsebranch:istmt)
-| FunCallStmt (f:funcall)
+| Loop (var: varname) (from:Z) (to:Z) (body:hstmt)
+| If (cond:rvalue) (thenbranch:hstmt) (elsebranch:hstmt)
+| FunCallStmt (f:hbuiltin) (* call for side-effect *)
 | Return (r:rvalue).
 
-Inductive iprogram :=
-| Program (bindings: varname -> option itype) (body:istmt). (* TODO: map? *)
-
+Inductive hprogram :=
+| Program (bindings: varname -> option htype) (body:hstmt).
