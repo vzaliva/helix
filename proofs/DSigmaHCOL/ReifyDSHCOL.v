e447a30be22cda210b0d20abdc135666df564a06
Vadim Zaliva <lord@crocodile.org>
2018-07-23 20:21:14 +0200

    matching SHOperator names (as strings)

--- proofs/DSigmaHCOL/ReifyDSHCOL.v
+++ proofs/DSigmaHCOL/ReifyDSHCOL.v
@@ -23,13 +23,44 @@ Definition varbinding:Type := ident*DSHCOLType.
 Definition varbindings:Type := list varbinding.
 
 (* placholder *)
-Definition toDSHCOLType (t:term): option DSHCOLType := None.
+Definition toDSHCOLType (t:term): option DSHCOLType := Some DSHCOLTypeNat.
 
-(* placholder. tmInferInstance? *)
-Definition isSHOperatorInstance (t:term): bool:= false.
+Require Import Coq.MSets.MSets.
+Require Import Coq.MSets.MSetWeakList.
+Require Import Coq.MSets.MSetInterface.
+
+Module StringDec.
+  Definition t : Set := string.
+  Definition eq : t -> t -> Prop := eq.
+  Definition eq_equiv : RelationClasses.Equivalence eq := _.
+  Definition eq_dec : forall x y : t, {eq x y} + {~ eq x y} := string_dec.
+End StringDec.
+
+Module StringSet := MSets.MSetWeakList.Make StringDec.
+
+Definition operator_names :=
+  fold_right StringSet.add StringSet.empty
+                  ["Helix.SigmaHCOL.SigmaHCOL.eUnion" ;
+                     "Helix.SigmaHCOL.SigmaHCOL.eT" ;
+                     "Helix.SigmaHCOL.SigmaHCOL.SHPointwise" ;
+                     "Helix.SigmaHCOL.SigmaHCOL.SHBinOp" ;
+                     "Helix.SigmaHCOL.SigmaHCOL.SHInductor" ;
+                     "Helix.SigmaHCOL.SigmaHCOL.IUnion" ;
+                     "Helix.SigmaHCOL.SigmaHCOL.ISumUnion" ;
+                     "Helix.SigmaHCOL.SigmaHCOL.IReduction" ;
+                     "Helix.SigmaHCOL.SigmaHCOL.SHCompose" ;
+                     "Helix.SigmaHCOL.TSigmaHCOL.SafeCast" ;
+                     "Helix.SigmaHCOL.TSigmaHCOL.UnSafeCast" ;
+                     "Helix.SigmaHCOL.TSigmaHCOL.HTSUMUnion"].
+
+Definition isSHOperator (t:term): bool:=
+  match t with
+  | (tApp (tConst n _) _) => StringSet.mem n operator_names
+  | _ => false
+  end.
 
 Fixpoint stripGlobalVars (vars:varbindings) (t:term): option (varbindings*term) :=
-  if isSHOperatorInstance t then Some (vars,t)
+  if isSHOperator t then Some (vars,t)
   else
     match t with
     | tLambda (nNamed n) vt b =>
@@ -39,6 +70,8 @@ Fixpoint stripGlobalVars (vars:varbindings) (t:term): option (varbindings*term) 
       end
     | _ => None
     end.
+
+Compute (stripGlobalVars [] dast).
 
 Definition reifySHCOL {i o: nat}
            {fm: Monoid.Monoid RthetaFlags}
