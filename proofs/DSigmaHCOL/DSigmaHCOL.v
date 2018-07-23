77e265c544ae4617e4604592c8965c78e05a3c09
Vadim Zaliva <lord@crocodile.org>
2018-07-23 14:19:45 +0200

    `DSHInductor` added. Nat functions and expressions placeholders

--- proofs/DSigmaHCOL/DSigmaHCOL.v
+++ proofs/DSigmaHCOL/DSigmaHCOL.v
@@ -3,7 +3,6 @@
 Require Import Coq.Lists.List.
 
 Require Import Helix.Util.Misc.
-Require Import Helix.Util.FinNat.
 Require Import Helix.SigmaHCOL.Rtheta.
 Require Import Helix.SigmaHCOL.SVector.
 
@@ -13,40 +12,46 @@ Import Monoid.
 
 Global Open Scope nat_scope.
 
-Inductive DSHBinaryFunction :=
+Inductive DSHBinCarrierA :=
 | DSHBinPlus
 | DSHBinMinus
 | DSHBinMult
 | DSHBinMin
 | DSHBinMax.
 
-Definition DSHNatFunction: Type := list nat.
+(* Placeholder *)
+Definition DSHNatExpr: Type := unit.
 
-Record DSHIBinCarrierA (n:nat): Type
-  := mkDSHIBinCarrierA {
-         dsf_binf: DSHBinaryFunction ;
-         dsf_indf: DSHNatFunction ;
-         dsf_param: avector n
-       }.
+(* Placeholder *)
+Definition DSHNatFunction: Type := unit.
+
+(* Placeholder *)
+Inductive DSHIBinCarrierA: Type :=
+| DSHIBinCarrierA_ign (f: DSHBinCarrierA): DSHIBinCarrierA
+| DSHIBinCarrierA_vec {n:nat}
+                      (f: DSHBinCarrierA)
+                      (nf: DSHNatFunction)
+                      (dsf_param: avector n).
 
 Inductive DSHOperator: Monoid RthetaFlags -> nat -> nat -> Type :=
-| DSHeUnion {o: nat} {b: FinNat o} {fm} (z: CarrierA): DSHOperator fm 1 o
-| DSHeT {i: nat} {b:FinNat i} {fm}: DSHOperator fm i 1
-| DSHPointwise {pn n: nat} {fm} (f: DSHIBinCarrierA pn): DSHOperator fm n n
-| DSHBinOp {pn} {o} {fm} (f: DSHIBinCarrierA pn): DSHOperator fm (o+o) o
-| DSHIUnion {i o n: nat} (dot: DSHBinaryFunction) (initial: CarrierA):
+| DSHeUnion {fm}  {o: nat} {b: DSHNatExpr} (z: CarrierA): DSHOperator fm 1 o
+| DSHeT {fm} {i: nat} {b:DSHNatExpr}: DSHOperator fm i 1
+| DSHPointwise {fm} {i: nat} (f: DSHIBinCarrierA): DSHOperator fm i i
+| DSHBinOp {o} {fm} (f: DSHIBinCarrierA): DSHOperator fm (o+o) o
+| DSHInductor {fm} (n:DSHNatExpr) (f: DSHBinCarrierA) (initial: CarrierA): DSHOperator fm 1 1
+| DSHIUnion {i o: nat} (n:nat) (dot: DSHBinCarrierA) (initial: CarrierA):
     DSHOperator Monoid_RthetaFlags i o -> DSHOperator Monoid_RthetaFlags i o
-| DSHISumUnion {i o :nat} (n: nat):
+| DSHISumUnion {i o:nat} (n: nat):
     DSHOperator Monoid_RthetaFlags i o -> DSHOperator Monoid_RthetaFlags i o
-| DSHIReduction {i o n: nat} (dot: DSHBinaryFunction) (initial: CarrierA):
+| DSHIReduction {i o: nat} (n: nat) (dot: DSHBinCarrierA) (initial: CarrierA):
     DSHOperator Monoid_RthetaSafeFlags i o -> DSHOperator Monoid_RthetaSafeFlags i o
-| DSHCompose {i1 o2 o3: nat} {fm}:
+| DSHCompose {fm} {i1 o2 o3: nat}:
     DSHOperator fm o2 o3 -> DSHOperator fm i1 o2 -> DSHOperator fm i1 o3
 | DSHSafeCast {i o:nat}:
     DSHOperator Monoid_RthetaSafeFlags i o -> DSHOperator Monoid_RthetaFlags i o
 | DSHUnSafeCast {i o:nat}:
     DSHOperator Monoid_RthetaFlags i o -> DSHOperator Monoid_RthetaSafeFlags i o
-| DSHHTSUMUnion {i o:nat} {fm} (dot: DSHBinaryFunction):
+| DSHHTSUMUnion {fm} {i o:nat} (dot: DSHBinCarrierA):
     DSHOperator fm i o -> DSHOperator fm i o -> @DSHOperator fm i o.
 
 
