96f27fe1306dd74c7ebfa05213b67c8e2ac6bab4
Vadim Zaliva <lord@crocodile.org>
2018-11-01 17:57:39 -0700

    implicit `FloatT` in FSH

--- proofs/FSigmaHCOL/FSigmaHCOL.v
+++ proofs/FSigmaHCOL/FSigmaHCOL.v
@@ -15,51 +15,51 @@ Inductive FloatV : FloatT -> Type :=
 | Float32V (b32:binary32) : FloatV Float32
 | Float64V (b64:binary64) : FloatV Float64.
 
-Inductive FSHVal (ft:FloatT): Type :=
-| FSHnatVal (n:nat): FSHVal ft
-| FSHFloatVal (a:FloatV ft): FSHVal ft
-| FSHvecVal {n:nat} (v:vector (FloatV ft) n): FSHVal ft.
+Inductive FSHVal {ft:FloatT}: Type :=
+| FSHnatVal (n:nat): FSHVal 
+| FSHFloatVal (a:FloatV ft): FSHVal 
+| FSHvecVal {n:nat} (v:vector (FloatV ft) n): FSHVal.
 
-Inductive FExpr (ft:FloatT) : Type :=
-| AVar  : nat -> FExpr ft
-| AConst: FloatV ft -> FExpr ft
-| ANth  : forall n, VExpr ft n -> NExpr ft -> FExpr ft
-| AAbs  : FExpr ft -> FExpr ft
-| APlus : FExpr ft -> FExpr ft -> FExpr ft
-| AMinus: FExpr ft -> FExpr ft -> FExpr ft
-| AMult : FExpr ft -> FExpr ft -> FExpr ft
-| AMin  : FExpr ft -> FExpr ft -> FExpr ft
-| AMax  : FExpr ft -> FExpr ft -> FExpr ft
-| AZless: FExpr ft -> FExpr ft -> FExpr ft
+Inductive FExpr {ft:FloatT} : Type :=
+| AVar  : nat -> FExpr
+| AConst: FloatV ft -> FExpr
+| ANth  : forall n, VExpr n -> NExpr -> FExpr
+| AAbs  : FExpr -> FExpr
+| APlus : FExpr -> FExpr  -> FExpr
+| AMinus: FExpr -> FExpr  -> FExpr
+| AMult : FExpr -> FExpr  -> FExpr
+| AMin  : FExpr -> FExpr  -> FExpr
+| AMax  : FExpr -> FExpr  -> FExpr
+| AZless: FExpr -> FExpr  -> FExpr
 with
 (* Expressions which evaluate to `nat` *)
-NExpr (ft:FloatT): Type :=
-| NVar  : nat -> NExpr ft
-| NConst: nat -> NExpr ft
-| NDiv  : NExpr ft -> NExpr ft -> NExpr ft
-| NMod  : NExpr ft -> NExpr ft -> NExpr ft
-| NPlus : NExpr ft -> NExpr ft -> NExpr ft
-| NMinus: NExpr ft -> NExpr ft -> NExpr ft
-| NMult : NExpr ft -> NExpr ft -> NExpr ft
-| NMin  : NExpr ft -> NExpr ft -> NExpr ft
-| NMax  : NExpr ft -> NExpr ft -> NExpr ft
+NExpr {ft:FloatT}: Type :=
+| NVar  : nat -> NExpr
+| NConst: nat -> NExpr
+| NDiv  : NExpr -> NExpr -> NExpr
+| NMod  : NExpr -> NExpr -> NExpr
+| NPlus : NExpr -> NExpr -> NExpr
+| NMinus: NExpr -> NExpr -> NExpr
+| NMult : NExpr -> NExpr -> NExpr
+| NMin  : NExpr -> NExpr -> NExpr
+| NMax  : NExpr -> NExpr -> NExpr
 (* Expressions which evaluate to vector of floats *)
-with VExpr (ft:FloatT): nat -> Type :=
-     | VVar  {n:nat}: nat -> VExpr ft n
-     | VConst {n:nat}: vector (FloatV ft) n -> VExpr ft n.
+with VExpr {ft:FloatT}: nat -> Type :=
+     | VVar  {n:nat}: nat -> VExpr n
+     | VConst {n:nat}: vector (FloatV ft) n -> VExpr n.
 
-Definition FSHUnFloat := FExpr.
-Definition FSHIUnFloat := FExpr.
-Definition FSHBinFloat := FExpr.
-Definition FSHIBinFloat := FExpr.
+Definition FSHUnFloat   {ft:FloatT} := @FExpr ft.
+Definition FSHIUnFloat  {ft:FloatT} := @FExpr ft.
+Definition FSHBinFloat  {ft:FloatT} := @FExpr ft.
+Definition FSHIBinFloat {ft:FloatT} := @FExpr ft.
 
-Inductive FSHOperator (ft:FloatT): nat -> nat -> Type :=
-| FSHeUnion {o: nat} (b: NExpr ft) (z: (FloatV ft)): FSHOperator ft 1 o
-| FSHeT {i: nat} (b:NExpr ft): FSHOperator ft i 1
-| FSHPointwise {i: nat} (f: FSHIUnFloat ft): FSHOperator ft i i
-| FSHBinOp {o} (f: FSHIBinFloat ft): FSHOperator ft (o+o) o
-| FSHInductor (n:NExpr ft) (f: FSHBinFloat ft) (initial: FloatV ft): FSHOperator ft 1 1
-| FSHIUnion {i o: nat} (n:nat) (dot: FSHBinFloat ft) (initial: FloatV ft): FSHOperator ft i o -> FSHOperator ft i o
-| FSHIReduction {i o: nat} (n: nat) (dot: FSHBinFloat ft) (initial: FloatV ft): FSHOperator ft i o -> FSHOperator ft i o
-| FSHCompose {i1 o2 o3: nat}: FSHOperator ft o2 o3 -> FSHOperator ft i1 o2 -> FSHOperator ft i1 o3
-| FSHHTSUMUnion {i o:nat} (dot: FSHBinFloat ft): FSHOperator ft i o -> FSHOperator ft i o -> @FSHOperator ft i o.
+Inductive FSHOperator {ft:FloatT}: nat -> nat -> Type :=
+| FSHeUnion {o: nat} (b: @NExpr ft) (z: (FloatV ft)): FSHOperator 1 o
+| FSHeT {i: nat} (b:@NExpr ft): FSHOperator i 1
+| FSHPointwise {i: nat} (f: @FSHIUnFloat ft): FSHOperator i i
+| FSHBinOp {o} (f: @FSHIBinFloat ft): FSHOperator (o+o) o
+| FSHInductor (n:@NExpr ft) (f: @FSHBinFloat ft) (initial: FloatV ft): FSHOperator 1 1
+| FSHIUnion {i o: nat} (n:nat) (dot: @FSHBinFloat ft) (initial: FloatV ft): FSHOperator i o -> FSHOperator i o
+| FSHIReduction {i o: nat} (n: nat) (dot: @FSHBinFloat ft) (initial: FloatV ft): FSHOperator i o -> FSHOperator i o
+| FSHCompose {i1 o2 o3: nat}: FSHOperator o2 o3 -> FSHOperator i1 o2 -> FSHOperator i1 o3
+| FSHHTSUMUnion {i o:nat} (dot: @FSHBinFloat ft): FSHOperator i o -> FSHOperator i o -> @FSHOperator ft i o.
