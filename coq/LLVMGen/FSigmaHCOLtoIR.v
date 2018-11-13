64573e8fbe143adc927ca1d6929283c4cc5aa319
Vadim Zaliva <lord@crocodile.org>
2018-11-12 19:48:24 -0800

    Revert "state monad"
    This reverts commit 5789a85d6c09a0c9d37b5df8870cc5ad0b7c8329.

--- coq/LLVMGen/FSigmaHCOLtoIR.v
+++ coq/LLVMGen/FSigmaHCOLtoIR.v
@@ -9,9 +9,6 @@ Require Import Flocq.IEEE754.Binary.
 Require Import Coq.Numbers.BinNums. (* for Z scope *)
 Require Import Coq.ZArith.BinInt.
 
-Require Import ExtLib.Data.Monads.StateMonad.
-Require Import ExtLib.Structures.Monads.
-
 Program Definition FloatV64Zero := Float64V (@FF2B _ _ (F754_zero false) _).
 
 Program Definition FloatV64One := Float64V (BofZ _ _ _ _ 1%Z).
@@ -84,192 +81,154 @@ Definition genIRGlobals
             |}
        ).
 
-Section WithState.
+Record IRState :=
+  mkIRstate
+    {
+      block_count: nat ;
+      local_count: nat ;
+      void_count : nat ;
+      vars: list (raw_id * typ)
+    }.
 
-  Import MonadNotation.
-  Local Open Scope monad_scope.
+Definition newState: IRState :=
+  {|
+    block_count := 0 ;
+    local_count := 0 ;
+    void_count  := 0 ;
+    vars := []
+  |}.
 
-  Record IRState : Type :=
-    mkIRstate
-      {
-        block_count: nat ;
-        local_count: nat ;
-        void_count : nat ;
-        vars: list (raw_id * typ)
-      }.
+(* Returns block ID and a new state where it is incremented *)
+Definition incBlock (st:IRState): (IRState*block_id) :=
+  ({|
+      block_count := S (block_count st);
+      local_count := local_count st ;
+      void_count := void_count st ;
+      vars := vars st
+    |}, Anon (Z.of_nat (block_count st))).
 
-  Definition newState: IRState :=
-    {|
-      block_count := 0 ;
-      local_count := 0 ;
-      void_count  := 0 ;
-      vars := []
-    |}.
-
-  Variable state_m : Type -> Type.
-  Context {Monad_m: Monad state_m}.
-  Context {State_m: MonadState IRState state_m}.
-
-  Definition incBlock (st:IRState): state_m unit :=
-    put {|
-        block_count := S (block_count st);
-        local_count := local_count st ;
-        void_count := void_count st ;
-        vars := vars st
-      |}.
-
-  Definition getNextBlock (st:IRState): block_id :=
-    Anon (Z.of_nat (block_count st)).
-
-  Definition incLocal (st:IRState): state_m unit :=
-    put {|
+(* Returns local ID and a new state where it is incremented *)
+Definition incLocal (st:IRState): (IRState*raw_id) :=
+  ({|
       block_count := block_count st ;
       local_count := S (local_count st) ;
       void_count  := void_count st ;
       vars := vars st
-    |}.
+    |}, Anon (Z.of_nat (local_count st))).
 
-  Definition getNextLocal (st:IRState): local_id :=
-    Anon (Z.of_nat (local_count st)).
+(* Returns void ID and a new state where it is incremented *)
+Definition incVoid (st:IRState): (IRState*int) :=
+  ({|
+      block_count := block_count st ;
+      local_count := local_count st ;
+      void_count  := S (void_count st) ;
+      vars := vars st
+    |}, Z.of_nat (void_count st)).
 
-  Definition incVoid (st:IRState): state_m unit:=
-    put {|
-        block_count := block_count st ;
-        local_count := local_count st ;
-        void_count  := S (void_count st) ;
-        vars := vars st
-      |}.
+Definition addVars (st:IRState) (newvars: list (raw_id * typ)): IRState :=
+  {|
+    block_count := block_count st ;
+    local_count := local_count st ;
+    void_count  := void_count st ;
+    vars := vars st ++ newvars
+  |}.
 
-  Definition getNextVoid (st:IRState): Z := Z.of_nat (void_count st).
-
-  Definition addVars (st:IRState) (newvars: list (raw_id * typ)): state_m unit :=
-    put {|
-        block_count := block_count st ;
-        local_count := local_count st ;
-        void_count  := void_count st ;
-        vars := vars st ++ newvars
-      |}.
-
-  Definition allocTempArray
-             {ft: FloatT}
-             (name: local_id)
-             (nextblock: block_id)
-             (size: nat): state_m (local_id * block)
-    :=
-      st <- get ;;
-         let retid := getNextLocal st in
-         incLocal st ;;
-                  let bid := getNextBlock st in
-                  ret (name,
-                       {|
-                         blk_id    := bid ;
-                         blk_phis  := [];
-                         blk_code  := [(IId name,
-                                        INSTR_Alloca (getIRType (@FSHvecValType ft size)) None (Some 16%Z))]; (* TODO: default align to config *)
-                         blk_term  := (IId retid, TERM_Br_1 nextblock) (* TODO: IVoid? *)
-                       |}).
-
-  Fixpoint genIR
-           {i o: nat}
+Definition allocTempArray
            {ft: FloatT}
-           (x y: local_id)
+           (st: IRState)
+           (name: local_id)
            (nextblock: block_id)
-           (fshcol: @FSHOperator ft i o) {struct fshcol}
-    : state_m (block_id * list block)
-    := match fshcol with
-       | FSHeUnion o b z => ret (nextblock, [])
-       | FSHeT i b => ret (nextblock, [])
-       | FSHPointwise i f => ret (nextblock, [])
-       | FSHBinOp o f => ret (nextblock, [])
-       | FSHInductor n f initial => ret (nextblock, [])
-       | FSHIUnion i o n dot initial x => ret (nextblock, [])
-       | FSHIReduction i o n dot initial x => ret (nextblock, [])
-       | FSHCompose i1 o2 o3 f g =>
-         st <- get ;;
-            let tmpid := getNextLocal st in
-            incLocal st ;;
-                     fm <- genIR tmpid y nextblock f ;;
-                     let '(fb, f') := fm in
-                     gm <- genIR x tmpid fb g ;;
-                        let '(gb, g') := gm in
-                        am <- @allocTempArray ft tmpid fb o2 ;;
-                           let '(alloid, tmpalloc) := am in
-                           ret (alloid, [tmpalloc]++g'++f')
-       | FSHHTSUMUnion i o dot f g => ret (nextblock, [])
-       end.
+           (size: nat): (IRState * local_id * block)
+  :=
+    let (st,retid) := incLocal st in
+    let (st,bid) := incBlock st in
+    (st, name,
+     {|
+       blk_id    := bid ;
+       blk_phis  := [];
+       blk_code  := [(IId name,
+                      INSTR_Alloca (getIRType (@FSHvecValType ft size)) None (Some 16%Z))]; (* TODO: default align to config *)
+       blk_term  := (IId retid, TERM_Br_1 nextblock) (* TODO: IVoid? *)
+     |}).
 
-  Definition LLVMGen_m
-             {i o: nat}
-             {ft: FloatT}
-             (globals: list (string* (@FSHValType ft)))
-             (fshcol: @FSHOperator ft i o) (funname: string)
-    : state_m (toplevel_entities (list block))
-    :=
-      let x := Name "X" in
-      let xtyp := TYPE_Pointer (getIRType (@FSHvecValType ft i)) in
-      let y := Name "Y" in
-      let ytyp := TYPE_Pointer (getIRType (@FSHvecValType ft o)) in
-
-      st <- get ;; addVars st (List.map
-                                (fun g:(string* (@FSHValType ft)) =>
-                                   let (n,t) := g in (Name n, getIRType t))
-                                globals) ;;
-         st <- get ;; addVars st [(x, xtyp)] ;;
-         st <- get ;;
-         let rid := getNextBlock st in
-         incBlock st ;;
-                  st <- get ;;
-                  let rsid := getNextBlock st in
-                  let retblock :=
-                      {|
-                        blk_id    := rid ;
-                        blk_phis  := [];
-                        blk_code  := [];
-                        blk_term  := (IId rsid, TERM_Ret_void)
-                      |} in
-                  incBlock st ;;
-                           mbody <- genIR x y rid fshcol ;;
-                           let '(_,body) := mbody in
-                           ret
-                             (genIRGlobals globals ++
-                                           [TLE_Definition
-                                              {|
-                                                df_prototype   :=
-                                                  {|
-                                                    dc_name        := Name funname;
-                                                    dc_type        := TYPE_Function TYPE_Void [xtyp;ytyp ] ;
-                                                    dc_param_attrs := ([],[[PARAMATTR_Align 16%Z] ; (* TODO: align to config *)
-                                                                             [PARAMATTR_Align 16%Z]]);
-                                                    dc_linkage     := None;
-                                                    dc_visibility  := None;
-                                                    dc_dll_storage := None;
-                                                    dc_cconv       := None;
-                                                    dc_attrs       := [];
-                                                    dc_section     := None;
-                                                    dc_align       := None;
-                                                    dc_gc          := None;
-                                                  |} ;
-                                                df_args        := [x;y];
-                                                df_instrs      := body ++ [retblock]
-                                              |}
-                                           ]
-                             ).
-
-End WithState.
+Fixpoint genIR
+         {i o: nat}
+         {ft: FloatT}
+         (st: IRState)
+         (x y: local_id)
+         (nextblock: block_id)
+         (fshcol: @FSHOperator ft i o): (IRState * block_id * list block)
+  := match fshcol with
+     | FSHeUnion o b z => (st, nextblock, [])
+     | FSHeT i b => (st, nextblock, [])
+     | FSHPointwise i f => (st, nextblock, [])
+     | FSHBinOp o f => (st, nextblock, [])
+     | FSHInductor n f initial => (st, nextblock, [])
+     | FSHIUnion i o n dot initial x => (st, nextblock, [])
+     | FSHIReduction i o n dot initial x => (st, nextblock, [])
+     | FSHCompose i1 o2 o3 f g =>
+       let '(st, tmpid) := incLocal st in
+       let '(st, fb, f') := genIR st tmpid y nextblock f in
+       let '(st, gb, g') := genIR st x tmpid fb g in
+       let '(st, alloid, tmpalloc) := @allocTempArray ft st tmpid fb o2 in
+       (st, alloid, [tmpalloc]++g'++f')
+     | FSHHTSUMUnion i o dot f g => (st, nextblock, [])
+     end.
 
 Definition LLVMGen
            {i o: nat}
            {ft: FloatT}
            (globals: list (string* (@FSHValType ft)))
            (fshcol: @FSHOperator ft i o) (funname: string)
-  : toplevel_entities (list block) :=
-  @evalState IRState (toplevel_entities (list block))
-             (
-               @LLVMGen_m
-                 _
-                 _
-                 (MonadState_state IRState)
-                 i o ft
-                 globals fshcol funname
-             )
-             newState.
+  :option (toplevel_entities (list block))
+  :=
+    let x := Name "X" in
+    let xtyp := TYPE_Pointer (getIRType (@FSHvecValType ft i)) in
+    let y := Name "Y" in
+    let ytyp := TYPE_Pointer (getIRType (@FSHvecValType ft o)) in
+    let st := newState in
+
+    let st :=
+        addVars st
+                (List.map
+                   (fun g:(string* (@FSHValType ft)) =>
+                      let (n,t) := g in (Name n, getIRType t))
+                   globals) in
+
+    let st := addVars st [(x, xtyp)] in
+
+    let (st,rid) := incBlock st in
+    let (st,rsid) := incBlock st in
+    let retblock :=
+        {|
+          blk_id    := rid ;
+          blk_phis  := [];
+          blk_code  := [];
+          blk_term  := (IId rsid, TERM_Ret_void)
+        |} in
+    let (st,body) := genIR st x y rid fshcol in
+    let body := body ++ [retblock] in
+    Some
+      (genIRGlobals globals ++
+                    [TLE_Definition
+                       {|
+                         df_prototype   :=
+                           {|
+                             dc_name        := Name funname;
+                             dc_type        := TYPE_Function TYPE_Void [xtyp;ytyp ] ;
+                             dc_param_attrs := ([],[[PARAMATTR_Align 16%Z] ; (* TODO: align to config *)
+                                                      [PARAMATTR_Align 16%Z]]);
+                             dc_linkage     := None;
+                             dc_visibility  := None;
+                             dc_dll_storage := None;
+                             dc_cconv       := None;
+                             dc_attrs       := [];
+                             dc_section     := None;
+                             dc_align       := None;
+                             dc_gc          := None;
+                           |} ;
+                         df_args        := [x;y];
+                         df_instrs      := body
+                       |}
+                    ]
+      ).
