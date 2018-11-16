1e8da3e1c0a1a4657e836d36b70fcd2794b2afca
Vadim Zaliva <lord@crocodile.org>
2018-11-16 11:28:52 -0800

    printing names of tests

--- ml/test.ml
+++ ml/test.ml
@@ -1,5 +1,4 @@
 open Arg
-open ExtrOcamlIntConv
 open FSigmaHCOLtoIR
 open FSigmaHCOL
 open Camlcoq
@@ -7,18 +6,8 @@ open Core
 open Tests
 
 let verbose = ref false
+let printtests = ref false
 let output_file_prefix = "test_"
-
-(* OCaml wrapper over extracted Coq code *)
-let ocaml_LLVMGen
-      (i:int) (o:int)
-      (globalnames: (string*coq_FSHValType) list)
-      fshcol (funname: string)
-  =
-  coq_LLVMGen
-    (nat_of_int i) (nat_of_int o) Float64
-    (List.map globalnames ~f:(fun (x,y) -> (coqstring_of_camlstring x, y)))
-    fshcol (coqstring_of_camlstring funname)
 
 let output_ll_file filename ast =
   let open Format in
@@ -42,14 +31,20 @@ let process_test { i=i ; o=o; name=name; op=op; globals=globals} =
 
 (* Use the --test option to run unit tests and the quit the program. *)
 let args =
-  [ ("-v", Set verbose, "enables more verbose compilation output")]
+  [
+    ("-v", Set verbose, "enables more verbose compilation output");
+    ("-p", Set printtests, "print names of all tests (for automation)");
+  ]
 
 let _ =
   Arg.parse args (fun _ -> ())  "USAGE: ./test [-v]\n";
-  if List.fold
-       (List.map all_tests ~f:process_test)
-       ~init:true ~f:(&&)
+  if !printtests
   then
-    exit 0
+    begin
+      ignore (List.map all_tests
+                ~f:(fun t -> Printf.printf "%s\n" (camlstring_of_coqstring (name t))));
+      exit 0
+    end
   else
-    exit 1
+    exit (if List.fold (List.map all_tests ~f:process_test) ~init:true ~f:(&&)
+          then 0 else 1)
