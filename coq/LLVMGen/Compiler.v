9648e0a9e242a7ecf47dcd30dd071c2869fe48af
Vadim Zaliva <lord@crocodile.org>
2018-12-19 17:02:27 -0800

    re-implemented inductor

--- coq/LLVMGen/Compiler.v
+++ coq/LLVMGen/Compiler.v
@@ -918,55 +918,33 @@ Section monadic.
              (st: IRState)
              (nextblock: block_id): m (IRState * segment)
     :=
-      let '(st, entry_block_id) := incBlockNamed st "Inductor" in
-      let '(st, zero_block_id) := incBlockNamed st "Inductor_zero" in
-      let '(st, nz_block_id) := incBlockNamed st "Inductor_non_zero" in
-      let '(st, cmp) := incLocal st in
-      let '(st, void0) := incVoid st in
+      let '(st, loopcontblock) := incBlockNamed st "Inductor_lcont" in
+      let '(st, loopvar) := incLocalNamed st "Inductor_i" in
       let xytyp := getIRType (@FSHvecValType ft 1) in
       let xyptyp := TYPE_Pointer xytyp in
       let '(st, py) := incLocal st in
+      let '(st, storeid0) := incVoid st in
+      let '(st, void1) := incVoid st in
       '(st, nexp, ncode) <- genNExpr st n ;;
-       let entry_block := {|
-             blk_id    := entry_block_id ;
-             blk_phis  := [];
-             blk_code  := ncode ++ [
-                                  (IId py,  INSTR_Op (OP_GetElementPtr
-                                                        xytyp (xyptyp, (EXP_Ident (ID_Local y)))
-                                                        [(IntType, EXP_Integer 0%Z);
-                                                           (IntType,EXP_Integer 0%Z)]
-
-                                  ));
-                                    (IId cmp, INSTR_Op (OP_ICmp Eq
-                                                                IntType
-                                                                nexp
-                                                                (EXP_Integer 0%Z)
-                                    ))
-                                ];
-             blk_term  := (IVoid void0,
-                           TERM_Br (TYPE_I 1%Z, EXP_Ident (ID_Local cmp)) zero_block_id nz_block_id);
-             blk_comments := None
-
-           |} in
-
-       let '(st, storeid0) := incVoid st in
-       let '(st, void1) := incVoid st in
        ini <- genFloatV initial ;;
-       let zero_block := {|
-             blk_id    := zero_block_id ;
-             blk_phis  := [];
-             blk_code  := [
+
+       let init_code := ncode ++ [
+                                (IId py,  INSTR_Op (OP_GetElementPtr
+                                                      xytyp (xyptyp, (EXP_Ident (ID_Local y)))
+                                                      [(IntType, EXP_Integer 0%Z);
+                                                         (IntType,EXP_Integer 0%Z)]
+
+                                ));
+
                                   (IVoid storeid0, INSTR_Store false
-                                                              ((FloatTtyp ft), ini)
-                                                              (TYPE_Pointer (FloatTtyp ft),
-                                                               (EXP_Ident (ID_Local py)))
-                                                              (ret 8%Z))
-                                ];
-             blk_term  := (IVoid void1, TERM_Br_1 nextblock);
-             blk_comments := None
-           |} in
+                                                               ((FloatTtyp ft), ini)
+                                                               (TYPE_Pointer (FloatTtyp ft),
+                                                                (EXP_Ident (ID_Local py)))
+                                                               (ret 8%Z))
 
+                              ] in
 
+       let '(st, body_block_id) := incBlockNamed st "InductorLoopBody" in
        let '(st, storeid1) := incVoid st in
        let '(st, void2) := incVoid st in
        let '(st, px) := incLocal st in
@@ -975,38 +953,37 @@ Section monadic.
        let st := addVars st [(ID_Local yv, (FloatTtyp ft)); (ID_Local xv, (FloatTtyp ft))] in
        '(st, fexpr, fexpcode) <- genFExpr st f ;;
         st <- dropVars st 2 ;;
-       let nz_block := {|
-             blk_id    := nz_block_id ;
-             blk_phis  := [];
-             blk_code  := [
-                           (IId px,  INSTR_Op (OP_GetElementPtr
-                                                 xytyp (xyptyp, (EXP_Ident (ID_Local x)))
-                                                 [(IntType, EXP_Integer 0%Z);
-                                                    (IntType,EXP_Integer 0%Z)]
+        let body_block := {|
+              blk_id    := body_block_id ;
+              blk_phis  := [];
+              blk_code  := [
+                            (IId px,  INSTR_Op (OP_GetElementPtr
+                                                  xytyp (xyptyp, (EXP_Ident (ID_Local x)))
+                                                  [(IntType, EXP_Integer 0%Z);
+                                                     (IntType,EXP_Integer 0%Z)]
 
-                           ));
-                             (IId yv, INSTR_Load false (FloatTtyp ft)
-                                                 (TYPE_Pointer (FloatTtyp ft),
-                                                  (EXP_Ident (ID_Local py)))
-                                                 (ret 8%Z));
-                             (IId xv, INSTR_Load false (FloatTtyp ft)
-                                                 (TYPE_Pointer (FloatTtyp ft),
-                                                  (EXP_Ident (ID_Local px)))
-                                                 (ret 8%Z))
-                         ]
-                            ++ fexpcode ++
-                            [
-                              (IVoid storeid1, INSTR_Store false
-                                                           ((FloatTtyp ft), fexpr)
-                                                           (TYPE_Pointer (FloatTtyp ft),
-                                                            (EXP_Ident (ID_Local py)))
-                                                           (ret 8%Z))
-                            ];
-             blk_term  := (IVoid void2, TERM_Br_1 nextblock);
-             blk_comments := None
-           |} in
-
-       ret (st, (entry_block_id, [entry_block ; zero_block; nz_block])).
+                            ));
+                              (IId yv, INSTR_Load false (FloatTtyp ft)
+                                                  (TYPE_Pointer (FloatTtyp ft),
+                                                   (EXP_Ident (ID_Local py)))
+                                                  (ret 8%Z));
+                              (IId xv, INSTR_Load false (FloatTtyp ft)
+                                                  (TYPE_Pointer (FloatTtyp ft),
+                                                   (EXP_Ident (ID_Local px)))
+                                                  (ret 8%Z))
+                          ]
+                             ++ fexpcode ++
+                             [
+                               (IVoid storeid1, INSTR_Store false
+                                                            ((FloatTtyp ft), fexpr)
+                                                            (TYPE_Pointer (FloatTtyp ft),
+                                                             (EXP_Ident (ID_Local py)))
+                                                            (ret 8%Z))
+                             ];
+              blk_term  := (IVoid void2, TERM_Br_1 loopcontblock);
+              blk_comments := None
+            |} in
+        genLoop "Inductor" (EXP_Integer 0%Z) nexp loopvar loopcontblock body_block_id [body_block] init_code st nextblock.
 
   Fixpoint genIR
            {i o: nat}
@@ -1046,7 +1023,10 @@ Section monadic.
           add_comment
           (genLoop "BinOp" (EXP_Integer 0%Z) (EXP_Integer (Z.of_nat n)) loopvar loopcontblock body_entry body_blocks [] st nextblock)
           "--- Operator: FSHBinOp ---"
-       | FSHInductor n f initial => genFSHInductor x y n f initial st nextblock
+       | FSHInductor n f initial =>
+          add_comment
+            (genFSHInductor x y n f initial st nextblock)
+            "--- Operator: FSHInductor ---"
        | FSHIUnion i o n _ _ child =>
          let '(st, loopcontblock) := incBlockNamed st "Union_lcont" in
          let '(st, loopvar) := incBlockNamed st "Union_i" in
