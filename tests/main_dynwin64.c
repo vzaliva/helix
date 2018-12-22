3da3c17e006164ac3661708db06d6e8153f1cd60
Vadim Zaliva <lord@crocodile.org>
2018-12-21 17:08:32 -0800

    better random generation (normal numbers only)

--- tests/main_dynwin64.c
+++ tests/main_dynwin64.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 double D[3];
 /* HELIX version */
@@ -32,7 +33,20 @@ int dwmonitor(const double *X, const double *D)
     return w2;
 }
 
-void main()
+
+double random_double()
+{
+    double res;
+    do
+    {
+        unsigned char *p = (unsigned char*)&res;
+        for(int j=0; j<8; j++)
+            *p++=rand();
+    } while(!isnormal(res));
+    return res;
+}
+
+void test_random()
 {
     double x[5];
     double y[1];
@@ -42,24 +56,28 @@ void main()
     for(int i=0;i<100;i++)
     {
         /* Random X */
-        unsigned char *p = (unsigned char*)x;
-        for(int j=0; j<5*8; j++)
-            *p++=rand();
+        for(int j=0; j<5; j++)
+            x[j]=random_double();
 
         /* Ramdom D */
-        p = (unsigned char*)D;
-        for(int j=0; j<3*8; j++)
-            *p++=rand();
+        for(int j=0; j<3; j++)
+            D[j]==random_double();
         
         int f = dwmonitor(x,D);
 
         y[0]=0;
         dynwin64(x, y);
 
-        if((double)f != y[i])
+        if((double)f != y[0])
+            printf("! Iteration %d, Y=%lg, Expected %d\n",i,y[0],f);
+        else
+            printf("  Iteration %d, Y=%lg, Expected %d\n",i,y[0],f);
+
+        /*
+          if((double)f != y[i])
         {
             printf("FAIL!\n");
-            printf("Iteration %d, Y=%lf, Expected %d\n",i,y[i],f);
+            printf("Iteration %d, Y=%lg, Expected %d\n",i,y[0],f);
             printf("X=\n");
             for(int j=0; j<5; j++)
                 printf("\t%d:\t%le\n",j,x[j]);
@@ -69,7 +87,42 @@ void main()
             
             exit(1);
         }
+         */
     }
     printf("PASS\n");
     exit(0);
+}
+
+/* test once on known values for debugging */
+void test_once()
+{
+    double x[5]={1., 2., 3., 4., 5.};
+    double y[1]={0.};
+    
+    D[0]=10.;
+    D[1]=20.;
+    D[2]=30.;
+    srand(time(NULL));
+    
+    int f = dwmonitor(x,D);
+    
+    y[0]=0;
+    dynwin64(x, y);
+    
+    if((double)f != y[0])
+    {
+        printf("FAIL!\n");
+        printf("Y=%lg, Expected %d\n",y[0],f);
+        exit(1);
+    }
+
+    printf("PASS\n");
+    exit(0);
+}
+
+
+void main()
+{
+    test_random();
+    //test_once();
 }
