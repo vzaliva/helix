050a0b76f98adfe74b12fce72b9c84dd4b150e22
Vadim Zaliva <lord@crocodile.org>
2019-02-01 15:10:02 -0800

    indentation

--- coq/SigmaHCOL/SigmaHCOLMem.v
+++ coq/SigmaHCOL/SigmaHCOLMem.v
@@ -66,87 +66,86 @@ Section WithFlags.
     : mem_block -> option mem_block
     := fun x => Some (svector_to_mem_block (op (mem_block_to_svector x))).
 
+End WithFlags.
 
-  End WithFlags.
+(* y[j] := x[i] *)
+Definition map_mem_block_elt (x:mem_block) (i:nat) (y:mem_block) (j:nat)
+  : option mem_block :=
+  match mem_lookup i x with
+  | None => None
+  | Some v => Some (mem_add j v y)
+  end.
 
-  (* y[j] := x[i] *)
-  Definition map_mem_block_elt (x:mem_block) (i:nat) (y:mem_block) (j:nat)
-    : option mem_block :=
-    match mem_lookup i x with
-    | None => None
-    | Some v => Some (mem_add j v y)
-    end.
+(* AKA: "embed" *)
+Definition eUnion_mem (b: nat) (x:mem_block): option mem_block :=
+  map_mem_block_elt x 0 (mem_empty) b.
 
-  (* AKA: "embed" *)
-  Definition eUnion_mem (b: nat) (x:mem_block): option mem_block :=
-    map_mem_block_elt x 0 (mem_empty) b.
+(* AKA "pick" *)
+Definition eT_mem (b: nat) (x:mem_block): option mem_block :=
+  map_mem_block_elt x b (mem_empty) 0.
 
-  (* AKA "pick" *)
-  Definition eT_mem (b: nat) (x:mem_block): option mem_block :=
-    map_mem_block_elt x b (mem_empty) 0.
-
-  Fixpoint Gather_mem
-           {i o: nat}
-           (f: index_map o i)
-           (x: mem_block) : option mem_block
-    :=
-      let i' := ⟦ f ⟧ o in
-      let map_one ys := map_mem_block_elt x i' ys o in
-      match o return (index_map o i) -> option mem_block with
-      | O => fun _ => map_one (mem_empty)
-      | S o' => fun f' =>
-                 match Gather_mem (shrink_index_map_domain f') x with
-                 | None => None
-                 | Some ys => map_one ys
-                 end
-      end f.
-
-  Fixpoint Scatter_mem_aux
-           {i o: nat}
-           (j: nat)
-           {f: index_map i o}
-           (fi: inverse_index_map f)
-           (x: mem_block) : option mem_block
-    :=
-      let o' := inverse_index_f f fi j in
-      let map_one ys := map_mem_block_elt x j ys o' in
-      match j with
-      | O => map_one (mem_empty)
-      | S j' => match Scatter_mem_aux j' fi x with
+Fixpoint Gather_mem
+         {i o: nat}
+         (f: index_map o i)
+         (x: mem_block) : option mem_block
+  :=
+    let i' := ⟦ f ⟧ o in
+    let map_one ys := map_mem_block_elt x i' ys o in
+    match o return (index_map o i) -> option mem_block with
+    | O => fun _ => map_one (mem_empty)
+    | S o' => fun f' =>
+               match Gather_mem (shrink_index_map_domain f') x with
                | None => None
                | Some ys => map_one ys
                end
-      end.
+    end f.
 
-  Definition Scatter_mem {i o: nat} (f: index_map i o)
-    :
-      mem_block -> option mem_block
-    :=
-      Scatter_mem_aux i (build_inverse_index_map f).
+Fixpoint Scatter_mem_aux
+         {i o: nat}
+         (j: nat)
+         {f: index_map i o}
+         (fi: inverse_index_map f)
+         (x: mem_block) : option mem_block
+  :=
+    let o' := inverse_index_f f fi j in
+    let map_one ys := map_mem_block_elt x j ys o' in
+    match j with
+    | O => map_one (mem_empty)
+    | S j' => match Scatter_mem_aux j' fi x with
+             | None => None
+             | Some ys => map_one ys
+             end
+    end.
 
-  Fixpoint IUnion_mem_aux
+Definition Scatter_mem {i o: nat} (f: index_map i o)
+  :
+    mem_block -> option mem_block
+  :=
+    Scatter_mem_aux i (build_inverse_index_map f).
+
+Fixpoint IUnion_mem_aux
+         {n: nat}
+         (j: nat) (jc: j<n)
+         (op_family_f: forall k (kc:k<n), mem_block -> option mem_block)
+         (x: mem_block) {struct j}: option mem_block :=
+  let oy := op_family_f j jc x in
+  match j return j<n -> option mem_block with
+  | O => fun _ => oy
+  | S j' => fun jc' =>
+             match oy, op_family_f j' (Nat.lt_succ_l j' n jc') x with
+             | Some y, Some ys => mem_merge y ys
+             | _, _ => None
+             end
+  end jc.
+
+Definition IUnion_mem
            {n: nat}
-           (j: nat) (jc: j<n)
            (op_family_f: forall k (kc:k<n), mem_block -> option mem_block)
-           (x: mem_block) {struct j}: option mem_block :=
-    let oy := op_family_f j jc x in
-    match j return j<n -> option mem_block with
-    | O => fun _ => oy
-    | S j' => fun jc' =>
-               match oy, op_family_f j' (Nat.lt_succ_l j' n jc') x with
-               | Some y, Some ys => mem_merge y ys
-               | _, _ => None
-               end
-    end jc.
-
-  Definition IUnion_mem
-             {n: nat}
-             (op_family_f: forall k (kc:k<n), mem_block -> option mem_block)
-             (x: mem_block): option mem_block
-    :=
-      match n as m return n=m -> option mem_block with
-      | 0 => fun _ => Some x
-      | S n' => fun E => @IUnion_mem_aux n 0
-                                     (eq_ind_r (lt 0) (zero_lt_Sn n') E)
-                                     op_family_f x
-      end eq_refl.
+           (x: mem_block): option mem_block
+  :=
+    match n as m return n=m -> option mem_block with
+    | 0 => fun _ => Some x
+    | S n' => fun E => @IUnion_mem_aux n 0
+                                   (eq_ind_r (lt 0) (zero_lt_Sn n') E)
+                                   op_family_f x
+    end eq_refl.
