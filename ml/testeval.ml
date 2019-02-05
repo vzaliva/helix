47ce521e699ed18dd02b8f7467edd29688265e5e
Vadim Zaliva <lord@crocodile.org>
2019-02-04 18:29:55 -0800

    reusing vellvm ML `Interpeter` module

--- ml/testeval.ml
+++ ml/testeval.ml
@@ -9,10 +9,8 @@ let printtests = ref false
 let single = ref ""
 let justcompile = ref false
 let output_file_prefix = "test_"
-let debug_flag = ref false
 
-module DV = Tests.IO.DV
-module A = ANSITerminal
+module AT = ANSITerminal
 
 let output_ll_file filename ast =
   let open Format in
@@ -22,70 +20,6 @@ let output_ll_file filename ast =
   pp_force_newline ppf ();
   pp_print_flush ppf () ;
   Out_channel.close channel
-
-(* TODO: probaly should be part of ADDRESS module interface*)
-let pp_addr : Format.formatter -> M.addr -> unit
-  = fun ppf _ -> fprintf ppf "DVALUE_Addr(?)"
-
-let rec pp_dvalue : Format.formatter -> DV.dvalue -> unit =
-  let pp_comma_space ppf () = pp_print_string ppf ", " in
-  fun ppf ->
-  function
-  | DVALUE_Addr   x -> pp_addr ppf x
-  | DVALUE_I1     x -> fprintf ppf "DVALUE_I1(%d)"  (Camlcoq.Z.to_int (DynamicValues.Int1.unsigned x))
-  | DVALUE_I8     x -> fprintf ppf "DVALUE_I8(%d)"  (Camlcoq.Z.to_int (DynamicValues.Int8.unsigned x))
-  | DVALUE_I32    x -> fprintf ppf "DVALUE_I32(%d)" (Camlcoq.Z.to_int (DynamicValues.Int32.unsigned x))
-  | DVALUE_I64    x -> fprintf ppf "DVALUE_I64(%s)" (Int64.to_string (Z.to_int64 (DynamicValues.Int64.unsigned x)))
-  | DVALUE_Double x -> fprintf ppf "DVALUE_Double(%F)" (camlfloat_of_coqfloat x)
-  | DVALUE_Float  x -> fprintf ppf "DVALUE_Float(%F)"  (camlfloat_of_coqfloat32 x)
-  | DVALUE_Undef    -> fprintf ppf "DVALUE_Undef"
-  | DVALUE_Poison   -> fprintf ppf "DVALUE_Poison"
-  | DVALUE_None     -> fprintf ppf "DVALUE_None"
-  | DVALUE_Struct        l -> fprintf ppf "DVALUE_Struct(%a)"        (pp_print_list ~pp_sep:pp_comma_space pp_dvalue) l
-  | DVALUE_Packed_struct l -> fprintf ppf "DVALUE_Packet_struct(%a)" (pp_print_list ~pp_sep:pp_comma_space pp_dvalue) l
-  | DVALUE_Array         l -> fprintf ppf "DVALUE_Array(%a)"         (pp_print_list ~pp_sep:pp_comma_space pp_dvalue) l
-  | DVALUE_Vector        l -> fprintf ppf "DVALUE_Vector(%a)"        (pp_print_list ~pp_sep:pp_comma_space pp_dvalue) l
-
-let debug (msg:string) =
-  if !debug_flag then
-    Printf.printf "DEBUG: %s\n%!" msg
-
-let rec step m =
-  let open Core0 in (* ITree.Core *)
-  match observe m with
-  (* Internal steps compute as nothing *)
-  | TauF x -> step x
-
-  (* We finished the computation *)
-  | RetF v -> Ok v
-
-  (* The failE effect is a failure *)
-  | VisF (OpenSum.Coq_inrE s, _) ->
-     Error (Camlcoq.camlstring_of_coqstring s)
-
-  (* The only visible effects from LLVMIO that should propagate to the interpreter are:
-     - Call to external functions
-     - Debug
-   *)
-  | VisF (OpenSum.Coq_inlE e, k) ->
-     begin match Obj.magic e with
-
-     | IO.Call(_, f, _) ->
-        (Printf.printf "UNINTERPRETED EXTERNAL CALL: %s - returning 0l to the caller\n"
-           (Camlcoq.camlstring_of_coqstring f));
-        step (k (Obj.magic (DV.DVALUE_I64 DynamicValues.Int64.zero)))
-
-     | IO.Debug(msg) ->
-        (debug (Camlcoq.camlstring_of_coqstring msg);
-         step (k (Obj.magic DV.DVALUE_None)))
-
-     | IO.Alloca _   -> Error "top-level Alloca"
-     | IO.Load (_,_) -> Error "top-level Load"
-     | IO.Store (_,_) -> Error "top-level Store"
-     | IO.GEP (_,_,_) -> Error "top-level GEP"
-     | IO.ItoP _ -> Error "top-level ItoP"
-     | IO.PtoI _ -> Error "top-level PtoI"
-     end
 
 let gsize t =
   let open FSigmaHCOL in
@@ -116,43 +50,43 @@ let process_test t =
                              | Float32 -> FSigmaHCOL.Float32V f
                              | Float64 -> FSigmaHCOL.Float64V f
                   ) in
-  if !debug_flag then
+  if !Interpreter.debug_flag then
     begin
       Printf.printf "Generating %d floats:\n" rs ;
       List.iteri randoms ~f:(fun i v -> Printf.printf "\t%d\t-\t%s\n" i (string_of_FloatV v))
     end ;
   match Tests.runFSHCOLTest t randoms with
   | (None,_) ->
-     A.printf [A.white; A.on_red] "Error" ;
-     A.printf [A.yellow] ": %s" oname ;
-     A.printf [] " F-HCOL Compilation failed" ;
+     AT.printf [AT.white; AT.on_red] "Error" ;
+     AT.printf [AT.yellow] ": %s" oname ;
+     AT.printf [] " F-HCOL Compilation failed" ;
      false
   | (Some ast, None) ->
      if !justcompile then
        (output_ll_file (output_file_prefix ^ oname ^ ".ll") ast ; true)
      else
        begin
-         A.printf [A.white; A.on_red] "Error" ;
-         A.printf [A.yellow] ": %s" oname ;
-         A.printf [] " LLVM Compilation failed" ;
+         AT.printf [AT.white; AT.on_red] "Error" ;
+         AT.printf [AT.yellow] ": %s" oname ;
+         AT.printf [] " LLVM Compilation failed" ;
          false
        end
   | (Some ast, Some trace) ->
      if !justcompile then
        (output_ll_file (output_file_prefix ^ oname ^ ".ll") ast ; true)
      else
-       match step trace with
+       match Interpreter.step trace with
        | Error msg ->
-          A.printf [A.white; A.on_red] "Error";
-          A.printf [A.yellow] ": %s :" oname ;
-          A.printf [] "LLVM Intepretation failed with: %s\n" msg ;
+          AT.printf [AT.white; AT.on_red] "Error";
+          AT.printf [AT.yellow] ": %s :" oname ;
+          AT.printf [] "LLVM Intepretation failed with: %s\n" msg ;
           false
        | Ok dv ->
-          A.printf [A.black; A.on_green] "OK" ;
-          A.printf [A.yellow] ": %s :" oname ;
-          A.printf [] " Result:\n" ;
+          AT.printf [AT.black; AT.on_green] "OK" ;
+          AT.printf [AT.yellow] ": %s :" oname ;
+          AT.printf [] " Result:\n" ;
           let ppf = std_formatter in
-          pp_dvalue ppf dv ;
+          Interpreter.pp_dvalue ppf dv ;
           pp_force_newline ppf ();
           pp_print_flush ppf () ;
           true
@@ -162,7 +96,7 @@ let args =
     ("-t", Set_string single, "run single test") ;
     ("-c", Set justcompile, "save IR code to file and exit") ;
     ("-v", Set verbose, "enables more verbose compilation output");
-    ("-d", Set debug_flag, "enables debuging output");
+    ("-d", Set Interpreter.debug_flag, "enables debuging output");
     ("-p", Set printtests, "print names of all tests (for automation)");
   ]
 
