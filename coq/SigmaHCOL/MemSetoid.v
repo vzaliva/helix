eba582f800747276bf5b5834a22f216c63ec7c6e
Vadim Zaliva <lord@crocodile.org>
2019-02-26 10:32:04 -0800

    unfinished setoid stuff

--- coq/SigmaHCOL/MemSetoid.v
+++ coq/SigmaHCOL/MemSetoid.v
@@ -1,5 +1,6 @@
 Require Import Helix.SigmaHCOL.Memory.
 Require Import Helix.Util.FMapSetoid.
+Require Import Helix.Util.OptionSetoid.
 Require Import Helix.HCOL.CarrierType.
 
 Require Import Coq.Classes.RelationClasses.
@@ -37,4 +38,77 @@ Global Instance mem_block_Equiv_Equivalence:
 Proof.
   apply NMS.EquivSetoid_Equivalence.
   typeclasses eauto.
+Qed.
+
+Global Instance mem_lookup_proper:
+  Proper ((eq) ==> (=) ==> (=)) (mem_lookup).
+Proof.
+  simpl_relation.
+  unfold mem_lookup.
+
+  unfold equiv,mem_block_Equiv,mem_block_equiv,NM.Equiv in H0.
+  destruct H0 as [H0 H1].
+  specialize (H0 y).
+  specialize (H1 y).
+
+  destruct ((NM.find (elt:=CarrierA) y x0)) eqn:E1;
+    destruct (NM.find (elt:=CarrierA) y y0) eqn:E2; simpl.
+  -
+    apply RelUtil.opt_r_Some.
+    apply H1.
+    +
+      apply NM.find_2, E1.
+    +
+      apply NM.find_2, E2.
+  -
+    exfalso.
+    apply NM.find_2, NMS.MapsTo_In, H0 in E1.
+    apply NMS.F.not_find_in_iff in E2.
+    congruence.
+  -
+    exfalso.
+    apply NM.find_2,NMS.MapsTo_In, H0 in E2.
+    apply NMS.F.not_find_in_iff in E1.
+    congruence.
+  -
+    reflexivity.
+Qed.
+
+Global Instance mem_mapsto_proper:
+  Proper ((eq) ==> (=) ==> (=) ==> iff) (mem_mapsto).
+Proof.
+  simpl_relation.
+  unfold mem_mapsto.
+
+  unfold equiv,mem_block_Equiv,mem_block_equiv,NM.Equiv in H1.
+  destruct H1 as [H1 H2].
+  specialize (H1 y).
+  specialize (H2 y).
+  split.
+  -
+    intros H.
+
+Admitted.
+
+
+Global Instance mem_in_proper:
+  Proper ((eq) ==> (=) ==> iff) (mem_in).
+Proof.
+  simpl_relation.
+  unfold mem_in.
+  split.
+  -
+    intros H.
+    apply NMS.In_MapsTo in H.
+    destruct H as [e H].
+    apply NMS.MapsTo_In with (e:=e).
+    rewrite <- H0.
+    apply H.
+  -
+    intros H.
+    apply NMS.In_MapsTo in H.
+    destruct H as [e H].
+    apply NMS.MapsTo_In with (e:=e).
+    rewrite H0.
+    apply H.
 Qed.
