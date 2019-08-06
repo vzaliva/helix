2628e917373dd7cfdce5f4dc30f9ca24aa8ee907
Vadim Zaliva <lord@crocodile.org>
2019-08-06 14:17:41 -0700

    constant unfolding

--- coq/MSigmaHCOL/ReifyMSHCOL.v
+++ coq/MSigmaHCOL/ReifyMSHCOL.v
@@ -52,82 +52,103 @@ Run TemplateProgram
               "SHCOL_Op_Names" "parse_SHCOL_Op_Name"
     ).
 
-Fixpoint compileSHCOL2MSHCOL (t:term) {struct t}: TemplateMonad (term) :=
-  match t with
-  | tLambda (nNamed n) vt b =>
-    tmPrint ("lambda " ++ n)  ;;
-            c <- compileSHCOL2MSHCOL b ;;
-            tmReturn(tLambda (nNamed n) vt c)
-  | tApp (tConst opname u) args =>
-    match parse_SHCOL_Op_Name opname, args with
-    | Some n_eUnion, [fm ; svalue; o ; b ; bc] =>
-      tmPrint "eUnion" ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHeUnion" u)
-                              [o; b ; bc])
-    | Some n_eT, [fm ; svalue; i ; b ; bc] =>
-      tmPrint "eT" ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHeT" u)
-                              [i; b; bc])
-    | Some n_SHPointwise, [fm ; svalue; n ; f ; pF ] =>
-      tmPrint "SHPointwise" ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHPointwise" u)
-                              [n; f; pF])
-    | Some n_SHBinOp, [fm ; svalue; o ; f ; pF]
-      =>
-      tmPrint "SHBinOp" ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHBinOp" u)
-                              [o; f; pF])
-    | Some n_SHInductor, [fm ; svalue; n ; f ; pF ; z] =>
-      tmPrint "SHInductor" ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHInductor" u)
-                              [n; f; pF; z])
-    | Some n_IUnion, [svalue; i ; o ; n ; f ; pF ; scompat ; op_family] =>
-      tmPrint "IUnion" ;;
-              c <- compileSHCOL2MSHCOL op_family ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHIUnion" u)
-                              [i; o; n; c])
-    | Some n_ISumUnion, [i ; o ; n ; op_family] =>
-      (* Same as [IUnion] *)
-      tmPrint "ISumUnion" ;;
-              c <- compileSHCOL2MSHCOL op_family ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHIUnion" u)
-                              [i; o; n; c])
-    | Some n_IReduction, [svalue; i ; o ; n ; f ; pF ; scompat ; op_family] =>
-      tmPrint "IReduction" ;;
-              c <- compileSHCOL2MSHCOL op_family ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHIReduction" u)
-                              [i; o; n; svalue; f; pF; c])
-    | Some n_SHCompose, [fm ; svalue; i1 ; o2 ; o3 ; op1 ; op2] =>
-      tmPrint "SHCompose" ;;
-              c1 <- compileSHCOL2MSHCOL op1 ;;
-              c2 <- compileSHCOL2MSHCOL op2 ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHCompose" u)
-                              [i1; o2; o3; c1; c2])
-    | Some n_SafeCast, [svalue; i ; o ; c] =>
-      tmPrint "SafeCast" ;;
-              compileSHCOL2MSHCOL c
-    | Some n_UnSafeCast, [svalue; i ; o ; c] =>
-      tmPrint "UnSafeCast" ;;
-              compileSHCOL2MSHCOL c
-    | Some n_HTSUMUnion, [fm ; i ; o ; svalue; dot ; _ ; _; op1 ; op2] =>
-      tmPrint "HTSumunion" ;;
-              c1 <- compileSHCOL2MSHCOL op1 ;;
-              c2 <- compileSHCOL2MSHCOL op2 ;;
-              tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MHTSUMUnion" u)
-                              [i; o; dot; c1; c2])
+Fixpoint compileSHCOL2MSHCOL (t:term) (fuel: nat) {struct fuel}: TemplateMonad (term) :=
+  match fuel with
+  | O => tmFail "expression complexity limit reached"
+  | S fuel' =>
+    match t with
+    | tConst cname _ =>
+      tmPrint ("Trying to unfold constant" ++ cname) ;;
+              et <- tmUnquote t ;;
+              (match et with
+               | existT_typed_term _ e =>
+                 e' <-  tmEval (unfold "Top.foo") e ;;
+                    t' <- tmQuote e' ;;
+                    match t' with
+                    | tConst cname' _ =>
+                      if string_beq cname cname' then
+                        tmFail ("Could not unfold constant " ++ cname')
+                      else
+                        tmPrint ("Sucessfully nfolded constant " ++ cname) ;;
+                                compileSHCOL2MSHCOL t' fuel'
+                    | _ =>
+                      tmPrint ("Sucessfully nfolded constant " ++ cname) ;;
+                              compileSHCOL2MSHCOL t' fuel'
+                    end
+               end)
+    | tLambda (nNamed n) vt b =>
+      tmPrint ("lambda " ++ n)  ;;
+              c <- compileSHCOL2MSHCOL b fuel' ;;
+              tmReturn(tLambda (nNamed n) vt c)
+    | tApp (tConst opname u) args =>
+      match parse_SHCOL_Op_Name opname, args with
+      | Some n_eUnion, [fm ; svalue; o ; b ; bc] =>
+        tmPrint "eUnion" ;;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHeUnion" u)
+                                [o; b ; bc])
+      | Some n_eT, [fm ; svalue; i ; b ; bc] =>
+        tmPrint "eT" ;;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHeT" u)
+                                [i; b; bc])
+      | Some n_SHPointwise, [fm ; svalue; n ; f ; pF ] =>
+        tmPrint "SHPointwise" ;;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHPointwise" u)
+                                [n; f; pF])
+      | Some n_SHBinOp, [fm ; svalue; o ; f ; pF]
+        =>
+        tmPrint "SHBinOp" ;;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHBinOp" u)
+                                [o; f; pF])
+      | Some n_SHInductor, [fm ; svalue; n ; f ; pF ; z] =>
+        tmPrint "SHInductor" ;;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHInductor" u)
+                                [n; f; pF; z])
+      | Some n_IUnion, [svalue; i ; o ; n ; f ; pF ; scompat ; op_family] =>
+        tmPrint "IUnion" ;;
+                c <- compileSHCOL2MSHCOL op_family fuel' ;;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHIUnion" u)
+                                [i; o; n; c])
+      | Some n_ISumUnion, [i ; o ; n ; op_family] =>
+        (* Same as [IUnion] *)
+        tmPrint "ISumUnion" ;;
+                c <- compileSHCOL2MSHCOL op_family fuel';;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHIUnion" u)
+                                [i; o; n; c])
+      | Some n_IReduction, [svalue; i ; o ; n ; f ; pF ; scompat ; op_family] =>
+        tmPrint "IReduction" ;;
+                c <- compileSHCOL2MSHCOL op_family fuel' ;;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHIReduction" u)
+                                [i; o; n; svalue; f; pF; c])
+      | Some n_SHCompose, [fm ; svalue; i1 ; o2 ; o3 ; op1 ; op2] =>
+        tmPrint "SHCompose" ;;
+                c1 <- compileSHCOL2MSHCOL op1 fuel' ;;
+                c2 <- compileSHCOL2MSHCOL op2 fuel' ;;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MSHCompose" u)
+                                [i1; o2; o3; c1; c2])
+      | Some n_SafeCast, [svalue; i ; o ; c] =>
+        tmPrint "SafeCast" ;;
+                compileSHCOL2MSHCOL c fuel'
+      | Some n_UnSafeCast, [svalue; i ; o ; c] =>
+        tmPrint "UnSafeCast" ;;
+                compileSHCOL2MSHCOL c fuel'
+      | Some n_HTSUMUnion, [fm ; i ; o ; svalue; dot ; _ ; _; op1 ; op2] =>
+        tmPrint "HTSumunion" ;;
+                c1 <- compileSHCOL2MSHCOL op1 fuel' ;;
+                c2 <- compileSHCOL2MSHCOL op2 fuel' ;;
+                tmReturn  (tApp (tConst "Helix.MSigmaHCOL.MSigmaHCOL.MHTSUMUnion" u)
+                                [i; o; dot; c1; c2])
 
-    | None, _ =>
-      tmFail ("Usupported SHCOL operator" ++ opname)
-    | _, _ =>
-      tmFail ("Usupported arguments "
-                ++ string_of_list string_of_term args
-                ++ "for SHCOL operator" ++ opname)
+      | None, _ =>
+        tmFail ("Usupported function call" ++ opname)
+      | _, _ =>
+        tmFail ("Usupported arguments "
+                  ++ string_of_list string_of_term args
+                  ++ "for SHCOL operator " ++ opname)
+      end
+    | _ as t =>
+      tmFail ("Usupported SHCOL syntax " ++ (AstUtils.string_of_term t))
     end
-  | _ as t =>
-    tmFail ("Usupported SHCOL syntax" ++ (AstUtils.string_of_term t))
   end.
-
-
 
 Fixpoint tmUnfoldList {A:Type} (names:list string) (e:A): TemplateMonad A :=
   match names with
@@ -137,16 +158,16 @@ Fixpoint tmUnfoldList {A:Type} (names:list string) (e:A): TemplateMonad A :=
   end.
 
 Definition reifySHCOL {A:Type} (expr: A)
-           (unfold_names: list ident) (* list of top-level names to unfold *)
+           (fuel: nat)
            (res_name: string)
            (lemma_name:  string): TemplateMonad unit
   :=
-    let extra_unfold_names := List.app ["SHFamilyOperatorCompose"; "IgnoreIndex"; "Fin1SwapIndex"; "Fin1SwapIndex2"; "IgnoreIndex2"; "mult_by_nth"; "plus"; "mult"; "const"] unfold_names in
-    eexpr <- tmUnfoldList extra_unfold_names expr ;;
+    let unfold_names := ["SHFamilyOperatorCompose"; "IgnoreIndex"; "Fin1SwapIndex"; "Fin1SwapIndex2"; "IgnoreIndex2"; "mult_by_nth"; "plus"; "mult"; "const"] in
+    eexpr <- tmUnfoldList unfold_names expr ;;
           ast <- @tmQuote A eexpr ;;
-          tmPrint ("AST" ++ (AstUtils.string_of_term ast)) ;;
-          mast <- compileSHCOL2MSHCOL ast ;;
-          tmPrint ("MAST" ++ (AstUtils.string_of_term mast)) ;;
+          (* tmPrint ("AST" ++ (AstUtils.string_of_term ast)) ;; *)
+          mast <- compileSHCOL2MSHCOL ast fuel ;;
+          (* tmPrint ("MAST" ++ (AstUtils.string_of_term mast)) ;; *)
           mexpr <- tmUnquote mast ;;
           (match mexpr with
            | existT_typed_term mexprt mexprv =>
@@ -155,15 +176,16 @@ Definition reifySHCOL {A:Type} (expr: A)
                     ;; tmReturn tt
            end).
 
-
 (* Testing only *)
 (*
 Require Import Omega.
 Program Definition foo := @eUnion Monoid_RthetaFlags CarrierAz 10 3 _.
 Next Obligation. omega. Defined.
 
+Test Quote foo.
+
 Obligation Tactic := idtac.
-Run TemplateProgram (reifySHCOL foo ["foo"] "foo_def" "foo_lemma").
+Run TemplateProgram (reifySHCOL foo 100 "foo_def" "foo_lemma").
 Check foo_def.
 Print foo_def.
 
@@ -174,5 +196,4 @@ Lemma foo_lemma:
 Proof.
   typeclasses eauto.
 Qed.
-
 *)
