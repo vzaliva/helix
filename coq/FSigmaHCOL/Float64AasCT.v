2140f3832e8f67a61e6da365c1a3435af823c65b
Vadim Zaliva <lord@crocodile.org>
2020-01-10 10:10:40 -0500

    proof of `binary64_Setoid`

--- coq/FSigmaHCOL/Float64AasCT.v
+++ coq/FSigmaHCOL/Float64AasCT.v
@@ -8,7 +8,22 @@ Require Import Helix.MSigmaHCOL.CType.
 Instance binary64_Equiv: Equiv binary64 := eq.
 
 Instance binary64_Setoid: Setoid binary64.
-Admitted.
+Proof.
+  split.
+  -
+    intros x.
+    unfold equiv, binary64_Equiv.
+    reflexivity.
+  -
+    intros x y E.
+    unfold equiv, binary64_Equiv in *.
+    auto.
+  -
+    intros x y z Exy Eyz.
+    unfold equiv, binary64_Equiv in *.
+    rewrite Exy, Eyz.
+    reflexivity.
+Qed.
 
 Module MFloat64AasCT <: CType.
   Definition t := binary64.
