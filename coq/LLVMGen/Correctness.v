4c5908541af589a1ab95f867fe9785aec859a5cb
Vadim Zaliva <lord@crocodile.org>
2020-01-16 21:25:23 -0800

    indentation

--- coq/LLVMGen/Correctness.v
+++ coq/LLVMGen/Correctness.v
@@ -185,107 +185,111 @@ Require Import ITree.Interp.TranslateFacts.
 Require Import ITree.Basics.CategoryFacts.
 Require Import StateFacts.
 
-  Import Coq.Strings.String Strings.Ascii.
-  Open Scope string_scope.
-  Open Scope char_scope.
+Import Coq.Strings.String Strings.Ascii.
+Open Scope string_scope.
+Open Scope char_scope.
 
-  Import CatNotations.
+Import CatNotations.
 
-  (* TODO: This is a redefinition from [DSigmaHCOLITree]. To remove after proper reorganization into two files *)
-  (* TODO: Actually even more so there should be a clean part of the tactics that do the generic structural
+(* TODO: This is a redefinition from [DSigmaHCOLITree]. To remove after proper reorganization into two files *)
+(* TODO: Actually even more so there should be a clean part of the tactics that do the generic structural
    rewriting, and a wrapper around it doing stuff specific to this denotation. We should only need the former
    here I believe *)
 
-    Ltac inv_option :=
-      match goal with
-      | h: Some _ ≡ Some _ |-  _ => inv h
-      | h: None   ≡ Some _ |-  _ => inv h
-      | h: Some _ ≡ None   |-  _ => inv h
-      | h: None   ≡ None   |-  _ => inv h
-      end.
+Ltac inv_option :=
+  match goal with
+  | h: Some _ ≡ Some _ |-  _ => inv h
+  | h: None   ≡ Some _ |-  _ => inv h
+  | h: Some _ ≡ None   |-  _ => inv h
+  | h: None   ≡ None   |-  _ => inv h
+  end.
 
-    Ltac inv_sum :=
-      match goal with
-      | h: inl _ ≡ inl _ |-  _ => inv h
-      | h: inr _ ≡ inr _ |-  _ => inv h
-      | h: inl _ ≡ inr _ |-  _ => inv h
-      | h: inr _ ≡ inl _ |-  _ => inv h
-      end.
+Ltac inv_sum :=
+  match goal with
+  | h: inl _ ≡ inl _ |-  _ => inv h
+  | h: inr _ ≡ inr _ |-  _ => inv h
+  | h: inl _ ≡ inr _ |-  _ => inv h
+  | h: inr _ ≡ inl _ |-  _ => inv h
+  end.
 
-    Ltac inv_mem_lookup_err :=
-      unfold mem_lookup_err, trywith in *;
-      break_match_hyp; cbn in *; try (inl_inr || inv_sum || inv_sum).
+Ltac inv_mem_lookup_err :=
+  unfold mem_lookup_err, trywith in *;
+  break_match_hyp; cbn in *; try (inl_inr || inv_sum || inv_sum).
 
-    Ltac inv_memory_lookup_err :=
-      unfold memory_lookup_err, trywith in *;
-      break_match_hyp; cbn in *; try (inl_inr || inv_sum || inv_sum).
-
-
-    Ltac state_steps :=
-      cbn;
-      repeat (
-          ((match goal with
-            | |- ITree.bind (lift_Derr _) _ ≈ _ => break_match_goal; inv_memory_lookup_err; inv_option
-            | |- ITree.bind (lift_Serr _) _ ≈ _ => break_match_goal; inv_memory_lookup_err; inv_option
-            | |- ITree.bind (State.interp_state _ (lift_Derr _) _) _ ≈ _ => break_match_goal; inv_option
-            | |- ITree.bind (State.interp_state _ (lift_Serr _) _) _ ≈ _ => break_match_goal; inv_option
-            end) || state_step); cbn).
+Ltac inv_memory_lookup_err :=
+  unfold memory_lookup_err, trywith in *;
+  break_match_hyp; cbn in *; try (inl_inr || inv_sum || inv_sum).
 
 
-  Lemma denote_bks_nil: forall s, D.denote_bks [] s ≈ ret (inl s).
-  Proof.
-    intros s; unfold D.denote_bks.
-    unfold loop.
-    cbn. rewrite bind_ret_l.
+Ltac state_steps :=
+  cbn;
+  repeat (
+      ((match goal with
+        | |- ITree.bind (lift_Derr _) _ ≈ _ => break_match_goal; inv_memory_lookup_err; inv_option
+        | |- ITree.bind (lift_Serr _) _ ≈ _ => break_match_goal; inv_memory_lookup_err; inv_option
+        | |- ITree.bind (State.interp_state _ (lift_Derr _) _) _ ≈ _ => break_match_goal; inv_option
+        | |- ITree.bind (State.interp_state _ (lift_Serr _) _) _ ≈ _ => break_match_goal; inv_option
+        end) || state_step); cbn).
+
+
+Lemma denote_bks_nil: forall s, D.denote_bks [] s ≈ ret (inl s).
+Proof.
+  intros s; unfold D.denote_bks.
+  unfold loop.
+  cbn. rewrite bind_ret_l.
+  match goal with
+  | |- KTree.iter ?body ?s ≈ _ =>
+    rewrite (unfold_iter body s)
+  end.
+  state_steps.
+  reflexivity.
+Qed.
+
+(* We could probably fix [env] to be [nil] *)
+Lemma compile_FSHCOL_correct:
+  forall (op: DSHOperator) st bid_out st' bid_in bks σ env mem g ρ mem_llvm,
+    genIR op st bid_out ≡ inr (st',(bid_in,bks)) ->
+    eutt (bisim σ)
+         (translate (@subevent _ E _) (interp_Mem (denoteDSHOperator σ op) mem))
+         (translate (@subevent _ E _)
+                    (interp_to_L3' helix_intrinsics
+                                   (D.denote_bks (normalize_types_blocks env bks) bid_in)
+                                   g ρ mem_llvm)).
+Proof.
+  induction op; intros; rename H into HCompile.
+  - inv HCompile.
+    unfold interp_Mem. simpl denoteDSHOperator.
+    rewrite interp_state_ret, translate_ret.
+    simpl normalize_types_blocks.
+    admit.
+  - destruct src, dst.
+    simpl in HCompile.
+    repeat break_match_hyp; try inl_inr.
+    inv Heqs; inv HCompile.
     match goal with
-    | |- KTree.iter ?body ?s ≈ _ =>
-      rewrite (unfold_iter body s)
+    | |- context[add_comment _ ?ss] => generalize ss; intros ls
     end.
-    state_steps.
-    reflexivity.
-    Qed.
+    unfold interp_Mem. simpl denoteDSHOperator.
 
-  (* We could probably fix [env] to be [nil] *)
-  Lemma compile_FSHCOL_correct:
-    forall (op: DSHOperator) st bid_out st' bid_in bks σ env mem g ρ mem_llvm,
-      genIR op st bid_out ≡ inr (st',(bid_in,bks)) ->
-      eutt (bisim σ)
-           (translate (@subevent _ E _) (interp_Mem (denoteDSHOperator σ op) mem))
-           (translate (@subevent _ E _)
-                      (interp_to_L3' helix_intrinsics
-                                     (D.denote_bks (normalize_types_blocks env bks) bid_in)
-                                     g ρ mem_llvm)).
-  Proof.
-    induction op; intros; rename H into HCompile.
-    - inv HCompile.
-      unfold interp_Mem. simpl denoteDSHOperator.
-      rewrite interp_state_ret, translate_ret.
-      simpl normalize_types_blocks.
-      admit.
-    - destruct src, dst.
-      simpl in HCompile.
-      repeat break_match_hyp; try inl_inr.
-      inv Heqs; inv HCompile.
-      match goal with
-      | |- context[add_comment _ ?ss] => generalize ss; intros ls
-      end.
-      unfold interp_Mem. simpl denoteDSHOperator.
+Admitted.
 
-  Admitted.
+(* The relation to provide is not [TT] but rather the singleton pair of the empty memories *)
+Lemma compiler_correct_aux:
+  forall (p:FSHCOLProgram)
+    (data:list binary64)
+    (pll: toplevel_entities typ (list (LLVMAst.block typ))),
+    compile p data ≡ inr pll ->
+    eutt (fun _ _ => True) (semantics_FSHCOL p data) (semantics_llvm pll).
+Proof.
+Admitted.
 
-  (* The relation to provide is not [TT] but rather the singleton pair of the empty memories *)
-  Lemma compiler_correct_aux:
-    forall (p:FSHCOLProgram) data pll,
-      compile p data ≡ inr pll ->
-      eutt (fun _ _ => True) (semantics_FSHCOL p data) (semantics_llvm pll).
-  Proof.
-  Admitted.
-
-  Theorem compiler_correct:
-    exists RR,
-    forall (p:FSHCOLProgram) data pll,
-      compile p data ≡ inr pll ->
-      eutt RR (semantics_FSHCOL p data) (semantics_llvm pll).
-  Proof.
-    eexists; eapply compiler_correct_aux.
-  Qed.
+Theorem compiler_correct:
+  exists RR,
+  forall (p:FSHCOLProgram)
+    (data:list binary64)
+    (pll: toplevel_entities typ (list (LLVMAst.block typ))),
+    compile p data ≡ inr pll ->
+    eutt RR (semantics_FSHCOL p data) (semantics_llvm pll).
+Proof.
+  eexists; eapply compiler_correct_aux.
+Qed.
