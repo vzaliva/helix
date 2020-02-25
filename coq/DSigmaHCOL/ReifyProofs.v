a444570bd426f1d4f180e21c7efb855b383ae192
Vadim Zaliva <lord@crocodile.org>
2020-02-24 19:07:13 -0800

    removed unused pre-condition

--- coq/DSigmaHCOL/ReifyProofs.v
+++ coq/DSigmaHCOL/ReifyProofs.v
@@ -2281,7 +2281,6 @@ Global Instance BinOp_DSH_pure
        (a: AExpr)
        `{dft : DSHIBinCarrierA a}
        {ts: TypeSig}
-       (TCA: AExpr_typechecks a (TypeSig_append DSHIBinCarrierA_TypeSig ts))
   :
     DSH_pure (DSHBinOp o x_p y_p a) ts x_p y_p.
 Proof.
