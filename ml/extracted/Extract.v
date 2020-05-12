2e6147181e10d8e510b2d7c7c037a95095467bce
Yannick <y.zakowski@gmail.com>
2020-05-12 15:00:26 -0400

    Low quality quotation marks

--- ml/extracted/Extract.v
+++ ml/extracted/Extract.v
@@ -39,7 +39,7 @@ Extract Inlined Constant Flocq.IEEE754.Binary.FF2R => "(fun _ -> assert false)".
 Extract Inlined Constant Flocq.IEEE754.Binary.B2R => "(fun _ -> assert false)".
 Extract Inlined Constant Flocq.IEEE754.Binary.round_mode => "(fun _ -> assert false)".
 Extract Inlined Constant Flocq.Calc.Bracket.inbetween_loc => "(fun _ -> assert false)".
-Extract Inlined Constant Archi.ppc64 => “false”.
+Extract Inlined Constant Archi.ppc64 => "false".
 
 Set Extraction AccessOpaque.
 (* NOTE: assumes that this file is compiled from /src *)
