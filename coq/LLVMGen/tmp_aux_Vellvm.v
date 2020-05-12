636322358fbd5fe959dd06fdbc163930ea352179
Yannick <y.zakowski@gmail.com>
2020-05-12 18:29:24 -0400

    Structural manipulation of generation of mcfg

--- coq/LLVMGen/tmp_aux_Vellvm.v
+++ coq/LLVMGen/tmp_aux_Vellvm.v
@@ -8,6 +8,7 @@ From ITree Require Import
 
 From Vellvm Require Import
      Util
+     AstLib
      LLVMEvents
      DynamicTypes
      CFG
@@ -516,3 +517,264 @@ Section ElimEvent.
   (*     elim_l _ t ≈  interp (case_ h ((fun _ e => trigger e): Handler F F)) t. *)
 
 End ElimEvent.
+
+(* I want an Ltac library for Vellvm. Here are a few first elementary pieces *)
+(* TODO YZ : do it right, learn Ltac(2) *)
+
+Ltac flatten_goal :=
+  match goal with
+  | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
+  end.
+
+Ltac flatten_hyp h :=
+  match type of h with
+  | context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
+  end.
+
+Ltac flatten_all :=
+  match goal with
+  | h: context[match ?x with | _ => _ end] |- _ => let Heq := fresh "Heq" in destruct x eqn:Heq
+  | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
+  end.
+
+Ltac inv H := inversion H; try subst; clear H.
+
+Global Tactic Notation "intros !" := repeat intro.
+
+(* inv by name of the Inductive relation *)
+Ltac invn f :=
+    match goal with
+    | [ id: f |- _ ] => inv id
+    | [ id: f _ |- _ ] => inv id
+    | [ id: f _ _ |- _ ] => inv id
+    | [ id: f _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ _ _ _ _ |- _ ] => inv id
+    end.
+
+(* destruct by name of the Inductive relation *)
+Ltac destructn f :=
+    match goal with
+    | [ id: f |- _ ] => destruct id
+    | [ id: f _ |- _ ] => destruct id
+    | [ id: f _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ _ _ _ _ |- _ ] => destruct id
+    end.
+
+(* apply by name of the Inductive relation *)
+Ltac appn f :=
+    match goal with
+    | [ id: f |- _ ] => apply id
+    | [ id: f _ |- _ ] => apply id
+    | [ id: f _ _ |- _ ] => apply id
+    | [ id: f _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ _ _ _ _ |- _ ] => apply id
+    end.
+
+(* eapply by name of the Inductive relation *)
+Ltac eappn f :=
+    match goal with
+    | [ id: f |- _ ] => eapply id
+    | [ id: f _ |- _ ] => eapply id
+    | [ id: f _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ _ _ _ _ |- _ ] => eapply id
+    end.
+
+(** [break_match_hyp] looks for a [match] construct in some
+    hypothesis, and destructs the discriminee, while retaining the
+    information about the discriminee's value leading to the branch
+    being taken. *)
+Ltac break_match_hyp :=
+  match goal with
+    | [ H : context [ match ?X with _ => _ end ] |- _] =>
+      match type of X with
+        | sumbool _ _ => destruct X
+        | _ => destruct X eqn:?
+      end
+  end.
+
+(** [break_match_goal] looks for a [match] construct in your goal, and
+    destructs the discriminee, while retaining the information about
+    the discriminee's value leading to the branch being taken. *)
+Ltac break_match_goal :=
+  match goal with
+    | [ |- context [ match ?X with _ => _ end ] ] =>
+      match type of X with
+        | sumbool _ _ => destruct X
+        | _ => destruct X eqn:?
+      end
+  end.
+
+(** [break_match] breaks a match, either in a hypothesis or in your
+    goal. *)
+Ltac break_match := break_match_goal || break_match_hyp.
+
+Ltac inv_option :=
+  match goal with
+  | h: Some _ = Some _ |-  _ => inv h
+  | h: None   = Some _ |-  _ => inv h
+  | h: Some _ = None   |-  _ => inv h
+  | h: None   = None   |-  _ => inv h
+  end.
+
+Ltac inv_sum :=
+  match goal with
+  | h: inl _ = inl _ |-  _ => inv h
+  | h: inr _ = inr _ |-  _ => inv h
+  | h: inl _ = inr _ |-  _ => inv h
+  | h: inr _ = inl _ |-  _ => inv h
+  end.
+
+Section TLE_To_Modul.
+
+  Definition opt_first {T: Type} (o1 o2: option T): option T :=
+    match o1 with | Some x => Some x | None => o2 end.
+
+  Definition modul_app {T X} (m1 m2: modul T X): modul T X :=
+    let (name1, target1, layout1, tdefs1, globs1, decls1, defs1) := m1 in
+    let (name2, target2, layout2, tdefs2, globs2, decls2, defs2) := m2 in
+    {|
+      m_name := opt_first name1 name2;
+      m_target := opt_first target1 target2;
+      m_datalayout := opt_first layout1 layout2;
+      m_type_defs := tdefs1 ++ tdefs2;
+      m_globals := globs1 ++ globs2;
+      m_declarations := decls1 ++ decls2;
+      m_definitions := defs1 ++ defs2
+    |}.
+
+  Lemma modul_of_toplevel_entities_cons:
+    forall {T X} tle tles, 
+      @modul_of_toplevel_entities T X (tle :: tles) = modul_app (modul_of_toplevel_entities T [tle]) (modul_of_toplevel_entities T tles).
+  Proof.
+    intros.
+    unfold modul_of_toplevel_entities; cbn; f_equal;
+      try ((break_match_goal; reflexivity) || (rewrite <- !app_nil_end; reflexivity)).
+  Qed.
+
+  Lemma modul_of_toplevel_entities_app:
+    forall {T X} tle1 tle2, 
+    @modul_of_toplevel_entities T X (tle1 ++ tle2) = modul_app (modul_of_toplevel_entities T tle1) (modul_of_toplevel_entities T tle2).
+  Proof.
+    induction tle1 as [| tle tle1 IH]; intros; cbn; [reflexivity |].
+    rewrite modul_of_toplevel_entities_cons, IH; cbn.
+    f_equal;
+      try ((break_match_goal; reflexivity) || (rewrite <- !app_nil_end, app_assoc; reflexivity)).
+  Qed.
+
+  Infix "@" := (modul_app) (at level 60).
+
+  Open Scope list.
+  Lemma m_definitions_app: forall {T X} (p1 p2 : modul T X),
+      m_definitions (p1 @ p2) = m_definitions p1 ++ m_definitions p2.
+  Proof.
+    intros ? ? [] []; reflexivity.
+  Qed.
+
+  Lemma m_name_app: forall {T X} (p1 p2 : modul T X),
+      m_name (p1 @ p2) = opt_first (m_name p1) (m_name p2).
+  Proof.
+    intros ? ? [] []; reflexivity.
+  Qed.
+
+  Lemma m_target_app: forall {T X} (p1 p2 : modul T X),
+      m_target (p1 @ p2) = opt_first (m_target p1) (m_target p2).
+  Proof.
+    intros ? ? [] []; reflexivity.
+  Qed.
+
+  Lemma m_datalayout_app: forall {T X} (p1 p2 : modul T X),
+      m_datalayout (p1 @ p2) = opt_first (m_datalayout p1) (m_datalayout p2).
+  Proof.
+    intros ? ? [] []; reflexivity.
+  Qed.
+
+  Lemma m_type_defs_app: forall {T X} (p1 p2 : modul T X),
+      m_type_defs (p1 @ p2) = m_type_defs p1 ++ m_type_defs p2.
+  Proof.
+    intros ? ? [] []; reflexivity.
+  Qed.
+
+  Lemma m_globals_app: forall {T X} (p1 p2 : modul T X),
+      m_globals (p1 @ p2) = m_globals p1 ++ m_globals p2.
+  Proof.
+    intros ? ? [] []; reflexivity.
+  Qed.
+
+  Lemma m_declarations_app: forall {T X} (p1 p2 : modul T X),
+      m_declarations (p1 @ p2) = m_declarations p1 ++ m_declarations p2.
+  Proof.
+    intros ? ? [] []; reflexivity.
+  Qed.
+
+  Lemma map_option_cons_inv: forall {A B} (f : A -> option B) (a : A) (l : list A) (r : list B),
+      map_option f (a :: l) = Some r ->
+       exists b r',
+        f a = Some b /\
+        map_option f l = Some r' /\
+        r = b :: r'.
+  Proof.      
+    intros.
+    (* YZ TODO : Test on 8.11 if cbn also behaves annoyingly here *)
+    simpl in H; do 2 (break_match_hyp; try inv_option). 
+    do 2 eexists; repeat split; auto.
+  Qed.
+
+  Lemma map_option_cons: forall {A B} (f : A -> option B) (a : A) (b : B) (l : list A) (r : list B),
+        f a = Some b ->
+        map_option f l = Some r ->
+        map_option f (a :: l) = Some (b :: r).
+  Proof.      
+    intros * EQ1 EQ2; simpl; rewrite EQ1, EQ2; reflexivity.
+  Qed.
+
+  Lemma map_option_app_inv: forall {A B} (f : A -> option B) (l1 l2 : list A) (r : list B),
+      map_option f (l1 ++ l2) = Some r ->
+      exists r1 r2,
+        map_option f l1 = Some r1 /\
+        map_option f l2 = Some r2 /\
+        r = r1 ++ r2.
+  Proof.
+    induction l1 as [| x l1 IH]; intros * EQ.
+    - do 2 eexists; repeat split; try reflexivity; auto. 
+    - generalize EQ; intros EQ'; apply map_option_cons_inv in EQ'; destruct EQ' as (b & ? & EQ1 & EQ2 & ->). 
+      apply IH in EQ2; destruct EQ2 as (r1 & r2 & EQ2 & EQ3 & ->).
+      exists (b::r1), r2; repeat split; auto. 
+      apply map_option_cons; auto.
+  Qed.
+
+  Lemma mcfg_of_app_modul: forall {T} p1 p2 (m : mcfg T),
+      mcfg_of_modul _ (p1 @ p2) = Some m ->
+      exists m1 m2, mcfg_of_modul _ p1 = Some m1 /\
+               mcfg_of_modul _ p2 = Some m2 /\
+               m1 @ m2 = m.
+  Proof.
+    intros * EQ; cbn in EQ.
+    break_match_hyp; inv_option.
+    rewrite m_definitions_app in Heqo.
+    apply map_option_app_inv in Heqo; destruct Heqo as (? & ? & EQ1 & EQ2 & ->).
+    cbn; rewrite EQ1, EQ2; do 2 eexists; repeat split; try reflexivity.
+    cbn.
+    rewrite m_name_app, m_target_app, m_datalayout_app, m_type_defs_app, m_globals_app, m_declarations_app; reflexivity. 
+  Qed.
+
+End TLE_To_Modul.
+
+Infix "@" := (modul_app) (at level 60).
