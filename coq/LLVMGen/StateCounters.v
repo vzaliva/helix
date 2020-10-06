7988a727f10dd8716053d437f799168f100f80e6
Calvin Beck <hobbes@ualberta.ca>
2020-10-06 11:15:17 -0400

    Fix proofs, moves things into separate files.

--- coq/LLVMGen/StateCounters.v
+++ coq/LLVMGen/StateCounters.v
@@ -8,7 +8,28 @@ Import ListNotations.
 Set Implicit Arguments.
 Set Strict Implicit.
 
-(* TODO: Move this *)
+(* TODO: Not sure where this should belong *)
+Lemma resolve_PVar_simple : forall p s s' x v,
+    resolve_PVar p s ≡ inr (s', (x, v)) ->
+    exists sz n,
+      nth_error (Γ s') n ≡ Some (x, TYPE_Pointer (TYPE_Array sz TYPE_Double)) /\
+      MInt64asNT.from_Z sz ≡ inr v /\ p ≡ PVar n /\ s ≡ s'.
+Proof.
+  intros * H.
+  unfold resolve_PVar in H.
+  cbn* in H.
+  simp.
+  do 2 eexists; eauto.
+Qed.
+
+Ltac inv_resolve_PVar H :=
+  apply resolve_PVar_simple in H;
+  destruct H as (?sz & ?n & ?LUn & ?EQsz & -> & <-).
+
+
+Global Opaque resolve_PVar.
+
+(* TODO: Move this? *)
 Lemma incVoid_block_count :
   forall s1 s2 bid,
     incVoid s1 ≡ inr (s2, bid) ->
@@ -23,7 +44,7 @@ Proof.
   Opaque incVoid.
 Qed.
 
-(* TODO: Move this *)
+(* TODO: Move this? *)
 Lemma incLocal_block_count :
   forall s1 s2 bid,
     incLocal s1 ≡ inr (s2, bid) ->
@@ -38,7 +59,7 @@ Proof.
   Opaque incLocal.
 Qed.
 
-(* TODO: Move this *)
+(* TODO: Move this? *)
 Lemma genNExpr_block_count :
   ∀ (nexp : NExpr) (s1 s2 : IRState) (e : exp typ) (c : code typ),
     genNExpr nexp s1 ≡ inr (s2, (e, c)) → (block_count s2 ≡ block_count s1)%nat.
@@ -107,13 +128,25 @@ Proof.
   end; subst; auto; try lia.
 Qed.
 
+(* TODO: uncertain if this belongs somewhere else *)
+Lemma resolve_PVar_state :
+  forall p s1 s2 x,
+    resolve_PVar p s1 ≡ inr (s2, x) ->
+    s1 ≡ s2.
+Proof.
+  intros p s1 s2 [x v] H.
+  pose proof resolve_PVar_simple p s1 H as H0.
+  destruct H0 as (sz & n & H0).
+  intuition.
+Qed.
+
+
 (* TODO: move this *)
 Lemma genIR_block_count :
   forall op s1 s2 nextblock b bk_op,
     genIR op nextblock s1 ≡ inr (s2, (b, bk_op)) ->
     block_count s2 ≥ block_count s1.
 Proof.
-  (* TODO: after moving this it seems to be looping forever...? *)
   induction op;
     intros s1 s2 nextblock b bk_op H;
     cbn in H; simp;
@@ -145,6 +178,6 @@ Proof.
              genIR ?op nextblock s1 ≡ inr (s2, (b, bk_op)) → block_count s2 ≥ block_count s1,
            GEN: genIR ?op _ _ ≡ inr _ |- _ =>
          apply IH in GEN
-         end; cbn in *);
-         try lia.
+         end; cbn in * );
+      try lia.
 Qed.
