32d4af571c4fd47af491cf0f3e3f347fe9374ec0
euisuny <ey222@cornell.edu>
2021-01-04 22:51:08 -0500

    Working on bubbling tfor body in IMap

--- coq/LLVMGen/Correctness_IMap2.v
+++ coq/LLVMGen/Correctness_IMap2.v
@@ -15,6 +15,7 @@ Require Import Helix.LLVMGen.Correctness_While.
 From Vellvm Require Import Utils.Commutation.
 
 Require Import Paco.paco.
+From ITree Require Import HeterogeneousRelations.
 
 Import ProofMode.
 
@@ -38,35 +39,45 @@ Local Open Scope nat_scope.
 Section DSHIMap_is_tfor.
 
   (* Iterative body of [IMap] *)
-  Definition DSHIMap_tfor_body
+  Definition DSHIMap_body
              (σ : evalContext)
              (f : AExpr)
              (offset : nat)
-             (init acc : mem_block) :=
+             (init acc : mem_block) : itree Event mem_block :=
     v <-
        lift_Derr (mem_lookup_err "Error reading memory denoteDSHIMap" offset init);;
     vn <- lift_Serr (MInt64asNT.from_nat offset);;
     v'<- denoteIUnCType σ f vn v;;
     ret (mem_add offset v' acc).
 
-  (* [tfor] formulation of [DSHIMap]. *)
-  Definition DSHIMap_tfor
+  (* [tfor] formulation of [DSHIMap].
+     "Reverse/downward" indexing ([n - 1 .. 0]). *)
+  Definition DSHIMap_tfor_down
              (σ : evalContext)
-             (n : nat)
              (f : AExpr)
-             (x y : mem_block):
+             (i n : nat)
+             (init acc : mem_block):
     itree Event mem_block :=
     (* IMap has "reverse indexing" on its body *)
-    tfor (fun i acc => DSHIMap_tfor_body σ f (n - 1 - i) x acc) 0 n y.
+    tfor (fun i acc => DSHIMap_body σ f (n - 1 - i) init acc) i n acc.
+
+  (* "Right"-side-up indexing variant ([0 .. n - 1]). *)
+  Definition DSHIMap_tfor_up
+             (σ : evalContext)
+             (f : AExpr)
+             (i n : nat)
+             (init acc : mem_block):
+    itree Event mem_block :=
+    tfor (fun i acc => DSHIMap_body σ f i init acc) i n acc.
 
   (* [denoteDSHIMap] is equivalent to [tfor] with "reverse indexing" on an
      [IMap] body. *)
   Lemma denoteDSHIMap_as_tfor:
     forall (σ : evalContext) n f x y,
-      denoteDSHIMap n f σ x y ≈ DSHIMap_tfor σ n f x y.
+      denoteDSHIMap n f σ x y ≈ DSHIMap_tfor_down σ f 0 n x y.
   Proof.
     intros.
-    unfold DSHIMap_tfor. revert y.
+    unfold DSHIMap_tfor_down. revert y.
     induction n.
     - cbn. intros.
       setoid_rewrite tfor_0.
@@ -89,157 +100,134 @@ Section DSHIMap_is_tfor.
       cbn. assert (n - 0 - S i ≡ n - 1 - i) by lia. rewrite H0. reflexivity.
   Qed.
 
-  Definition mem_block_Equiv_up_to (n : nat) (m m' : mem_block) :=
-    forall k, k < n -> find k m = find k m'.
+  Definition pair_rel {A B} (RA : A -> A -> Prop) (RB : B -> B -> Prop) :=
+    (fun '(a, b) '(a', b') => RA a a' /\ RB b b').
 
   (* < Desired Lemma > *)
-  (* Lemma mem_block_equiv_is_order_independent : *)
-  (*   forall n mem init_vec, *)
-  (*       eutt (equiv_mem_block /2\ eq_mem) *)
-  (*       interp_helix (tfor body_up   0 n) (m, init_vec) *)
-  (*       interp_helix (tfor body_down 0 n) (m, init_vec) *)
-  (*       equiv_mem_block_frag i n *)
-  (*       equiv_mem_block <-> equiv_mem_block_frag 0 n. *)
-
-  (* TODO *)
-
-  (* < Generalized Lemma >
-        Equivalent to Desired lemma when i = 0
-   *)
-  (* TODO *)
-
-  (*
-        forall i, 0 <= i < n,
-        eutt (equiv_mem_block ** eq_mem)
-        interp_helix (vec <- (tfor body_up i n) init ;;
-                          (tfor body_down (n-i) n) vec)
-                    (vec <- tfor body_down i n) init ;;
-                          (tfor body_up  (n-i) n)
-   *)
-  (*
-        (* Inductive hypothesis with one step "bubbled up" *)
-        equiv_mem_block_frag i n 
-          ((vec0 <- (tfor body_up (S i) j) init ;;
-            (veci <- (body_up i) vec0 ;;
-          (vecn <- (tfor body_up j n) veci;;
-          (tfor body_down (n-i) n) vecn)
-          ((vec0 <- (tfor body_up (S i) n) init ;;
-          (veci <- (body_up i) vec0 ;;
-          (tfor body_down (n-i) n) vecn)
-  *)
-
-
-  (* Wishful? *)
-  Lemma tfor_IMap_rev_fixed :
-    forall {E} (σ : evalContext) (n : nat) (f : AExpr) (x y : mem_block) mem,
-    exists vec,
-      eutt (E:=E) (fun x y =>
-              match x, y with
-              | None, None => True
-              | Some (mH, mem_bl), Some (mH', mem_bl') =>
-                  mH ≡ mH' /\
-                  mem_block_Equiv_up_to n mem_bl mem_bl'
-              | _, _ => False
-              end)
-    (interp_helix (tfor (fun i acc => DSHIMap_tfor_body σ f (n - i) x acc) 0 n y) mem)
-    (Ret vec)
-      (* /\ (forall k, k < n -> exists v, find k vec = Some v). *).
-  Admitted.
-
-  (* Lemma mem_block_Equiv_up_to_eq : *)
-  (* ... *)
-  (*   mem_block_Equiv m m' ~ mem_block_Equiv_up_to n m m' *)
-
-
-  (* TODO: 1. Prove this
-           2. Generalize (t : nat -> itree void1 nat)
-           3. 
-   *)
-  Lemma pure_swap :
-    forall (t t' : unit -> itree void1 unit),
-      x <- t () ;; t' x ≈ x <- t' () ;; t x.
-  Admitted.
-
-(* TODO : F : nat -> binary64 Formal I, 0 <=I <k, mem1[i] = f i /\ mem2[n-i-1] = f (n-i-1) *)
-  (* The "IMap" operator is compiled "backwards", resulting in a need to
-     equate reversed and unreversed denotation with the mem_blocks equiv
-     relation. *)
-  (* needs to be stated after interpretation *)
-  (* TODO: interp (bind (trigger read1) (fun x => bind (trigger read2) (fun y => k x y) \approx interp (bind (trigger read 2) (fun y => bind (trigger read1) (fun x => k x y))  *)
-  Lemma tfor_IMap_rev :
-    forall {E} (σ : evalContext) (n : nat) (f : AExpr) (x y : mem_block) mem,
-    no_failure (E:=E)
-      (interp_helix (tfor (fun i acc => DSHIMap_tfor_body σ f (n - i) x acc) 0 n y) mem) ->
-    no_failure (E:=E)
-      (interp_helix (tfor (fun i acc => DSHIMap_tfor_body σ f i x acc) 0 n y) mem) ->
-    eutt (E:=E) (fun x y =>
-            match x, y with
-            | None, None => True
-            | Some (mH, mem_bl), Some (mH', mem_bl') =>
-                mH ≡ mH' /\
-                equiv mem_bl mem_bl'
-            | _, _ => False
-            end)
-  (interp_helix (tfor (fun i acc => DSHIMap_tfor_body σ f (n - i) x acc) 0 n y) mem)
-  (interp_helix (tfor (fun i acc => DSHIMap_tfor_body σ f i x acc) 0 n y) mem).
-Proof.
-
-  intros.
-  (* eapply eqit_trans. *)
-  (*   revert σ f x y. *)
-  (*   induction n. *)
-  (*   - intros. setoid_rewrite tfor_0. apply eutt_Ret. *)
-  (*     apply mem_block_Equiv_Reflexive. *)
-  (*   - intros. *)
-  (*     rewrite tfor_unroll. *)
-  (*     rewrite tfor_unroll. unfold equiv, mem_block_Equiv. *)
-  Admitted.
-
-  (* What kind of post conditions can we write? *)
-  Lemma IMap_body_post:
-    forall σ f n x y,
-    exists X, DSHIMap_tfor_body σ f n x y ⤳ X.
+  Lemma mem_block_equiv_is_order_independent :
+    ∀ (n : nat) (init_vec : memoryH) x y σ f,
+      eutt (E := void1) (Coqlib.option_rel (pair_rel (@eq memoryH) (@equiv mem_block _)))
+        (interp_helix (DSHIMap_tfor_up σ f 0 n x y) init_vec)
+        (interp_helix (DSHIMap_tfor_down σ f 0 n x y) init_vec).
   Proof.
-    intros. cbn.
-    evar (post : Memory.NM.t binary64 -> Prop).
-    exists post.
-    rewrite has_post_post_strong. unfold has_post_strong.
-    Unshelve.
-    2 : refine (fun m => find n m = find n m).
-    Admitted.
-
-  Lemma distinct_bind_swap :
-    forall E n1 n2 t1 t1' t2 t2',
-      eutt (E := E) (fun m m' => find n1 m = find n1 m') t1 t1' ->
-      eutt (E := E) (fun m m' => find n2 m = find n2 m') t2 t2' ->
-      n1 ≢ n2 ->
-      eutt (E := E) (@equiv mem_block _) (t1 ;; t2) (t2' ;; t1').
+    intros *.
+    unfold DSHIMap_tfor_up, DSHIMap_tfor_down.
   Admitted.
 
-  Lemma has_post_distinct :
-    forall E n (t t': itree E mem_block),
-      t' ⤳ (fun m => exists k, find n m = Some k) ->
-      (* has only modified a range that is not n1 *)
-      t ⤳ (fun m => find n m = None) ->
-      eutt (E := E) (@equiv mem_block _) (t ;; t') (t' ;; t).
+  Instance option_pair_rel_Equiv {A B} {R : A -> A -> Prop} {S : B -> B -> Prop}
+           {EQR: Equivalence R} {EQS: Equivalence S}
+    : Equivalence (Coqlib.option_rel (pair_rel R S)).
+  Proof.
+    split.
+    - red; intros [ [] | ]; constructor; unfold pair_rel; split; reflexivity.
+    - red; intros [ [] | ] [ [] | ] H; inv H ; constructor; unfold pair_rel in *.
+      destruct H2 as []. split; symmetry; auto.
+    - red; intros [ [] | ] [ [] | ] [ [] | ] H H'; inv H; inv H'; constructor;
+        unfold pair_rel in *.
+      destruct H2 as []; destruct H1 as []; split; etransitivity; eauto.
+  Qed.
+
+  Notation "⤉ ( R , S ) " := (Coqlib.option_rel (pair_rel R S)) (at level 10).
+
+  Definition equiv_mem_block_frag (i n : nat) (m m' : mem_block) :=
+    forall k, i <= k -> k < n -> find k m = find k m'.
+
+  Definition equiv_mem_block (n : nat) (m m' : mem_block) :=
+    equiv_mem_block_frag 0 n m m'.
+
+  Notation "m ≡[ i , n  ] m'" := (equiv_mem_block_frag i n m m') (at level 10).
+  Notation "m ≡[ n ] m'" := (equiv_mem_block n m m') (at level 10).
+
+  (* < Generalized Lemma > Equivalent to Desired lemma when i = 0 *)
+  Lemma mem_block_equiv_is_order_independent_gen :
+    ∀ (i n : nat) (memH : memoryH) σ f init,
+      0 <= i < n ->
+      eutt (E := void1) (⤉ (eq, equiv))
+           (interp_helix (vec <- DSHIMap_tfor_up σ f i n init init;;
+                          DSHIMap_tfor_down σ f (n - i) n init vec) memH)
+           (interp_helix (vec <- DSHIMap_tfor_down σ f i n init init;;
+                          DSHIMap_tfor_up σ f (n - i) n init vec) memH).
+  Proof.
+    intros * [LO HI].
+    Opaque DSHIMap_body.
+    revert i LO HI memH σ f init.
+    induction n; intros.
+    - inv HI.
+    - unfold DSHIMap_tfor_up, DSHIMap_tfor_down.
+      assert (EQ: S n - 1 ≡ n) by lia; rewrite EQ; clear EQ.
+      rewrite tfor_unroll; [ | lia].
+      rewrite tfor_unroll; [ | lia].
+
+      setoid_rewrite interp_helix_bind.
+
+      assert
+        (forall memH,
+            eutt (E := void1) (⤉ (eq, equiv))
+        (interp_helix
+          (ITree.bind'
+            (fun a : mem_block =>
+              tfor (fun (i0 : nat) (acc : mem_block) => DSHIMap_body σ f i0 init acc) (S i) (S n) a)
+            (DSHIMap_body σ f i init init)) memH)
+
+        (interp_helix
+          (ITree.bind'
+            (fun a : mem_block => DSHIMap_body σ f i init a)
+            (tfor (fun (i0 : nat) (acc : mem_block) => DSHIMap_body σ f i0 init acc) (S i) (S n) init)) memH)).
+      {
+        intros.
+        setoid_rewrite interp_helix_bind.
+        pose proof @commut_gen as CG.
+        admit.
+      }
+      specialize (H memH).
+      assert (Equivalence (⤉ (@eq memoryH, @equiv mem_block _))). {
+        intros. apply (@option_pair_rel_Equiv memoryH mem_block eq equiv _ mem_block_Equiv_Equivalence).
+      }
+
+      pose proof @trans_rcompose.
+      (* TODO: Make rcompose_trans an [eq_rel] *)
+      assert (@eq_rel
+                (option (memoryH * mem_block)) _
+                (rcompose (⤉ (eq, equiv)) (⤉ (eq, equiv))) (⤉ (eq, equiv))).
+      {
+        repeat intro. split; repeat intro. inv H2. etransitivity; eauto.
+        econstructor; eauto. reflexivity.
+      }
+
+      rewrite <- H2.
+
+      eapply eqit_trans.
+
+      (* Bubbling up one step. *)
+      {
+        eapply eutt_clo_bind.
+        apply H.
+        intros.
+        Unshelve.
+        2 : {
+          exact (fun x =>
+            match x with
+            | Some (mem', v) =>
+              interp_helix
+                (tfor (λ (i0 : nat) (acc : mem_block),
+                        DSHIMap_body σ f (n - i0) init acc) (S n - i) (S n) v) mem'
+            | None => Ret None
+            end).
+        }
+        inv H3. reflexivity. unfold pair_rel in H4. destruct x; destruct y.
+        destruct H4; subst.
+        pose proof mem_block_Equiv_Equivalence.
+        assert (forall E f i n, Proper (@equiv mem_block _ ==> eutt eq) (@tfor E mem_block f i n)). {
+          repeat intro. admit.
+        }
+        rewrite H4.
+        reflexivity.
+      }
+
+      (* One step bubbled on the LHS. *)
+      clear H H0 H1 H2.
+      unfold DSHIMap_tfor_down, DSHIMap_tfor_up in IHn.
+
   Admitted.
-
-  Notation mem := mem_block.
-
-  Definition predicate := mem -> Prop.
-
-  Definition mem_disjoint (m1 m2 : mem) :=
-      let kx := mem_keys_set m1 in
-      let ky := mem_keys_set m2 in
-      is_disjoint kx ky.
-
-  Definition star (p1 p2 : predicate) : predicate :=
-    (fun m => exists m1 m2, m = mem_union m1 m2 /\
-                    p1 m1 /\ p2 m2 /\
-                    mem_disjoint m1 m2).
-
-  Notation "m1 ⊍ m2" := (mem_union m1 m2).
-  Notation "m1 ⟂ m2" := (mem_disjoint m1 m2) (at level 40).
-
 
 End DSHIMap_is_tfor.
