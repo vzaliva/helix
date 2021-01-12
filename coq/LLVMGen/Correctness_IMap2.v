e3529ac867350f5dc26cd2af590a5102cc3407cb
euisuny <ey222@cornell.edu>
2021-01-11 23:10:40 -0500

    More work on IMap

--- coq/LLVMGen/Correctness_IMap2.v
+++ coq/LLVMGen/Correctness_IMap2.v
@@ -55,11 +55,11 @@ Section DSHIMap_is_tfor.
   Definition DSHIMap_tfor_down
              (σ : evalContext)
              (f : AExpr)
-             (i n : nat)
+             (i n e: nat)
              (init acc : mem_block):
     itree Event mem_block :=
     (* IMap has "reverse indexing" on its body *)
-    tfor (fun i acc => DSHIMap_body σ f (n - 1 - i) init acc) i n acc.
+    tfor (fun i acc => DSHIMap_body σ f (e - 1 - i) init acc) i n acc.
 
   (* "Right"-side-up indexing variant ([0 .. n - 1]). *)
   Definition DSHIMap_tfor_up
@@ -74,7 +74,7 @@ Section DSHIMap_is_tfor.
      [IMap] body. *)
   Lemma denoteDSHIMap_as_tfor:
     forall (σ : evalContext) n f x y,
-      denoteDSHIMap n f σ x y ≈ DSHIMap_tfor_down σ f 0 n x y.
+      denoteDSHIMap n f σ x y ≈ DSHIMap_tfor_down σ f 0 n n x y.
   Proof.
     intros.
     unfold DSHIMap_tfor_down. revert y.
@@ -108,7 +108,7 @@ Section DSHIMap_is_tfor.
     ∀ (n : nat) (init_vec : memoryH) x y σ f,
       eutt (E := void1) (Coqlib.option_rel (pair_rel (@eq memoryH) (@equiv mem_block _)))
         (interp_helix (DSHIMap_tfor_up σ f 0 n x y) init_vec)
-        (interp_helix (DSHIMap_tfor_down σ f 0 n x y) init_vec).
+        (interp_helix (DSHIMap_tfor_down σ f 0 n n x y) init_vec).
   Proof.
     intros *.
     unfold DSHIMap_tfor_up, DSHIMap_tfor_down.
@@ -142,109 +142,205 @@ Section DSHIMap_is_tfor.
   Instance option_pair_Equivalence: (Equivalence (⤉ (@eq memoryH, @equiv mem_block _))).
   Proof.
     intros. apply (@option_pair_rel_Equiv memoryH mem_block eq equiv _ mem_block_Equiv_Equivalence).
-  Admitted.
+  Qed.
+
+
+  Lemma imap_body_post:
+    forall E m1 m2 init σ f i,
+      no_failure (interp_helix (E := E) (DSHIMap_body σ f i init m2) m1) ->
+    forall m0 b t0 b1,
+      MInt64asNT.from_nat i ≡ inr t0 ->
+      mem_lookup_err "Error reading memory denoteDSHIMap" i init ≡ inr b ->
+      Returns (E := E) (Some (m0, b1)) (interp_helix (denoteIUnCType σ f t0 b) m1) ->
+      Returns (Some (m0, mem_add i b1 m2)) (interp_helix (E := E) (DSHIMap_body σ f i init m2) m1).
+  Proof.
+    intros. cbn in *.
+    assert (H' := H).
+    apply no_failure_helix_bind_prefix in H.
+    rewrite interp_helix_bind.
+    unfold lift_Derr in H.
+
+    destruct (mem_lookup_err "Error reading memory denoteDSHIMap" i init) eqn: MEM.
+
+    try_abs.
+    cbn. rewrite interp_helix_ret. cbn. rewrite bind_ret_l.
+
+
+    eapply no_failure_helix_bind_continuation in H'.
+    2 : {
+      cbn. rewrite interp_helix_ret. constructor. cbn. reflexivity.
+    }
+
+    clear H.
+    assert (H := H').
+    eapply no_failure_helix_bind_prefix in H'.
+    unfold lift_Serr in H'. destruct (MInt64asNT.from_nat i) eqn: HM. cbn in *.
+    try_abs. cbn.
+    rewrite interp_helix_bind; cbn. rewrite interp_helix_ret. cbn.
+    rewrite bind_ret_l.
+    rewrite interp_helix_bind.
+
+    eapply no_failure_helix_bind_continuation in H.
+    2 : {
+      cbn. rewrite interp_helix_ret. constructor. cbn. reflexivity.
+    }
+
+
+    inv H0. inv H1.
+    eapply no_failure_helix_bind_prefix in H.
+    red in H.
+    eapply Returns_bind. apply H2. cbn. rewrite interp_helix_ret. cbn. constructor. reflexivity.
+  Qed.
 
   (* < Generalized Lemma > Equivalent to Desired lemma when i = 0 *)
-  (*
-      Rough proof sketch for inductive case:
-
-      We want to show:
-         (0 ... (S m)) (e ... (S m)) ≈ ((S m) ... i) ((S m) ... e)
-      Where we have as IH:
-         (i ... m) (e ... m) ≈ (m ... i) (m ... e)
-
-      1) First manipulate the RHS...
-      By [commut_gen] and [tfor_ss_dep], the RHS can be rewritten to the following:
-         (0 ... m) (m) (e ... (S m)) ≈ (m ... i) (m) ((S m) ... e)
-      By [eutt_clo_bind] and [tfor_unroll], this is equivalent to:
-         (0 ... m) (m) (e ... (S m)) ≈ (m ... i) (m ... e)
-
-      2) At this point, we can use the IH to rewrite on the RHS.
-
-      3) Clean up the LHS.
-        Since we know that (i ... (S m)) ≈ (i .. m) (m) (TODO: aux. lemma)
-         (0 ... (S m)) (e ... (S m)) ≈ ((S m) ... i) ((S m) ... e) is equivalent to
-         (0 ... m) (m) (e ... (S m)) ≈ ((S m) ... i) ((S m) ... e)
-
-      Using [commut_gen] [tfor_unroll] again similarly on the LHS, we can show
-      that both sides are equivalent to each other. *)
-  Lemma mem_block_equiv_is_order_independent_gen :
-    ∀ (i n : nat) (memH : memoryH) σ f init,
-      0 <= i < n ->
-      eutt (E := void1) (⤉ (eq, equiv))
-           (interp_helix (vec <- DSHIMap_tfor_up σ f 0 (n - i) init init;;
-                          DSHIMap_tfor_down σ f (n - i) n init vec) memH)
-           (interp_helix (vec <- DSHIMap_tfor_down σ f 0 (n - i) init init;;
-                          DSHIMap_tfor_up σ f (n - i) n init vec) memH).
+  Lemma swap_ind:
+    ∀ n i : nat,
+      (∀ (memH : memoryH) (σ : evalContext) (f : AExpr) (init : mem_block),
+          eutt (E := void1) (⤉ (eq, equiv))
+            (interp_helix (vec <- tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) 0 i init;;
+              tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n - 1 - i0) init acc) 0 (n - i) vec) memH)
+            (interp_helix (vec <- tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (S i - 1 - i0) init acc) 0 i init;;
+              tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) (S i) n vec) memH))
+      → ∀ (memH : memoryH) (σ : evalContext) (f : AExpr) (init : mem_block),
+        0 < S i
+        → S i < n
+        -> no_failure (E := void1) (interp_helix (vec <- tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) 0 (S i) init;;
+                                tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n - 1 - i0) init acc) 0 (n - S i) vec) memH)
+        → eutt (E := void1) (⤉ (eq, equiv))
+          (interp_helix (vec <- tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) 0 (S i) init;;
+          tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n - 1 - i0) init acc) 0 (n - S i) vec) memH)
+          (interp_helix (vec <- tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (S (S i) - 1 - i0) init acc) 0 (S i) init;;
+            tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) (S (S i)) n vec) memH).
   Proof.
-    intros * [LO HI].
-    assert (INEQ: n - i > 0) by lia.
+    intros n i IHi memH σ f init LO HI NOFAIL.
     Opaque DSHIMap_body.
-    revert n i INEQ memH σ f init LO HI.
-    intros n i.
-    induction (n - i); intros.
-    - inv INEQ.
-    - clear INEQ.
-      unfold DSHIMap_tfor_up, DSHIMap_tfor_down in *.
-      assert (EQ: S n0 - 1 ≡ n0) by lia; rewrite EQ; clear EQ.
+    assert (EQ: S (S i) - 1 ≡ S i) by lia; rewrite EQ; clear EQ.
 
-      (* 1) RHS unrolling and swapping *)
-      setoid_rewrite tfor_unroll at 2. 2 : shelve. (* TODO: bounds *)
-      assert (EQ: n0 - 0 ≡ n0) by lia; rewrite EQ; clear EQ.
+      setoid_rewrite tfor_split with (j := i) at 1; try lia.
 
       assert (
-        SWAP_RHS:
-          eutt (E := void1) eq
-              (interp_helix (a <- DSHIMap_body σ f n0 init init ;;
-                tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n0 - i0) init acc) 1 (S n0) a) memH)
-              (interp_helix (a <- tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n0 - i0) init acc) 1 (S n0) init ;;
-                DSHIMap_body σ f n0 init a) memH)). {
+        SWAP_LHS:
+          eutt (E := void1) (⤉ (eq, equiv))
+
+            (interp_helix (vec <- (tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) 0 i init) ;;
+                           vec <- (tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) i (S i) vec) ;;
+                            tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n - 1 - i0) init acc) 0 (n - S i) vec) memH)
+
+            (interp_helix (vec <- (tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) 0 i init) ;;
+                           (tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n - 1 - i0) init acc) 0 (n - i) vec)) memH)). {
         intros; setoid_rewrite interp_helix_bind.
+        eapply eutt_clo_bind. reflexivity.
+
+        intros [[] |] [[]|] EQ; inv EQ.
+        2 : reflexivity.
+        setoid_rewrite tfor_split with (j := n - S i) at 2; try lia.
+        cbn.
+
+        assert (
+           H: tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n - 1 - i0) init acc) (n - S i) (n - i) m2 ≈
+                 tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) i (S i) m2). {
+          rewrite tfor_unroll. 2 : lia.
+          assert (EQ : S (n - S i) ≡ n - i) by lia; rewrite EQ; clear EQ.
+          assert (EQ : n - 1 - (n - S i) ≡ i) by lia; rewrite EQ; clear EQ.
+          setoid_rewrite tfor_0. rewrite bind_ret_r. rewrite tfor_unroll; try lia.
+          setoid_rewrite tfor_0. rewrite bind_ret_r.
+          reflexivity.
+        }
+
+        setoid_rewrite <- H; clear H.
+
+        rewrite interp_helix_bind.
+
+        assert (forall m m2, eutt (E := void1) eq
+              (interp_helix (tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n - 1 - i0) init acc) (n - S i) (n - i) m2) m)
+              (interp_helix (DSHIMap_body σ f i init m2) m)). {
+          intros.
+          rewrite tfor_unroll.
+          assert (EQ : n - 1 - (n - S i) ≡ i) by lia; rewrite EQ; clear EQ.
+          assert (EQ : S (n - S i) ≡ n - i) by lia; rewrite EQ; clear EQ.
+          setoid_rewrite tfor_0. rewrite bind_ret_r. reflexivity. lia.
+        }
+
+        setoid_rewrite H.
+        match goal with
+        | [ |- _ ?R ] => remember R as RHS
+        end.
+        assert (RHS ≈
+          interp_helix (vec <- tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (n - 1 - i0) init acc) 0 (n - S i) m2;;
+                        (DSHIMap_body σ f i init vec)) m1). {
+          subst. setoid_rewrite interp_helix_bind.
+          eapply eutt_clo_bind. reflexivity.
+
+          intros [[] |] [[] |] EQ; inv EQ. 2 : reflexivity.
+          apply H.
+        }
+        rewrite H0. clear H0 HeqRHS H RHS.
+
+        setoid_rewrite interp_helix_bind.
+
+        (* match goal with *)
+        (* | [ |- eutt _ (ITree.bind ?pre ?post) (ITree.bind ?pre' ?post') ] => *)
+        (*     remember pre as PRE ; remember post as POST; *)
+        (*     remember pre' as PRE' ; remember post' as POST' *)
+        (* end. *)
+
+        (* eapply commut_gen'.  *)
+        (*     (Q1 := fun x => Returns x PRE) *)
+        (*     (Q2 := fun x => Returns x PRE'). *)
+        (* - admit. *)
+        (* - admit. *)
+        (* - rewrite has_post_post_strong. eapply eutt_Returns_. eauto. *)
+        (* - rewrite has_post_post_strong. eapply eutt_Returns_. eauto. *)
+        (* - intros. rewrite has_post_post_strong. eapply eutt_Returns_. *)
+        (*   intros [[] |] EQ. subst. *)
+        (*   destruct a as [[] |]. *)
+
+
+
+        (*   intros. *)
+        (* - admit. *)
+        (* - admit. *)
+        admit.
+      }
+
+      setoid_rewrite bind_bind.
+      rewrite SWAP_LHS; clear SWAP_LHS.
+      rewrite IHi; try lia. clear IHi.
+
+      assert (EQ : S i - 1 ≡ i) by lia; rewrite EQ; clear EQ.
+      setoid_rewrite tfor_unroll at 2.
+
+      assert (EQ : S i - 0 ≡ S i) by lia; rewrite EQ; clear EQ.
+
+      assert (
+        SWAP_LHS:
+          eutt (E := void1) eq
+               (interp_helix
+                  (vec <- DSHIMap_body σ f (S i) init init;;
+                    tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (S i - i0) init acc) 1 (S i) vec ) memH)
+               (interp_helix
+                    (vec <- tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f (S i - i0) init acc) 1 (S i) init ;;
+                     DSHIMap_body σ f (S i) init vec) memH)). {
+        setoid_rewrite interp_helix_bind.
         eapply commut_gen; admit.
       }
+      all : try lia.
 
-      setoid_rewrite interp_helix_bind.
-
-      (* TODO: SWAP_RHS [eq] should be ⤉ (eq, equiv); another [eutt_clo_bind] is necessary... *)
-      rewrite SWAP_RHS; clear SWAP_RHS.
-      setoid_rewrite <- interp_helix_bind.
+      setoid_rewrite interp_helix_bind at 2.
+      rewrite SWAP_LHS; clear SWAP_LHS.
+      rewrite <- interp_helix_bind.
+      rewrite tfor_ss_dep.
+      all : try lia.
+      2 : { Unshelve. 4 : exact (fun n mem_bl => DSHIMap_body σ f (i - n) init mem_bl). intros; reflexivity. shelve. shelve. }
       setoid_rewrite bind_bind.
-      rewrite tfor_ss_dep. Unshelve. 7 : { exact (fun i0 x => DSHIMap_body σ f (n0 - 1 - i0) init x). }
+      setoid_rewrite interp_helix_bind.
+      eapply eutt_clo_bind. reflexivity.
 
-      assert (
-        UNROLL: eutt (E := void1) (⤉ (eq, equiv))
-             (interp_helix (vec <- tfor (λ (i0 : nat) (x : mem_block), DSHIMap_body σ f (n0 - 1 - i0) init x) 0 n0 init;;
-                            (acc' <- DSHIMap_body σ f n0 init vec;;
-                             tfor (λ (i0 : nat) (acc : mem_block), DSHIMap_body σ f i0 init acc) (S n0) n acc')) memH)
-             (interp_helix (vec <- tfor (λ (i : nat) (acc : mem_block), DSHIMap_body σ f (n0 - 1 - i) init acc) 0 n0 init;;
-                            tfor (λ (i : nat) (acc : mem_block), DSHIMap_body σ f i init acc) n0 n vec) memH)). {
-        setoid_rewrite tfor_unroll.
-        assert (EQ: n0 - 1 - 0 ≡ n0 - 1) by lia; rewrite EQ; clear EQ.
-        setoid_rewrite interp_helix_bind.
-        setoid_rewrite interp_helix_bind.
-        rewrite 2 bind_bind.
-        eapply eutt_clo_bind. reflexivity.
-        intros [[] |] [[]|] EQ; inv EQ.
-        eapply eutt_clo_bind. reflexivity.
-        intros [[] |] [[]|] EQ; inv EQ.
-        rewrite tfor_unroll. reflexivity. admit.
-        reflexivity.
-        eapply eutt_clo_bind. reflexivity.
-        intros [[] |] [[]|] EQ; inv EQ.
-        rewrite tfor_unroll. reflexivity. admit.
-        reflexivity. admit. admit.
-        (* TODO: admits are w.r.t bounds reasoning; should be trivial to dismiss *)
-      }
-
-      rewrite UNROLL; clear UNROLL.
-
-      (* Proper... *)
-
-      (* 2) Apply IH *)
-      rewrite <- IHn0.
-
-      (* LHS unrolling and swapping *)
-      rewrite tfor_unroll.
+      intros [[] |] [[] |] EQ; inv EQ. 2 : reflexivity.
+      rewrite tfor_split with (j := (S (S i))); try lia.
+      rewrite tfor_unroll. setoid_rewrite tfor_0.
+      rewrite bind_ret_r. reflexivity. lia.
+      Unshelve.
 
   Admitted.
 
