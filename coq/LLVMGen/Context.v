67050a7c2eb1df2e38e436316840bc21e363d579
euisuny <ey222@cornell.edu>
2021-01-20 16:41:43 -0500

    IMapBody with no swap; passes unit tests

--- coq/LLVMGen/Context.v
+++ coq/LLVMGen/Context.v
@@ -202,8 +202,7 @@ Proof.
       subst_Γs;
       auto.
   - simp.
-    rewrite Heql2 in H2. inv H2; auto.
-    rewrite Heqs4 in Heql4. inv Heql4; auto.
+    rewrite Heqs4 in H1. inv H1; auto.
   - simp. rewrite Heqs2 in Heql1; inv Heql1; auto.
   - simp. rewrite Heqs0 in Heql1; inv Heql1; auto.
   - eapply IHop1 in Heqs2; eauto.
