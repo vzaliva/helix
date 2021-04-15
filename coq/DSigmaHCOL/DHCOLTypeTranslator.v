2a185f4b22551c9f11f9814a82e252439bd3cbbc
Vadim Zaliva <lord@crocodile.org>
2021-04-14 20:49:03 -0700

    Revert "removed old workaround for universe inconsistency (not fixed)
    This reverts commit 98d1a04eb3e6aba8f8e8404bdc544261bac5f4d7.

--- coq/DSigmaHCOL/DHCOLTypeTranslator.v
+++ coq/DSigmaHCOL/DHCOLTypeTranslator.v
@@ -73,10 +73,34 @@ Module MDHCOLTypeTranslator
 
   Set Universe Polymorphism.
 
+  (* This should be defined as:
+
+   Definition NM_err_sequence
+           {A: Type}
+           (mv: NM.t (err A)): err (NM.t A)
+           := @NM_sequence A err Monad_err mv.
+
+   But it gives us a problem:
+
+   The term "Monad_err" has type "Monad err" while it is expected to have type
+   "Monad (fun B : Type => err B)".
+
+   *)
   Definition NM_err_sequence
              {A: Type}
              (mv: NM.t (err A)): err (NM.t A)
-    := @NM_sequence A err Monad_err mv.
+    := NM.fold
+         (fun k v acc =>
+            match v with
+            | inr v' =>
+              match acc with
+              | inr acc' => inr (NM.add k v' acc')
+              | inl msg => inl msg
+              end
+            | inl msg => inl msg
+            end)
+         mv
+         (inr (@NM.empty A)).
 
   (* This should use [NM_sequence] directly making [NM_err_sequence] unecessary, but we run into universe inconsistency *)
   Definition translate_mem_block (m:L.mem_block) : err L'.mem_block
