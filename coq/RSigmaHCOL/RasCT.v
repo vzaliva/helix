a5b2a7f49098a916d93d2e52d30433365e21ad2d
Ilia Zaichuk <zoickx@ztld.org>
2022-01-17 19:50:46 +0200

    Provide example instances for R as CarrierA

--- coq/RSigmaHCOL/RasCT.v
+++ coq/RSigmaHCOL/RasCT.v
@@ -75,3 +75,101 @@ Module MRasCT <: CType.
   Proof. solve_proper. Qed.
 
 End MRasCT.
+
+Require Import Helix.HCOL.CarrierType.
+
+Definition Abs_R : @Abs R R_Equiv Rle R0 Ropp.
+  econstructor.
+  instantiate (1:=Rabs x).
+  split; intros Z.
+  -
+    apply Rabs_right.
+    now apply Rle_ge.
+  -
+    now apply Rabs_left1.
+Defined.
+
+Instance CarrierDefs_R : CarrierDefs :=
+  {
+      CarrierA := R
+    ; CarrierAe := R_Equiv
+    ; CarrierAle := Rle
+    ; CarrierAlt := Rlt
+    ; CarrierAltdec := Rlt_dec
+    ; CarrierAequivdec := R.OT.eq_dec
+    ; CarrierAz := R0
+    ; CarrierA1 := R1
+    ; CarrierAplus := Rplus
+    ; CarrierAmult := Rmult
+    ; CarrierAneg := Ropp
+    ; CarrierAabs := Abs_R
+  }.
+  
+Instance CarrierProperties_R : @CarrierProperties CarrierDefs_R.
+Proof.
+  constructor.
+  all: unfold CarrierA, CarrierAle, CarrierAlt, CarrierDefs_R.
+  -
+    typeclasses eauto.
+  -
+    repeat constructor.
+    all: try typeclasses eauto.
+    all: cbn.
+    all: repeat try intros ?.
+    all: try eapply Rsrt.
+    + rewrite Radd_comm; apply Rsrt.
+    + apply Rplus_opp_l.
+    + rewrite Rmul_comm; apply Rsrt.
+    + apply Rmult_plus_distr_l.
+  -
+    constructor; try typeclasses eauto; repeat intros ?.
+    constructor; try typeclasses eauto; repeat intros ?.
+    constructor; try typeclasses eauto; repeat intros ?.
+    + now right.
+    + eapply Rle_trans; eassumption.
+    + now apply Rle_antisym.
+    + pose proof Rle_or_lt x y as [T | T];
+        [tauto | right; now apply Rlt_le].
+  -
+    constructor.
+    all: typeclasses eauto.
+  -
+    constructor; try typeclasses eauto; repeat intros ?.
+    constructor; try typeclasses eauto; repeat intros ?.
+    constructor; try typeclasses eauto; repeat intros ?.
+    all: try (cbv in *; congruence).
+    +
+      destruct (R.OT.eq_dec x z);
+        [right | left]; congruence.
+    +
+      destruct (R.OT.eq_dec x y);
+        now cbv in *.
+    +
+      destruct H.
+      pose proof Rlt_trans _ _ _ H H0.
+      now pose proof Rlt_irrefl x.
+    +
+      pose proof Rlt_dec x z as [T | T];
+        [tauto |].
+      right.
+      apply Rnot_lt_le in T.
+      eapply Rle_lt_trans; eassumption.
+    +
+      pose proof Req_appart_dec x y as [EQ | DEC].
+      *
+        subst.
+        split; repeat intros ?.
+        now cbv in *.
+        pose proof Rlt_irrefl y.
+        tauto.
+      *
+        destruct DEC;
+          split; intros;
+          try tauto.
+        now apply Rlt_not_eq.
+        intros C.
+        eapply Rlt_not_eq; [eassumption | congruence].
+    +
+      split;
+        [apply Rle_not_lt | apply Rnot_lt_le].
+Qed.
