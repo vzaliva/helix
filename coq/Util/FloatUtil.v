a79af90a8bf9b49ba047b3a178a59a54ac4088cf
Ilia Zaichuk <zoickx@ztld.org>
2022-05-20 20:37:25 +0300

    Add some general lemmas about floats

--- coq/Util/FloatUtil.v
+++ coq/Util/FloatUtil.v
@@ -363,61 +363,93 @@ Section Float64.
     congruence.
   Qed.
 
+  Lemma b64_max_to_R (x y : binary64) :
+    is_finite _ _ x ≡ true ->
+    is_finite _ _ y ≡ true ->
+    B64R (Float64Max x y) ≡ Rmax (B64R x) (B64R y).
+  Proof.
+    intros FX FY.
+    unfold Float64Max.
+    pose proof Rcompare_spec (B64R x) (B64R y) as RC.
+    rewrite Bcompare_correct by assumption.
+    destruct x, y;
+      invc FX; invc FY.
+    all: repeat
+           match goal with
+           | |- context [Rmax (B64R ?x) (B64R ?y)] =>
+               generalize dependent x;
+               generalize dependent y
+           end;
+      intros.
+    all: unfold Rmax.
+    all: repeat break_match.
+    all: invc RC.
+    all: lra.
+  Qed.
+  
+  Lemma b64_max_finite (x y : binary64) :
+    is_finite _ _ x ≡ true ->
+    is_finite _ _ y ≡ true ->
+    is_finite _ _ (Float64Max x y) ≡ true.
+  Proof.
+    intros FX FY.
+    unfold Float64Max.
+    destruct x, y;
+      invc FX; invc FY.
+    all: repeat break_match; reflexivity.
+  Qed.
+
+  Lemma b64_abs_to_R (f : binary64) :
+    B64R (b64_abs f) ≡ Rabs (B64R f).
+  Proof.
+    apply B2R_Babs.
+  Qed.
+  
+  Lemma b64_abs_finite (f : binary64) :
+    is_finite _ _ f ≡ true ->
+    is_finite _ _ (b64_abs f) ≡ true.
+  Proof.
+    intros.
+    unfold b64_abs.
+    now rewrite is_finite_Babs.
+  Qed.
+
+  Lemma float64_subnormal_eps_correct (x : binary64) :
+    is_finite _ _ x ≡ true ->
+    lt64 b64_0 x ->
+    le64 float64_subnormal_eps x.
+  Proof.
+    intros F NZ.
+    destruct x; invc F; invc NZ.
+    break_if; invc H0.
+    unfold le64, b64_compare.
+    rewrite !Bcompare_correct in *; try reflexivity.
+    pose proof e0 as B.
+    eapply bounded_ge_emin in B; [| reflexivity].
+    unfold B64R.
+    cbn.
+    assert (MIN : bpow radix2 (3 - 1024 - 53)
+            ≡ @Defs.F2R radix2 {| Defs.Fnum := 1; Defs.Fexp := -1074 |})
+      by (cbv; lra).
+    rewrite MIN in *; clear MIN.
+    generalize dependent
+      (@Defs.F2R radix2 {| Defs.Fnum := 1; Defs.Fexp := -1074 |}).
+    intros.
+    destruct B; [right | left].
+    now rewrite Rcompare_Lt.
+    now rewrite Rcompare_Eq.
+  Qed.
+
+  Lemma float_lt_to_le (x y : binary64) :
+    B64R x < B64R y ->
+    B64R float64_subnormal_eps <= B64R y - B64R x.
+  Admitted.
+
+  Fact b64_0_correct : B64R b64_0 ≡ 0.
+  Proof. reflexivity. Qed.
+  
+  Fact b64_1_correct : B64R b64_1 ≡ 1.
+  Proof. cbv. lra. Qed.
+
 End Float64.
 
-Section Float64CT.
-
-End Float64CT.
-
-Section Constants.
-
-  (* 0.0 *)
-  Definition b64_0 := B754_zero 53 1024 false.
-  (* 0.1 *)
-  Definition b64_0_1 :=
-    B754_finite 53 1024 false 7205759403792794 (-56)
-      (binary_float_of_bits_aux_correct 52 11 eq_refl eq_refl eq_refl
-         4591870180066957722).
-  (* 0.01 *)
-  Definition b64_0_01 :=
-    B754_finite 53 1024 false 5764607523034235 (-59)
-      (binary_float_of_bits_aux_correct 52 11 eq_refl eq_refl eq_refl
-         4576918229304087675).
-  (* 1.0 *)
-  Definition b64_1 :=
-    B754_finite 53 1024 false 4503599627370496 (-52)
-      (binary_float_of_bits_aux_correct 52 11 eq_refl eq_refl eq_refl
-         4607182418800017408).
-  (* 2.0 *)
-  Definition b64_2 :=
-    B754_finite 53 1024 false 4503599627370496 (-51)
-      (binary_float_of_bits_aux_correct 52 11 eq_refl eq_refl eq_refl
-         4611686018427387904).
-  (* 5.0 *)
-  Definition b64_5 :=
-    B754_finite 53 1024 false 5629499534213120 (-50)
-      (binary_float_of_bits_aux_correct 52 11 eq_refl eq_refl eq_refl
-         4617315517961601024).
-  (* 6.0 *)
-  Definition b64_6 :=
-    B754_finite 53 1024 false 6755399441055744 (-50)
-      (binary_float_of_bits_aux_correct 52 11 eq_refl eq_refl eq_refl
-         4618441417868443648).
-  (* 20.0 *)
-  Definition b64_20 :=
-    B754_finite 53 1024 false 5629499534213120 (-48)
-      (binary_float_of_bits_aux_correct 52 11 eq_refl eq_refl eq_refl
-         4626322717216342016).
-  (* 5000.0 *)
-  Definition b64_5000 :=
-    B754_finite 53 1024 false 5497558138880000 (-40)
-      (binary_float_of_bits_aux_correct 52 11 eq_refl eq_refl eq_refl
-         4662219572839972864).
-
-End Constants.
-
-Global Hint Unfold
-  b64_0 b64_0_1 b64_0_01 b64_1 b64_2 b64_5 b64_6 b64_20 b64_5000
-  : F64_const.
-
-  
